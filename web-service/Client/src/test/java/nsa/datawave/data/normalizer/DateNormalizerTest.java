63ad9c9c0a30ac098871ac30c501033f394287cc
Tom Nelson <thomas.a.nelson@gmail.com>
2017-05-08 15:03:58 -0400

    closes #30. Added a format string with ISO 8601 capable XXX tokens  Changes to be committed: 	modified:   web-service/Client/src/main/java/nsa/datawave/data/normalizer/DateNormalizer.java 	modified:   web-service/Client/src/test/java/nsa/datawave/data/normalizer/DateNormalizerTest.java

--- web-service/Client/src/test/java/nsa/datawave/data/normalizer/DateNormalizerTest.java
+++ web-service/Client/src/test/java/nsa/datawave/data/normalizer/DateNormalizerTest.java
@@ -27,7 +27,8 @@ public class DateNormalizerTest {
     DateNormalizer normalizer = new DateNormalizer();
     
     String[] inputDateStrings = {"2014-10-20T00:00:00.000Z", "20141020000000", "2014-10-20 00:00:00GMT", "2014-10-20 00:00:00Z", "2014-10-20 00:00:00",
-            "2014-10-20", "2014-10-20T00|00", "Mon Oct 20 00:00:00 GMT 2014", "2014-10-20T00:00:00Z", "2014-10-20t00:00:00z", "2014-10-20T00:00:00+00:00"};
+            "2014-10-20", "2014-10-20T00|00", "Mon Oct 20 00:00:00 GMT 2014", "2014-10-20T00:00:00Z", "2014-10-20t00:00:00z", "2014-10-20T00:00:00+00:00",
+            "Mon Oct 20 00:00:00 +00:00 2014"};
     
     @Test
     public void testAllFormats() throws Exception {
