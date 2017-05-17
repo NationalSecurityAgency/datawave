92fa6c9841d75566b7b2a80ebb0929d2e7a230b9
Ivan Bella <ivan@bella.name>
2017-05-17 15:38:25 -0400

    Fixes to the Ancestor logic

--- web-service/Client/src/main/java/nsa/datawave/webservice/modification/ModificationEventBase.java
+++ web-service/Client/src/main/java/nsa/datawave/webservice/modification/ModificationEventBase.java
@@ -8,6 +8,7 @@ import javax.xml.bind.annotation.XmlAccessorOrder;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
