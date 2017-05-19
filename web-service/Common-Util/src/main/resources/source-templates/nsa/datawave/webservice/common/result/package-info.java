2ebee8c80ad9660c4f05a1e8420b9a968c92ec79
Brian Loss <brianloss@gmail.com>
2017-05-19 15:11:41 -0400

    Add package-info.java files for some packages that had XmlRootElement annotated classes but no existing package-info.java.

---
+++ web-service/Common-Util/src/main/resources/source-templates/nsa/datawave/webservice/common/result/package-info.java
@@ -0,0 +1,6 @@
+@XmlSchema(namespace="${datawave.webservice.namespace}", elementFormDefault=XmlNsForm.QUALIFIED)
+package nsa.datawave.webservice.common.result;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
---
+++ web-service/Security/src/main/resources/source-templates/nsa/datawave/security/cache/package-info.java
@@ -0,0 +1,6 @@
+@XmlSchema(namespace="${datawave.webservice.namespace}", elementFormDefault=XmlNsForm.QUALIFIED)
+package nsa.datawave.security.cache;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
