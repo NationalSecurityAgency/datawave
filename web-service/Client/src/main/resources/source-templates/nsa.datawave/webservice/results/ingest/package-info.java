d4c35fc109b65ec51cbeccc78f669fcbaafa9ced
Brian Loss <brianloss@gmail.com>
2017-05-22 15:42:12 -0400

    Fix up package-info files

--- web-service/Client/src/main/resources/source-templates/nsa.datawave/webservice/results/ingest/package-info.java
+++ web-service/Client/src/main/resources/source-templates/nsa.datawave/webservice/results/ingest/package-info.java
@@ -1,7 +1,6 @@
 @XmlSchema(namespace="${datawave.webservice.namespace}", elementFormDefault=XmlNsForm.QUALIFIED, xmlns={@XmlNs(prefix = "", namespaceURI = "${datawave.webservice.namespace}")})
-package nsa.datawave.webservice.query.result.dqr;
+package nsa.datawave.webservice.results.ingest;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
-
