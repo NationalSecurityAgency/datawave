6d1332cc5c443f6915e40fe324854f7b4dc5c350
Brian Loss <brianloss@gmail.com>
2017-08-21 10:41:45 -0400

    fixes #83: add test case to ensure iterator stack is set up correctly

--- warehouse/query-core/src/test/java/nsa/datawave/mr/bulk/RecordIteratorTest.java
+++ warehouse/query-core/src/test/java/nsa/datawave/mr/bulk/RecordIteratorTest.java
@@ -2,21 +2,129 @@ package nsa.datawave.mr.bulk;
 
 import nsa.datawave.mr.bulk.split.FileRangeSplit;
 import nsa.datawave.mr.bulk.split.TabletSplitSplit;
+import nsa.datawave.security.iterator.ConfigurableVisibilityFilter;
+import org.apache.accumulo.core.client.IteratorSetting;
+import org.apache.accumulo.core.conf.AccumuloConfiguration;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.iterators.SortedKeyValueIterator;
 import org.apache.accumulo.core.iterators.SortedMapIterator;
+import org.apache.accumulo.core.iterators.system.DeletingIterator;
+import org.apache.accumulo.core.iterators.system.MultiIterator;
+import org.apache.accumulo.core.iterators.system.SynchronizedIterator;
+import org.apache.accumulo.core.iterators.system.VisibilityFilter;
+import org.apache.accumulo.core.security.Authorizations;
 import org.apache.hadoop.conf.Configuration;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.powermock.reflect.Whitebox;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import static org.junit.Assert.*;
 
 public class RecordIteratorTest {
+    
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+    
+    @Before
+    public void setup() throws Exception {
+        System.setProperty("hadoop.home.dir", temporaryFolder.getRoot().getCanonicalPath());
+    }
+    
+    @Test
+    public void testIteratorStackSetup() throws Exception {
+        TabletSplitSplit splits = new TabletSplitSplit(3);
+        
+        FileRangeSplit r1 = new FileRangeSplit(new Range(new Key("A123", "cf3123", "cq3"), new Key("A1239999", "cf3123", "cq3")), null, 0, 0, null);
+        FileRangeSplit r2 = new FileRangeSplit(new Range(new Key("B12", "cf212", "cq2"), new Key("B129999", "cf212", "cq2")), null, 0, 0, null);
+        FileRangeSplit r3 = new FileRangeSplit(new Range(new Key("C1", "cf11", "cq1"), new Key("C19999", "cf11", "cq1")), null, 0, 0, null);
+        
+        splits.add(r1);
+        splits.add(r2);
+        splits.add(r3);
+        
+        AccumuloConfiguration acuConf = AccumuloConfiguration.getDefaultConfiguration();
+        Configuration conf = new Configuration();
+        conf.set("recorditer.auth.string", "A1,A2,A3");
+        
+        IteratorSetting cfg1 = new IteratorSetting(1, ConfigurableVisibilityFilter.class);
+        cfg1.setName("visibilityFilter1");
+        cfg1.addOption(ConfigurableVisibilityFilter.AUTHORIZATIONS_OPT, "B1,B2,B3");
+        BulkInputFormat.addIterator(conf, cfg1);
+        
+        IteratorSetting cfg2 = new IteratorSetting(2, ConfigurableVisibilityFilter.class);
+        cfg2.setName("visibilityFilter2");
+        cfg2.addOption(ConfigurableVisibilityFilter.AUTHORIZATIONS_OPT, "C1,C2,C3");
+        BulkInputFormat.addIterator(conf, cfg2);
+        
+        RecordIterator recordIterator = new RecordIterator(splits, acuConf, conf);
+        
+        // Pull out the iterator we configured internally, and make sure the stack is:
+        // ConfigurableVisibilityFilter:
+        // VisibilityFilter: C1,C2,C3
+        // ConfigurableVisibilityFilter:
+        // VisibilityFilter: B1,B2,B3
+        // SynchronizedIterator
+        // VisibilityFilter: A1,A2,A3
+        // DeletingIterator
+        // MultiIterator
+        
+        Object source;
+        VisibilityFilter vf;
+        ConfigurableVisibilityFilter cvf;
+        TreeSet<String> actualAuths;
+        SortedKeyValueIterator internalIter = Whitebox.getInternalState(recordIterator, SortedKeyValueIterator.class);
+        
+        // Top of the stack is a ConfigurableVisibilityFilter (which creates a VisibilityFilter as its source) with auths C1,C2,C3
+        assertEquals(ConfigurableVisibilityFilter.class, internalIter.getClass());
+        cvf = ConfigurableVisibilityFilter.class.cast(internalIter);
+        source = Whitebox.getInternalState(cvf, "source");
+        assertEquals(VisibilityFilter.class, source.getClass());
+        vf = VisibilityFilter.class.cast(source);
+        actualAuths = new TreeSet<>(Arrays.asList(Whitebox.getInternalState(vf, Authorizations.class).toString().split(",")));
+        assertEquals(new TreeSet<>(Arrays.asList("C1", "C2", "C3")), actualAuths);
+        source = Whitebox.getInternalState(vf, "source");
+        
+        // Next on the stack is a ConfigurableVisibilityFilter (which creates a VisibilityFilter as its source) with auths B1,B2,B3
+        assertEquals(ConfigurableVisibilityFilter.class, source.getClass());
+        cvf = ConfigurableVisibilityFilter.class.cast(source);
+        source = Whitebox.getInternalState(cvf, "source");
+        assertEquals(VisibilityFilter.class, source.getClass());
+        vf = VisibilityFilter.class.cast(source);
+        actualAuths = new TreeSet<>(Arrays.asList(Whitebox.getInternalState(vf, Authorizations.class).toString().split(",")));
+        assertEquals(new TreeSet<>(Arrays.asList("B1", "B2", "B3")), actualAuths);
+        source = Whitebox.getInternalState(vf, "source");
+        
+        // Next on the stack is the "table iterators" which should be a SynchronizedIterator first.
+        assertEquals(SynchronizedIterator.class, source.getClass());
+        SynchronizedIterator si = SynchronizedIterator.class.cast(source);
+        source = Whitebox.getInternalState(si, "source");
+        
+        // The next table iterator should be the standard VisibilityFilter which will have auths A1,A2,A3
+        assertEquals(VisibilityFilter.class, source.getClass());
+        vf = VisibilityFilter.class.cast(source);
+        actualAuths = new TreeSet<>(Arrays.asList(Whitebox.getInternalState(vf, Authorizations.class).toString().split(",")));
+        assertEquals(new TreeSet<>(Arrays.asList("A1", "A2", "A3")), actualAuths);
+        source = Whitebox.getInternalState(vf, "source");
+        
+        // The next table iterator should be a DeletingIterator
+        assertEquals(DeletingIterator.class, source.getClass());
+        DeletingIterator di = DeletingIterator.class.cast(source);
+        source = Whitebox.getInternalState(di, "source");
+        
+        // And finally, at the bottom of the stack is a MultiIterator (which has no source field)
+        assertEquals(MultiIterator.class, source.getClass());
+        
+    }
     
     @Test
     public void testProgressDiffRows() throws IOException, InterruptedException {
