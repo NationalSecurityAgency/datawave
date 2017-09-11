3563626562894f15e2a3bdef350d19726591ab7a
Whitney O'Meara <whitney.omeara@wavestrike.com>
2017-09-11 16:00:03 -0400

    initial config-server commit

---
+++ services/config-service/src/main/java/datawave/microservice/config/server/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package datawave.microservice.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
