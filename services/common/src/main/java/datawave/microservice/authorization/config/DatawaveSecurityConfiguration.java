f9738d823d9ecb269d7602e3c1a7ad8a5b088f60
Brian Loss <brianloss@gmail.com>
2017-09-26 16:07:09 -0400

    re #67: fix code that removes "ROLE_" default role prefix so it can work with other services than authorization

--- services/common/src/main/java/datawave/microservice/authorization/config/DatawaveSecurityConfiguration.java
+++ services/common/src/main/java/datawave/microservice/authorization/config/DatawaveSecurityConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 
 /**
- * Provides a default {@link DatawaveSecurityProperties}.
+ * Provides a default {@link DatawaveSecurityProperties}, and also provide an empty default role prefix for method security annotations. We don't want our roles
+ * to be prefixed with "ROLE_".
  */
 @Configuration
 @EnableConfigurationProperties
@@ -17,5 +19,11 @@ public class DatawaveSecurityConfiguration {
     @ConditionalOnMissingBean
     public DatawaveSecurityProperties datawaveSecurityProperties() {
         return new DatawaveSecurityProperties();
+    }
+    
+    @Bean
+    @ConditionalOnMissingBean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("");
     }
 }
