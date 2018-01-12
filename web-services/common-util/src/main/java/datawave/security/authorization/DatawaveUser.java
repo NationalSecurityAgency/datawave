c4764ca9eab519fe86e6f5bd8d00c3f73785eb6c
Brian Loss <brianloss@gmail.com>
2018-01-12 12:38:28 -0500

    Update audit logging to log to a separate file. Update DatawaveUser to render to HTML.

--- web-services/common-util/src/main/java/datawave/security/authorization/DatawaveUser.java
+++ web-services/common-util/src/main/java/datawave/security/authorization/DatawaveUser.java
@@ -7,22 +7,27 @@ import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 import datawave.security.util.DnUtils;
+import datawave.webservice.HtmlProvider;
 
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * A user of a DATAWAVE service. Typically, one or more of these users (a chain where a user called an intermediate service which in turn called us) is
  * represented with a {@link DatawavePrincipal}.
  */
-public class DatawaveUser implements Serializable {
+public class DatawaveUser implements Serializable, HtmlProvider {
     public enum UserType {
         USER, SERVER
     }
     
     public static final DatawaveUser ANONYMOUS_USER = new DatawaveUser(SubjectIssuerDNPair.of("ANONYMOUS"), UserType.USER, null, null, null, -1L);
+    protected static final String TITLE = "Datawave User", EMPTY = "";
     
     private final String name;
     private final String commonName;
@@ -124,5 +129,93 @@ public class DatawaveUser implements Serializable {
     public String toString() {
         return "DatawaveUser{" + "name='" + getName() + "'" + ", userType=" + getUserType() + ", auths=" + getAuths() + ", roles=" + getRoles()
                         + ", creationTime=" + getCreationTime() + "}";
+    }
+    
+    @Override
+    @JsonIgnore
+    public String getTitle() {
+        return TITLE;
+    }
+    
+    @Override
+    @JsonIgnore
+    public String getHeadContent() {
+        return EMPTY;
+    }
+    
+    @Override
+    @JsonIgnore
+    public String getPageHeader() {
+        return TITLE + " - " + getCommonName();
+    }
+    
+    @Override
+    @JsonIgnore
+    public String getMainContent() {
+        StringBuilder builder = new StringBuilder();
+        
+        builder.append("<table>\n");
+        builder.append("<tr><td>Subject DN:</td><td>").append(getDn().subjectDN()).append("</td></tr>\n");
+        builder.append("<tr class='highlight'><td>User DN:</td><td>").append(getDn().issuerDN()).append("</td></tr>\n");
+        builder.append("<tr><td>User Type:</td><td>").append(getUserType()).append("</td></tr>\n");
+        builder.append("<tr class='highlight'><td>Creation Time:</td><td>").append(getCreationTime()).append("</td></tr>\n");
+        builder.append("<tr><td>Expiration Time:</td><td>").append(getExpirationTime()).append("</td></tr>\n");
+        builder.append("</table>\n");
+        
+        builder.append("<h2>Roles</h2>\n");
+        generateTable(getRoles(), builder);
+        
+        builder.append("<h2>Authorizations</h2>\n");
+        generateTable(getAuths(), builder);
+        
+        builder.append("<h2>Role To Auth Mapping</h2>\n");
+        builder.append("<table>\n");
+        builder.append("<tr><th>Role</th><th>Auth(s)</th><th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th><th>Role</th><th>Auth(s)</th></tr>");
+        boolean highlight = false;
+        for (Iterator<Map.Entry<String,Collection<String>>> iter = getRoleToAuthMapping().asMap().entrySet().iterator(); iter.hasNext(); /* empty */) {
+            if (highlight) {
+                builder.append("<tr class=\"highlight\">");
+            } else {
+                builder.append("<tr>");
+            }
+            highlight = !highlight;
+            
+            int cols = 2;
+            for (int i = 0; i < cols && iter.hasNext(); i++) {
+                if (iter.hasNext()) {
+                    Map.Entry<String,Collection<String>> entry = iter.next();
+                    builder.append("<td>").append(entry.getKey()).append("</td>");
+                    builder.append("<td>").append(entry.getValue().stream().collect(Collectors.joining(", "))).append("</td>");
+                } else {
+                    builder.append("<td></td>");
+                }
+                if (i < (cols - 1))
+                    builder.append("<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>");
+            }
+        }
+        builder.append("</table>\n");
+        
+        return builder.toString();
+    }
+    
+    private void generateTable(Collection<String> entries, StringBuilder builder) {
+        builder.append("<table>\n");
+        boolean highlight = false;
+        for (Iterator<String> iter = entries.iterator(); iter.hasNext(); /* empty */) {
+            if (highlight) {
+                builder.append("<tr class=\"highlight\">");
+            } else {
+                builder.append("<tr>");
+            }
+            highlight = !highlight;
+            
+            for (int i = 0; i < 4 && iter.hasNext(); i++) {
+                builder.append("<td>");
+                builder.append(iter.hasNext() ? iter.next() : "");
+                builder.append("</td>");
+            }
+            builder.append("</tr>");
+        }
+        builder.append("</table>\n");
     }
 }
