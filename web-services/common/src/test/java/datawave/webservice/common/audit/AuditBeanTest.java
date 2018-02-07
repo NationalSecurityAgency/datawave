442131a25686305a5e8f18e9d714edd38bc02f06
Brian Loss <brianloss@gmail.com>
2018-02-07 13:34:16 -0500

    Formatting changes

--- web-services/common/src/test/java/datawave/webservice/common/audit/AuditBeanTest.java
+++ web-services/common/src/test/java/datawave/webservice/common/audit/AuditBeanTest.java
@@ -21,20 +21,20 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class AuditBeanTest {
-
+    
     private AuditBean auditBean;
     private TestAuditor auditor;
-
+    
     private Map<String,List<String>> params = new HashMap<>();
-
+    
     @Before
     public void setup() {
         auditBean = new AuditBean();
         auditor = new TestAuditor();
-
+        
         Whitebox.setInternalState(auditBean, "auditParameters", new AuditParameters());
         Whitebox.setInternalState(auditBean, "auditor", auditor);
-
+        
         params.put(AuditParameters.USER_DN, Arrays.asList("someUser"));
         params.put(AuditParameters.QUERY_STRING, Arrays.asList("someQuery"));
         params.put(AuditParameters.QUERY_SELECTORS, Arrays.asList("sel1", "sel2"));
@@ -42,21 +42,21 @@ public class AuditBeanTest {
         params.put(AuditParameters.QUERY_AUDIT_TYPE, Arrays.asList(Auditor.AuditType.ACTIVE.name()));
         params.put(AuditParameters.QUERY_SECURITY_MARKING_COLVIZ, Arrays.asList("ALL"));
         params.put(AuditParameters.QUERY_DATE, Arrays.asList(Long.toString(new Date().getTime())));
-
+        
     }
-
+    
     @Test
     public void restCallTest() {
-        MultivaluedMap<String, String> mvMapParams = new MultivaluedMapImpl<>();
+        MultivaluedMap<String,String> mvMapParams = new MultivaluedMapImpl<>();
         mvMapParams.putAll(params);
         auditBean.audit(mvMapParams);
-
+        
         AuditParameters expected = new AuditParameters();
         expected.validate(mvMapParams);
-
+        
         AuditParameters actual = new AuditParameters();
         actual.validate(AuditParameters.parseMessage(auditor.params));
-
+        
         assertEquals(expected.getUserDn(), actual.getUserDn());
         assertEquals(expected.getQuery(), actual.getQuery());
         assertEquals(expected.getSelectors(), actual.getSelectors());
@@ -64,16 +64,16 @@ public class AuditBeanTest {
         assertEquals(expected.getAuditType(), actual.getAuditType());
         assertEquals(expected.getColviz(), actual.getColviz());
     }
-
+    
     @Test
     public void internalCallTest() throws Exception {
         AuditParameters auditParams = new AuditParameters();
         auditParams.validate(params);
         auditBean.audit(auditParams);
-
+        
         AuditParameters actual = new AuditParameters();
         actual.validate(AuditParameters.parseMessage(auditor.params));
-
+        
         assertEquals(auditParams.getUserDn(), actual.getUserDn());
         assertEquals(auditParams.getQuery(), actual.getQuery());
         assertEquals(auditParams.getSelectors(), actual.getSelectors());
@@ -81,11 +81,11 @@ public class AuditBeanTest {
         assertEquals(auditParams.getAuditType(), actual.getAuditType());
         assertEquals(auditParams.getColviz(), actual.getColviz());
     }
-
+    
     private static class TestAuditor implements Auditor {
-
-        Map<String, String> params;
-
+        
+        Map<String,String> params;
+        
         @Override
         public void audit(AuditParameters msg) throws Exception {
             params = msg.toMap();
