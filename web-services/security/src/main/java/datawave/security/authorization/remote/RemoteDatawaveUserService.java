1658272accce0210cef32852e7dc6d3281bb2487
Brian Loss <brianloss@gmail.com>
2018-04-25 14:46:39 +0000

    fixes #211: Update HTTPClient/Core
    * Update dependencies
    * Move duplicated code to a common base class
    * Update shutdown to wait for a minute for pending requests to finish before shutting down

--- web-services/security/src/main/java/datawave/security/authorization/remote/RemoteDatawaveUserService.java
+++ web-services/security/src/main/java/datawave/security/authorization/remote/RemoteDatawaveUserService.java
@@ -3,73 +3,34 @@ package datawave.security.authorization.remote;
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.annotation.Metric;
 import com.codahale.metrics.annotation.Timed;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.spotify.dns.DnsSrvResolver;
-import com.spotify.dns.DnsSrvResolvers;
 import com.spotify.dns.LookupResult;
 import datawave.configuration.RefreshableScope;
-import datawave.security.authorization.AuthorizationException;
-import datawave.security.authorization.CachedDatawaveUserService;
-import datawave.security.authorization.DatawaveUser;
-import datawave.security.authorization.DatawaveUserInfo;
-import datawave.security.authorization.SubjectIssuerDNPair;
-import datawave.security.util.DnUtils;
-import datawave.webservice.security.JWTTokenHandler;
+import datawave.security.authorization.*;
+import datawave.webservice.common.remote.RemoteHttpService;
 import datawave.webservice.util.NotEqualPropertyExpressionInterpreter;
 import org.apache.deltaspike.core.api.config.ConfigProperty;
 import org.apache.deltaspike.core.api.exclude.Exclude;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
-import org.apache.http.impl.client.DefaultServiceUnavailableRetryStrategy;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
-import org.jboss.security.JSSESecurityDomain;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.xbill.DNS.DClass;
-import org.xbill.DNS.ExtendedResolver;
 import org.xbill.DNS.Lookup;
 import org.xbill.DNS.Record;
 import org.xbill.DNS.Type;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.annotation.Priority;
 import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
 import javax.interceptor.Interceptor;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLException;
-import javax.net.ssl.X509KeyManager;
 import java.io.IOException;
-import java.net.ConnectException;
 import java.net.URISyntaxException;
-import java.net.UnknownHostException;
-import java.security.Key;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -82,17 +43,10 @@ import java.util.stream.Collectors;
 // Make this alternative active for the entire application per the CDI 1.2 specification
 @Priority(Interceptor.Priority.APPLICATION)
 @Exclude(onExpression = "dw.security.use.remoteuserservice!=true", interpretedBy = NotEqualPropertyExpressionInterpreter.class)
-public class RemoteDatawaveUserService implements CachedDatawaveUserService {
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    
+public class RemoteDatawaveUserService extends RemoteHttpService implements CachedDatawaveUserService {
     private ObjectReader datawaveUserReader;
     private ObjectReader datawaveUserListReader;
     private ObjectReader datawaveUserInfoListReader;
-    private CloseableHttpClient client;
-    private JWTTokenHandler jwtTokenHandler;
-    
-    @Inject
-    private JSSESecurityDomain jsseSecurityDomain;
     
     @Inject
     @ConfigProperty(name = "dw.remoteDatawaveUserService.useSrvDnsLookup", defaultValue = "false")
@@ -145,8 +99,6 @@ public class RemoteDatawaveUserService implements CachedDatawaveUserService {
     @Inject
     @Metric(name = "dw.remoteDatawaveUserService.failures", absolute = true)
     private Counter failureCounter;
-    
-    private DnsSrvResolver dnsSrvResolver;
     
     @Override
     @Timed(name = "dw.remoteDatawaveUserService.lookup", absolute = true)
@@ -312,139 +264,59 @@ public class RemoteDatawaveUserService implements CachedDatawaveUserService {
         uriCustomizer.accept(builder);
         HttpGet getRequest = new HttpGet(builder.build());
         requestCustomizer.accept(getRequest);
-        return client.execute(getRequest, r -> {
-            if (r.getStatusLine().getStatusCode() != 200) {
-                throw new ClientProtocolException("Unable to " + errorSupplier.get() + ": " + r.getStatusLine() + " " + EntityUtils.toString(r.getEntity()));
-            } else {
-                return resultConverter.apply(r.getEntity());
-            }
-        });
+        return super.execute(getRequest, resultConverter, errorSupplier);
     }
     
     @PostConstruct
     protected void init() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new GuavaModule());
+        super.init();
         datawaveUserReader = objectMapper.readerFor(DatawaveUser.class);
         datawaveUserListReader = objectMapper.readerFor(objectMapper.getTypeFactory().constructCollectionType(Collection.class, DatawaveUser.class));
         datawaveUserInfoListReader = objectMapper.readerFor(objectMapper.getTypeFactory().constructCollectionType(Collection.class, DatawaveUserInfo.class));
-        
-        if (useSrvDNS) {
-            if (srvDnsServers != null && !srvDnsServers.isEmpty()) {
-                try {
-                    ExtendedResolver resolver = new ExtendedResolver(srvDnsServers.toArray(new String[srvDnsServers.size()]));
-                    resolver.setLoadBalance(true);
-                    resolver.setPort(srvDnsPort);
-                    Lookup.setDefaultResolver(resolver);
-                } catch (UnknownHostException e) {
-                    throw new IllegalArgumentException("Unable to resolve SRV DNS hosts: " + srvDnsServers + ": " + e.getMessage(), e);
-                }
-            }
-            dnsSrvResolver = DnsSrvResolvers.newBuilder().build();
-        }
-        
-        try {
-            SSLContext ctx = SSLContext.getInstance("TLSv1.2");
-            ctx.init(jsseSecurityDomain.getKeyManagers(), jsseSecurityDomain.getTrustManagers(), null);
-            
-            String alias = jsseSecurityDomain.getKeyStore().aliases().nextElement();
-            X509KeyManager keyManager = (X509KeyManager) jsseSecurityDomain.getKeyManagers()[0];
-            X509Certificate[] certs = keyManager.getCertificateChain(alias);
-            Key signingKey = keyManager.getPrivateKey(alias);
-            
-            jwtTokenHandler = new JWTTokenHandler(certs[0], signingKey, 24, TimeUnit.HOURS, objectMapper);
-            
-            ArrayList<Header> defaultHeaders = new ArrayList<>();
-            defaultHeaders.add(new BasicHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType()));
-            // If we're using HTTP, then add our cert in as a header so the authorization service knows who we are.
-            if ("http".equals(authServiceScheme)) {
-                defaultHeaders.add(new BasicHeader("X-SSL-clientcert-subject", DnUtils.normalizeDN(certs[0].getSubjectX500Principal().getName())));
-                defaultHeaders.add(new BasicHeader("X-SSL-clientcert-issuer", DnUtils.normalizeDN(certs[0].getIssuerX500Principal().getName())));
-            }
-            
-            // @formatter:off
-            client = HttpClients.custom()
-                    .setSSLContext(ctx)
-                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                    .setDefaultHeaders(defaultHeaders)
-                    .setMaxConnTotal(maxConnections)
-                    .setRetryHandler(new DatawaveRetryHandler(retryCount, unavailableRetryCount, unavailableRetryDelay, retryCounter))
-                    .setServiceUnavailableRetryStrategy(new DatawaveUnavailableRetryStrategy(unavailableRetryCount, unavailableRetryDelay, retryCounter))
-                    .build();
-            // @formatter:on
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-            log.error("Unable to instantiate SSL context: " + e);
-            throw new IllegalStateException(e);
-        } catch (KeyStoreException e) {
-            log.error("Unable to retrieve aliases from KeyStore.");
-            throw new IllegalStateException(e);
-        }
     }
     
-    @PreDestroy
-    protected void shutdown() {
-        try {
-            client.close();
-        } catch (IOException e) {
-            log.warn("Exception while shutting down HttpClient: " + e.getMessage(), e);
-        }
+    @Override
+    protected boolean useSrvDns() {
+        return useSrvDNS;
     }
     
-    private static class DatawaveRetryHandler extends DefaultHttpRequestRetryHandler {
-        private final int unavailableRetryCount;
-        private final int unavailableRetryDelay;
-        private final Counter retryCounter;
-        
-        public DatawaveRetryHandler(int retryCount, int unavailableRetryCount, int unavailableRetryDelay, Counter retryCounter) {
-            super(retryCount, false, Arrays.asList(UnknownHostException.class, SSLException.class));
-            this.unavailableRetryCount = unavailableRetryCount;
-            this.unavailableRetryDelay = unavailableRetryDelay;
-            this.retryCounter = retryCounter;
-        }
-        
-        @Override
-        public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
-            boolean shouldRetry = super.retryRequest(exception, executionCount, context);
-            if (exception instanceof ConnectException) {
-                shouldRetry = (executionCount <= unavailableRetryCount);
-                if (shouldRetry) {
-                    try {
-                        Thread.sleep(unavailableRetryDelay);
-                    } catch (InterruptedException e) {
-                        // Ignore -- we'll just end up retrying a little too fast
-                    }
-                }
-            }
-            if (shouldRetry) {
-                retryCounter.inc();
-            }
-            return shouldRetry;
-        }
+    @Override
+    protected List<String> srvDnsServers() {
+        return srvDnsServers;
     }
     
-    private static class DatawaveUnavailableRetryStrategy extends DefaultServiceUnavailableRetryStrategy {
-        private final int maxRetries;
-        private final Counter retryCounter;
-        
-        private DatawaveUnavailableRetryStrategy(int maxRetries, int retryInterval, Counter retryCounter) {
-            super(maxRetries, retryInterval);
-            this.maxRetries = maxRetries;
-            this.retryCounter = retryCounter;
-        }
-        
-        @Override
-        public boolean retryRequest(HttpResponse response, int executionCount, HttpContext context) {
-            // Note that a 404 can happen during service startup, so we want to retry.
-            boolean shouldRetry = executionCount <= maxRetries
-                            && (response.getStatusLine().getStatusCode() == HttpStatus.SC_SERVICE_UNAVAILABLE || response.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND);
-            if (shouldRetry) {
-                retryCounter.inc();
-            }
-            return shouldRetry;
-        }
+    @Override
+    protected int srvDnsPort() {
+        return srvDnsPort;
     }
     
-    private interface IOFunction<T> {
-        T apply(HttpEntity entity) throws IOException;
+    @Override
+    protected String serviceScheme() {
+        return authServiceScheme;
+    }
+    
+    @Override
+    protected int maxConnections() {
+        return maxConnections;
+    }
+    
+    @Override
+    protected int retryCount() {
+        return retryCount;
+    }
+    
+    @Override
+    protected int unavailableRetryCount() {
+        return unavailableRetryCount;
+    }
+    
+    @Override
+    protected int unavailableRetryDelay() {
+        return unavailableRetryDelay;
+    }
+    
+    @Override
+    protected Counter retryCounter() {
+        return retryCounter;
     }
 }
