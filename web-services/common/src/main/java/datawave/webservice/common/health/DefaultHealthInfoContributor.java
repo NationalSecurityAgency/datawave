2d7905e493e759681955ed9f045d9f81e90e7f40
Brian Loss <brianloss@gmail.com>
2018-07-25 13:20:56 -0400

    re #147 load resource from getClass()'s class path

--- web-services/common/src/main/java/datawave/webservice/common/health/DefaultHealthInfoContributor.java
+++ web-services/common/src/main/java/datawave/webservice/common/health/DefaultHealthInfoContributor.java
@@ -4,6 +4,7 @@ import datawave.webservice.common.health.HealthBean.BuildInfo;
 import datawave.webservice.common.health.HealthBean.CommitInfo;
 import datawave.webservice.common.health.HealthBean.GitInfo;
 import datawave.webservice.common.health.HealthBean.VersionInfo;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.io.IOException;
@@ -15,7 +16,8 @@ public class DefaultHealthInfoContributor implements HealthInfoContributor {
         String buildTime = null;
         GitInfo gitInfo = null;
         try {
-            Properties gitProps = PropertiesLoaderUtils.loadAllProperties("git.properties");
+            ClassPathResource cpr = new ClassPathResource("/git.properties", getClass());
+            Properties gitProps = PropertiesLoaderUtils.loadProperties(cpr);
             CommitInfo ci = new CommitInfo(gitProps.getProperty("git.commit.time"), gitProps.getProperty("git.commit.id.abbrev"));
             gitInfo = new GitInfo(ci, gitProps.getProperty("git.branch"));
             buildTime = gitProps.getProperty("git.build.time");
