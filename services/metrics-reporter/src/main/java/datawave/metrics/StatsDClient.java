NationalSecurityAgency/datawave

commit 56e1fd8a18c45d87a0dcb0a36bb088a792bef645

Java8 grooming  Changes to be committed: 	modified:   services/audit-service/src/test/java/datawave/microservice/audit/log/LogAuditorTest.java 	modified:   services/metrics-reporter/src/main/java/datawave/metrics/StatsDClient.java 	modified:   services/spring-boot-starter-datawave/src/main/java/datawave/microservice/authorization/jwt/JWTRestTemplate.java 	modified:   warehouse/balancers/src/test/java/datawave/ingest/table/balancer/ShardedTableTabletBalancerTest.java 	modified:   warehouse/common/src/test/java/datawave/common/util/concurrent/BoundedBlockingQueueTest.java 	modified:   warehouse/core/src/main/java/datawave/edge/util/EdgeKey.java 	modified:   warehouse/core/src/main/java/datawave/iterators/filter/ConfigurableAgeOffFilter.java 	modified:   warehouse/core/src/main/java/datawave/iterators/filter/TokenSpecParser.java 	modified:   warehouse/core/src/main/java/datawave/iterators/filter/TokenTtlTrie.java 	modified:   warehouse/core/src/main/java/datawave/marking/ColumnVisibilityHelper.java 	modified:   warehouse/core/src/main/java/datawave/marking/FlattenedVisibilityCache.java 	modified:   warehouse/core/src/main/java/datawave/marking/MarkingFunctions.java 	modified:   warehouse/core/src/main/java/datawave/mr/bulk/BulkInputFormat.java 	modified:   warehouse/core/src/main/java/datawave/query/parser/JavaRegexAnalyzer.java 	modified:   warehouse/core/src/main/java/datawave/query/util/StringTuple.java 	modified:   warehouse/core/src/main/java/datawave/util/ObjectFactory.java 	modified:   warehouse/core/src/main/java/datawave/util/StringMultimapSchema.java 	modified:   warehouse/core/src/test/java/datawave/edge/util/EdgeValueTest.java 	modified:   warehouse/data-dictionary-core/src/test/java/datawave/webservice/datadictionary/DatawaveDataDictionaryImplTest.java 	modified:   warehouse/ingest-core/src/main/java/datawave/ingest/config/RawRecordContainerImpl.java 	modified:   warehouse/ingest-core/src/main/java/datawave/ingest/data/config/CSVHelper.java 	modified:   wa

services/metrics-reporter/src/main/java/datawave/metrics/StatsDClient.java
@@ -23,7 +23,7 @@ class StatsDClient {
     private final String prefix;
     private final NonBlockingUdpSender sender;
     
-    private ThreadLocal<NumberFormat> numberFormat = new ThreadLocal<NumberFormat>() {
+    private ThreadLocal<NumberFormat> numberFormat = new ThreadLocal<>() {
         @Override
         protected NumberFormat initialValue() {
             NumberFormat formatter = NumberFormat.getInstance(Locale.US);

commit 5538cbf9665630d5deedac5c1cd4f1432272a4ca

Fix compile error introduced by 56e1fd8a18c45d87a0dcb0a36bb088a792bef645
Error was in StatsDClient.java: cannot use <> with anonymous inner classes.

services/metrics-reporter/src/main/java/datawave/metrics/StatsDClient.java
@@ -15,23 +15,17 @@ class StatsDClient {
     
     private static final Charset STATS_D_ENCODING = Charset.forName("UTF-8");
     
-    private static final StatsDClientErrorHandler NO_OP_HANDLER = new StatsDClientErrorHandler() {
-        @Override
-        public void handle(Exception e) { /* No-op */}
-    };
+    private static final StatsDClientErrorHandler NO_OP_HANDLER = e -> { /* No-op */};
     
     private final String prefix;
     private final NonBlockingUdpSender sender;
     
-    private ThreadLocal<NumberFormat> numberFormat = new ThreadLocal<>() {
-        @Override
-        protected NumberFormat initialValue() {
-            NumberFormat formatter = NumberFormat.getInstance(Locale.US);
-            formatter.setGroupingUsed(false);
-            formatter.setMaximumFractionDigits(19);
-            return formatter;
-        }
-    };
+    private ThreadLocal<NumberFormat> numberFormat = ThreadLocal.withInitial(() -> {
+        NumberFormat formatter = NumberFormat.getInstance(Locale.US);
+        formatter.setGroupingUsed(false);
+        formatter.setMaximumFractionDigits(19);
+        return formatter;
+    });
     
     public StatsDClient(String prefix, String hostname, int port) throws StatsDClientException {
         this(prefix, hostname, port, NO_OP_HANDLER);