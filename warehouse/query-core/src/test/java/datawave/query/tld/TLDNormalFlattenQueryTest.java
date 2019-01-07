d34265191542b4c3415f6f46164e0913b5a1f3d3
Ivan Bella <ivan@bella.name>
2019-01-07 22:27:50 +0000

    re #287: Removed unnessary routine override

--- warehouse/query-core/src/test/java/datawave/query/tld/TLDNormalFlattenQueryTest.java
+++ warehouse/query-core/src/test/java/datawave/query/tld/TLDNormalFlattenQueryTest.java
@@ -2,17 +2,9 @@ package datawave.query.tld;
 
 import datawave.ingest.json.util.JsonObjectFlattener.FlattenMode;
 import datawave.query.NormalFlattenQueryTest;
-import datawave.query.RebuildingScannerTestHelper;
 import datawave.query.tables.ShardQueryLogic;
 import datawave.query.tables.TLDQueryLogic;
-import datawave.query.testframework.AccumuloSetupHelper;
-import datawave.query.testframework.DataTypeHadoopConfig;
-import datawave.query.testframework.FileLoaderFactory;
 import org.apache.log4j.Logger;
-import org.junit.BeforeClass;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 /**
  * Test cases for flatten mode {@link FlattenMode@NORMAL}.
@@ -26,14 +18,4 @@ public class TLDNormalFlattenQueryTest extends NormalFlattenQueryTest {
     }
     
     public TLDNormalFlattenQueryTest() {}
-    
-    @BeforeClass
-    public static void filterSetup() throws Exception {
-        Collection<DataTypeHadoopConfig> dataTypes = new ArrayList<>();
-        dataTypes.add(flatten);
-        
-        final AccumuloSetupHelper helper = new AccumuloSetupHelper(dataTypes, FileLoaderFactory.FileType.JSON);
-        connector = helper.loadTables(log, RebuildingScannerTestHelper.TEARDOWN.EVERY_OTHER);
-    }
-    
 }
