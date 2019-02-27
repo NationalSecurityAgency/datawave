f145afbafc58fd83ff6025ea1a27fee389d0a150
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2019-02-27 12:59:14 +0000

    Changed TableNames class to be an interface.

--- warehouse/core/src/main/java/datawave/util/TableNames.java
+++ warehouse/core/src/main/java/datawave/util/TableNames.java
@@ -3,19 +3,19 @@ package datawave.util;
 /**
  * Class that contains default table names
  */
-public class TableNames {
+public interface TableNames {
     
-    public static final String DATE_INDEX_TABLE_NAME = "dateIndex";
+    String DATE_INDEX_TABLE_NAME = "dateIndex";
     
-    public static final String ERROR_SHARD_TABLE_NAME = "errorShard";
+    String ERROR_SHARD_TABLE_NAME = "errorShard";
     
-    public static final String INDEX_STATS_TABLE_NAME = "shardIndexStats";
+    String INDEX_STATS_TABLE_NAME = "shardIndexStats";
     
-    public static final String KNOWLEDGE_TABLE_NAME = "knowledgeShard";
+    String KNOWLEDGE_TABLE_NAME = "knowledgeShard";
     
-    public static final String METADATA_TABLE_NAME = "DatawaveMetadata";
+    String METADATA_TABLE_NAME = "DatawaveMetadata";
     
-    public static final String SHARD_TABLE_NAME = "shard";
-    public static final String SHARD_INDEX_TABLE_NAME = "shardIndex";
-    public static final String SHARD_RINDEX_TABLE_NAME = "shardReverseIndex";
+    String SHARD_TABLE_NAME = "shard";
+    String SHARD_INDEX_TABLE_NAME = "shardIndex";
+    String SHARD_RINDEX_TABLE_NAME = "shardReverseIndex";
 }
