5acf570d4314c593788f3b1f11cc1a3718fe02da
keith-ratcliffe <ratcliffe.keith@gmail.com>
2019-03-11 13:31:36 -0400

    re #176 - increased timeout for ShardedTableMapFileTest junit; re PR#353 - code autoformat

--- warehouse/ingest-core/src/test/java/datawave/ingest/mapreduce/job/ShardedTableMapFileTest.java
+++ warehouse/ingest-core/src/test/java/datawave/ingest/mapreduce/job/ShardedTableMapFileTest.java
@@ -81,7 +81,7 @@ public class ShardedTableMapFileTest {
         Assert.assertEquals(1, result.size());
     }
     
-    @Test(timeout = 30000)
+    @Test(timeout = 240000)
     public void testWriteSplitsToAccumuloAndReadThem() throws Exception {
         
         // Added timeout to this test b/c it could hang infinitely without failing, e.g., whenever
