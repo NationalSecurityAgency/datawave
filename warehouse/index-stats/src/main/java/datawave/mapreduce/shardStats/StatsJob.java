2ca1cb570b8e577a1353ed9b5535563c59349501
Bob Whitehurst <bmwmaestoso@gmail.com>
2019-05-09 17:45:04 +0000

    generation of shard statistics (#415)
    * Created a StatsJob which will sample data and generate the selectivity of terms.  These stats will eventually be incorporated into the query planning.

---
+++ warehouse/index-stats/src/main/java/datawave/mapreduce/shardStats/StatsJob.java
@@ -0,0 +1,287 @@
+package datawave.mapreduce.shardStats;
+
+import datawave.ingest.data.config.ingest.AccumuloHelper;
+import datawave.ingest.mapreduce.handler.shard.ShardStatsDataTypeHandler;
+import datawave.ingest.mapreduce.handler.shard.ShardedDataTypeHandler;
+import datawave.ingest.mapreduce.job.IngestJob;
+import datawave.ingest.mapreduce.job.MultiRFileOutputFormatter;
+import datawave.mr.bulk.BulkInputFormat;
+import datawave.mr.bulk.MultiRfileInputformat;
+import datawave.query.Constants;
+import datawave.util.StringUtils;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.IteratorSetting;
+import org.apache.accumulo.core.client.admin.SecurityOperations;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Range;
+import org.apache.accumulo.core.iterators.user.VersioningIterator;
+import org.apache.accumulo.core.security.Authorizations;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Level;
+
+import java.net.URISyntaxException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Map/Reduce job for determining the cardinality and selectivity for each distinct field name/dataype pair that exists in the shard table. Due to the large
+ * amount of data required to determine an exact cardinality and selectivity, the {@link com.clearspring.analytics.stream.cardinality.HyperLogLogPlus} class is
+ * used to provide an estimate of the cardinality and selectivity. For additional information, read the following papers:
+ * <ul>
+ * <li>"HyperLogLo: the analysis of a near-optimal cardinality estimation algorithm"</li>
+ * <li>"HyperLogLog in Practive: Algorithmic Engineering of a State of The Art Cardinality Estimation Algorithm" written by Stefan Huele, Marc NunKesser, and
+ * Alexander Hall.</li>
+ * </ul>
+ * <p>
+ * HyperLogLogPlus Notes
+ * <p>
+ * There are two parameters that are used to control how the hyper log class functions:
+ * <ul>
+ * <li>a sparse precision is used when the data set is small</li>
+ * <li>a normal precision is used when the dataset exceeds a threshold</li>
+ * </ul>
+ * Valid values for the sparse and normal precision are from 4 to 32, and the normal precision cannot b greater than the sparse precision. setting the sparse
+ * precision to 0 will only use the normal precision for all calculations. Setting the sparse precision to 0 has not been tested.
+ * <p>
+ * Higher precision values translate to more buckets and thus higher memory utilization. The number of buckets that are allocated is defined by the equation:
+ * 
+ * <pre>
+ *         buckets = 2 ** N   where N is the precision
+ * </pre>
+ * 
+ * As the precision is increased, the error percentage decreases and can be modeled by the equation:
+ * 
+ * <pre>
+ *         error percentage = 1.04 / sqrt(M)   where M is the number of buckets
+ * </pre>
+ * <p>
+ * Precision values should be selected based upon the desired error percentage and memory utilization. Based upon the information in the referenced papers,
+ * using the highest precision (32) will consume about 1.5K of memory.
+ * </p>
+ * <p>
+ * During development an anomaly was discovered when the sparse and normal precision values differed by more than 6. In random instances, when the cardinality
+ * for a large dataset was also large, the estimated cardinality produced by the hyper log class differed from actual cardinality up to 25%. The larger the
+ * difference between the sparse and normal precision values, the larger the error percentage. When specifying the sparse and normal precision values, it is
+ * best to choose values that are the same. All testing using the same sparse and normal precision resulted in consistent results that were within the expected
+ * error margin.
+ * </p>
+ */
+public class StatsJob extends IngestJob {
+    
+    // default log level for all classes
+    static final Level DEFAULT_LOG_LEVEL = Level.INFO;
+    
+    // default values used by both mapper and reducer
+    // constants for hyperloglogplus
+    static final int HYPERLOG_SPARSE_DEFAULT_VALUE = 24;
+    static final int HYPERLOG_NORMAL_DEFAULT_VALUE = 24;
+    static final String HYPERLOG_NORMAL_OPTION = "shardStats.hyperlog.normal";
+    static final String HYPERLOG_SPARSE_OPTION = "shardStats.hyperlog.sparse";
+    
+    static final String OUTPUT_TABLE_NAME = "shardStats.table.name";
+    static final String INPUT_TABLE_NAME = "shardStats.input.table";
+    static final String STATS_JOB_LOG_LEVEL = "stats.job.log.level";
+    static final String STATS_VISIBILITY = "shardStats.visibility";
+    
+    // instance members
+    private String inputTableName;
+    private String outputTableName;
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Running main");
+        System.exit(ToolRunner.run(new Configuration(), new StatsJob(), args));
+    }
+    
+    @Override
+    protected Configuration parseArguments(String[] args, Configuration conf) throws ClassNotFoundException, URISyntaxException, IllegalArgumentException {
+        Configuration parseConf = super.parseArguments(args, conf);
+        
+        // force bulk job
+        this.outputMutations = false;
+        this.useMapOnly = false;
+        
+        if (null != parseConf) {
+            parseStatsOptions(args, parseConf);
+            
+            parseConf.setStrings(MultiRFileOutputFormatter.CONFIGURED_TABLE_NAMES, this.outputTableName);
+            
+            this.mapper = StatsHyperLogMapper.class;
+            this.inputFormat = MultiRfileInputformat.class;
+        }
+        
+        return parseConf;
+    }
+    
+    @Override
+    protected void configureInputFormat(Job job, AccumuloHelper cbHelper, Configuration conf) throws Exception {
+        BulkInputFormat.setZooKeeperInstance(conf, cbHelper.getInstanceName(), cbHelper.getZooKeepers());
+        
+        // add the versioning iterator
+        IteratorSetting cfg = new IteratorSetting(100, VersioningIterator.class);
+        BulkInputFormat.addIterator(conf, cfg);
+        
+        // get authorizations
+        Connector conn = cbHelper.getConnector();
+        SecurityOperations secOps = conn.securityOperations();
+        Authorizations auths = secOps.getUserAuthorizations(cbHelper.getUsername());
+        
+        BulkInputFormat.setInputInfo(job, cbHelper.getUsername(), cbHelper.getPassword(), this.inputTableName, auths);
+        final Set<Range> scanShards = calculateRanges(conf);
+        BulkInputFormat.setRanges(job, scanShards);
+        
+        super.configureInputFormat(job, cbHelper, conf);
+    }
+    
+    @Override
+    protected void configureJob(Job job, Configuration conf, Path workDirPath, FileSystem outputFs) throws Exception {
+        super.configureJob(job, conf, workDirPath, outputFs);
+        
+        job.setReducerClass(StatsHyperLogReducer.class);
+    }
+    
+    /**
+     * Processes the options for the Stats job.
+     * 
+     * @param inArgs
+     *            input arguments to stats job
+     * @param conf
+     *            hadoop configuration
+     */
+    private void parseStatsOptions(final String[] inArgs, final Configuration conf) {
+        for (int n = 0; n < inArgs.length; n++) {
+            String[] args = inArgs[n].split("=");
+            JobArg arg = JobArg.getOption(args[0]);
+            if (null != arg) {
+                switch (arg) {
+                // job args
+                    case JOB_LOG_LEVEL:
+                        Level level = Level.toLevel(args[1], DEFAULT_LOG_LEVEL);
+                        log.setLevel(level);
+                        log.info("log level set to " + level.toString());
+                        break;
+                    default:
+                        conf.set(arg.key, args[1]);
+                        break;
+                }
+            }
+        }
+        
+        // validate required entries
+        String vis = conf.get(STATS_VISIBILITY);
+        if (null == vis) {
+            throw new IllegalStateException("column visibility property (" + STATS_VISIBILITY + ") is not set");
+        }
+        log.info("column visibility (" + vis + ")");
+        
+        this.inputTableName = conf.get(INPUT_TABLE_NAME);
+        if (null == this.inputTableName) {
+            throw new IllegalStateException("input table property (" + INPUT_TABLE_NAME + ") is not set");
+        }
+        log.info("input table(" + this.inputTableName + ")");
+        
+        this.outputTableName = conf.get(OUTPUT_TABLE_NAME);
+        if (null == this.outputTableName) {
+            throw new IllegalStateException("output table property (" + OUTPUT_TABLE_NAME + ") is not set");
+        }
+        log.info("output table(" + this.outputTableName + ")");
+    }
+    
+    private Set<Range> calculateRanges(Configuration conf) {
+        final Set<Range> ranges = new HashSet<>();
+        String[] shardsAndDays = StringUtils.split(this.inputPaths, ',');
+        this.inputPaths = "";
+        
+        // for each datatype of interest
+        log.info("using the following ranges");
+        for (String shard : shardsAndDays) {
+            // if shard is actualy a day, split into shards
+            if (shard.indexOf('_') < 0) {
+                // shard should be a day
+                int numShards = conf.getInt(ShardedDataTypeHandler.NUM_SHARDS, -1);
+                if (numShards < 0) {
+                    throw new IllegalArgumentException("Cannot determine the number of shards from the configuration.get(" + ShardedDataTypeHandler.NUM_SHARDS
+                                    + ")");
+                }
+                
+                // create a range for each shard
+                for (int n = 0; n < numShards; n++) {
+                    String shardNum = shard + '_' + n;
+                    Key firstKey = new Key(shardNum, "fi" + '\0');
+                    Key endKey = new Key(shardNum, "fi" + '\0' + Constants.MAX_UNICODE_STRING);
+                    
+                    Range r = new Range(firstKey, true, endKey, false);
+                    ranges.add(r);
+                    log.info(r.toString());
+                }
+            } else {
+                Key firstKey = new Key(shard, "fi" + '\0');
+                Key endKey = new Key(shard, "fi" + '\0' + Constants.MAX_UNICODE_STRING);
+                
+                Range r = new Range(firstKey, true, endKey, false);
+                ranges.add(r);
+                log.info(r.toString());
+            }
+        }
+        
+        return ranges;
+    }
+    
+    /**
+     * Helper enum to manage options with default values.
+     */
+    private enum JobArg {
+        // stats job options
+        INPUT_TABLE(INPUT_TABLE_NAME, ""),
+        OUTPUT_TABLE(OUTPUT_TABLE_NAME, ""),
+        COLUMN_VISIBILITY(STATS_VISIBILITY, ""),
+        JOB_LOG_LEVEL(STATS_JOB_LOG_LEVEL, DEFAULT_LOG_LEVEL),
+        HYPERLOG_NORMAL_PRECISION(HYPERLOG_NORMAL_OPTION, HYPERLOG_NORMAL_DEFAULT_VALUE),
+        HYPERLOG_SPARSE_PRECISION(HYPERLOG_SPARSE_OPTION, HYPERLOG_SPARSE_DEFAULT_VALUE),
+        
+        // mapper specific options
+        MAPPER_INPUT_INTERVAL(StatsHyperLogMapper.STATS_MAPPER_INPUT_INTERVAL, StatsHyperLogMapper.DEFAULT_INPUT_INTERVAL),
+        MAPPER_OUTPUT_INTERVAL(StatsHyperLogMapper.STATS_MAPPER_OUTPUT_INTERVAL, StatsHyperLogMapper.DEFAULT_OUTPUT_INTERVAL),
+        MAPPER_LOG_LEVEL(StatsHyperLogMapper.STATS_MAPPER_LOG_LEVEL, DEFAULT_LOG_LEVEL),
+        
+        // reducer specific options
+        MIN_COUNT(StatsHyperLogReducer.STATS_MIN_COUNT, StatsHyperLogReducer.DEFAULT_MIN_COUNT),
+        REDUCER_COUNTS(StatsHyperLogReducer.STATS_REDUCER_COUNTS, false),
+        REDUCER_VALUE_INTERVAL(StatsHyperLogReducer.STATS_REDUCER_VALUE_INTERVAL, StatsHyperLogReducer.DEFAULT_VALUE_INTERVAL),
+        REDUCER_LOG_LEVEL(StatsHyperLogReducer.STATS_REDUCER_LOG_LEVEL, DEFAULT_LOG_LEVEL), ;
+        
+        static JobArg getOption(String option) {
+            while (option.startsWith("-")) {
+                option = option.substring(1);
+            }
+            for (JobArg opt : JobArg.values()) {
+                if (opt.key.equals(option)) {
+                    return opt;
+                }
+            }
+            
+            return null;
+        }
+        
+        private String key;
+        private Object defaultValue;
+        
+        JobArg(String kVal, Object dVal) {
+            this.key = kVal;
+            this.defaultValue = dVal;
+        }
+        
+        @Override
+        public String toString() {
+            // @formatter:off
+            return "JobArg{" +
+                    "key='" + key + '\'' +
+                    ", defaultValue=" + defaultValue +
+                    '}';
+            // @formatter:on
+        }
+    }
+}
---
+++ warehouse/ingest-scripts/src/main/resources/bin/util/buildIndexStats.sh
@@ -0,0 +1,364 @@
+#!/bin/bash
+
+#========================================================================================
+# Purpose
+# Creates index statistics for a single date from the data in the shard table. The input
+# argument should be a single day, however, for testing and development purposes the
+# number of shards can be limited.
+#
+# USAGE:
+#    SCRIPT [date] [number of shards]
+#    SCRIPT CSV list of shards
+# examples
+#    SCRIPT  => generate data for shards for the current day
+#    SCRIPT 20180909 => generate data for shards 20180909_0 ... _n (where n = $NUM_SHARDS)
+#    SCRIPT 20180909 3 => generate data for shards 20180909_0, ... _2 (shards 0 1 2)
+#    SCRIPT 20180909_1 => generate data for shard 20180909_1
+#    SCRIPT 20180909_1,20180909_3 => generate data for shard 20180909_1 and 20180909_3
+#
+#++++++++++++++++++++++++++++++++++++++++++++++++
+# HyperLogLogPlus
+# The HyperlogLogPlus algorithm is utilized to provide an estimate of the cardinality of
+# a dataset. There are two parameters that affect the memory and precision of the
+# algorithm, a sparse and normal precision. These parameters determine how many buckets
+# are allocated to represent the data, which corresponds to the amount of memory
+# utilization. Valid values are between 4 and 32 inclusive and the normal precision
+# cannot be greater than the sparse precision. The sparse representation is used for
+# smaller data sets and is automatically converted to the normal precision when the
+# data set exceeds a threshold based upon the sparse precision. The sparse representation
+# can be overriden by setting the value to 0. This will result in the use of the normal
+# representation only. Setting the sparse precision to 0 has not been tested.
+#
+# Higher precision values translate to more buckets and thus higher memory utilization.
+# The number of buckets that are allocated is modeled by  the equation:
+#
+#        buckets = 2 ** n  => where n is the precision
+#
+# As the precision is increased, the error percentage decreases and is modeled by
+# the equation:
+#
+#        error percentage = 1.0 / SQRT(m)   => where m is the number of buckets
+#
+# Precision values should be set based upon memory usage versus error percentage. Several
+# tests indicate that inconsistent results can occur when the sparse and normal precision
+# values differ. The greater the difference, the larger the error percentage. While most
+# of the generated cardinalities fell within the projected error percentage, in the few
+# instances where it did it was off by a very significant factor. Thus when selecting
+# sparse and normal precision values it is best to choose the same value for both.
+#
+#+++++++++++++++++++++++++++++++++++++++
+# Optional Environmental Variables
+# _MapperMemoryMB => memory allocated in MB to each mapper JVM process
+# _ReducerMemoryMB => memory allocated in MB to each reducer JVM process
+#
+# Optional Debug Environment Variables
+# There are several options that will allow for analysis of the debug data:
+#   _UniqueCounts => Collects actual unique counts in addition to the HyperLogLogPlus
+#           generated cardinality for analysis purposes. The mapper will generate the
+#           the unique count, while the reducer is the best location to view all of the
+#           entries from multiple mappers. The test script hlParse.sh is provided to
+#           parse the log file of the reducer. This process extracts the analysis data
+#           into a csv format, which can be loaded into a spreadsheet for analysis.
+#           NOTE: The _MapperMemoryMB may need to be set to avoid an OOM condition.
+#   _GCMapperLog => Enables GC logging for the mapper.
+#   _GCReducerLog => Enables GC logging for the reducer.
+#   _GCLog => Enables GC logging for both the mapper and reducer.
+#   _GCLogDir => Specifies the directory for the GC logs (overrides the default value).
+#   _HeapDump => Enable heap dump for mapper and reducer.
+#========================================================================================
+
+if [[ `uname` == "Darwin" ]]; then
+        THIS_SCRIPT=`python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $0`
+else
+        THIS_SCRIPT=`readlink -f $0`
+fi
+
+THIS_DIR="${THIS_SCRIPT%/*}"
+cd $THIS_DIR
+
+. ../ingest/ingest-env.sh
+
+declare -r _base=$(basename $0)
+declare -r _lockFile=${LOCK_FILE_DIR}/${_base}
+
+
+#===================================================================================
+# local functions
+function cleanup() {
+    declare -r _status=$1
+
+    rm -f "${_lockFile}"
+    trap 0 1 2 3 15
+    exit ${_status}
+}
+
+function setupLockFile() {
+    # check for current execution of this script
+    test -e ${_lockFile} && {
+        echo "ERROR: lock file exists => ${_lockFile}"
+        exit 1
+    }
+    touch "${_lockFile}"
+    trap "cleanup 1" 1 2 3 15
+    trap "cleanup 0" 0
+}
+
+#==================================
+# Sets JVM parameters for Mapper
+function setMapperJVMParameters() {
+    _MapperMemoryMB=${_MapperMemoryMB:-1024}
+    # memory setting may need to be adjusted based upon input data
+    # when _CountsOnly debug variable is set; check max container size
+
+    # allocate 1/3 memory to mapper io sort buffer but must be less than 2048
+    ((_MapInMemSortBufferMBSize = _MapperMemoryMB / 3))
+    if [[ 1536 -le ${_MapInMemSortBufferMBSize} ]]; then
+        _MapInMemSortBufferMBSize=1536
+    fi
+
+    # enable GC logging if requested - typically for debug purposes
+    # NOTE: there should only be one mapper; same file is used for all mappers
+    if [[ -n "${_GCLog}" || -n "${_GCMapperLog}" ]]; then
+        local -r _logFile="-Xloggc:${_GCLogDir}/gcMap-$$-${_dateStr}.log"
+        local -r _gcLog="${_GCLogOpts} ${_logFile}"
+    fi
+
+    _ChildMapOpts="\
+-Xmx${_MapperMemoryMB}m \
+${_DefaultJVMArgs} \
+${CHILD_INGEST_OPTS} \
+${_HeapDumpOpts} \
+${_gcLog}"
+}
+
+function setReducerJVMParameters() {
+    _ReducerMemoryMB=${_ReducerMemoryMB:-1024}
+
+    # enable GC logging if requested - typically for debug purposes
+    if [[ -n "${_GCLog}" || -n "${_GCMapperLog}" ]]; then
+        local -r _logFile="-Xloggc:${_GCLogDir}/gcReduce-$$-${_dateStr}.log"
+        local -r _gcLog="${_GCLogOpts} ${_logFile}"
+    fi
+
+    _ChildReduceOpts="\
+-Xmx${_ReducerMemoryMB}m \
+${_DefaultJVMArgs} \
+${CHILD_INGEST_OPTS} \
+${_HeapDumpOpts} \
+${_gcLog}"
+}
+
+#==================================
+# Sets required and optional parameters for the map/reduce job.
+function setStatsParameters() {
+    # default values for optional debug parameters
+    # see class files for description
+    #  =======  setting log level for mapper/redcuer/job
+    #  -stats.job.log.level=info
+    #  -stats.mapper.log.level=info
+    #  -stats.reducer.log.level=info
+    #  ======  settings for heartbeat intervals
+    #  -stats.mapper.input.interval=10000000
+    #  -stats.mapper.value.interval=100
+    #  -stats.mapper.uniquecount=false
+    #  -stats.reducer.value.interval=20
+    #  -stats.reducer.counts=false
+    local _StatsDebugOpts=""
+
+    # check for unique counts
+    test -n "${_UniqueCounts}" && {
+        _StatsDebugOpts="${_StatsDebugOpts} -stats.mapper.uniquecount=true -stats.reducer.counts=true"
+    }
+
+    # runtime parameters are set in shard-stats-config.xml
+
+    test -n "${_NumShards}" && {
+        local -r _shardOpts="-num.shards=${_NumShards}"
+    }
+
+    # input and output table names
+    _StatsOpts="
+${_StatsDebugOpts} \
+${_shardOpts} \
+"
+}
+
+#==================================
+# Determines the number of reducers to run based upon the number of shards.
+function calculateReducers() {
+    local -r _reducers=$1
+
+    # divisor for number of reducers based upon the number of shards
+    local -ir _divisor=6
+    local -i _num
+    if [[ -n "${_NumShards}" ]]; then
+        ((_num = _NumShards / _divisor))
+    else
+        IFS=","
+        set -- ${_argDate}
+        unset IFS
+        if [[ $# -gt 1 ]]; then
+            ((_num = _cnt / _divisor))
+        else
+            ((_num = NUM_SHARDS / _divisor))
+        fi
+    fi
+
+    test "${_num}" -eq 0 && _num=1
+
+    eval ${_reducers}=${_num}
+}
+
+# end local functions
+#===================================================================================
+
+setupLockFile
+
+
+#
+# set the job cache
+#
+. ../ingest/job-cache-env.sh
+
+#
+# set the classpath
+#
+. ../ingest/ingest-libs.sh
+
+if [[ -z "$DATAWAVE_INGEST_HOME" ]]; then
+    export DATAWAVE_INGEST_HOME=$THIS_DIR/../..
+fi
+
+#
+# set the ingest config files
+#
+declare -a _IngestConfig
+i=0
+for f in ../../config/shard-stats-config.xml; do
+    _IngestConfig[i++]=$(basename $f)
+done
+
+#
+# transform classpath into a comma separated list
+LIBJARS=${CLASSPATH//:/,}
+
+
+#==========================================
+# ingest parameters
+declare -r _today=$(date "+%Y%m%d%H%M%S")
+declare _argDate=$(date -d "now" "+%Y%m%d")
+declare -r _workDir=${WAREHOUSE_NAME_BASE_DIR}${BASE_WORK_DIR}/${_today}-$$/
+if (($# >= 1)); then
+    _argDate=$1
+    if [[ -n "$2" ]]; then
+        declare _NumShards=$2
+    fi
+fi
+
+#================================================
+# initialize map/reduce JVM settings
+test -n "${_HeapDump}" && declare -r _heap="-XX:-HeapDumpOnOutOfMemoryError"
+declare -r _DefaultJVMArgs="-XX:+UseConcMarkSweepGC \
+-Dfile.encoding=UTF8 \
+-Duser.timezone=GMT \
+-XX:+UseNUMA \
+${_heap} \
+-XX:NewRatio=2"
+declare -r _dateStr=$(date "+%Y_%m_%d-%H_%M_%S")
+declare -r _GCLogOpts="-XX:+PrintGC \
+-XX:+PrintGCTimeStamps \
+-XX:+PrintGCDateStamps \
+-XX:+PrintGCDetails"
+# set mapper/reducer log directory
+_GCLogDir="${_GCLogDir:-/tmp}"
+
+# set in memory sort io buffer size
+declare -i _MapInMemSortBufferMBSize
+declare _StatsOpts
+declare _ChildMapOpts
+declare _ChildReduceOpts
+setMapperJVMParameters
+setReducerJVMParameters
+setStatsParameters
+
+declare _NumReducers
+calculateReducers _NumReducers
+
+#============================================
+# Hadoop Job JVM Options
+export HADOOP_CLASSPATH=$CLASSPATH
+export HADOOP_OPTS="${_DefaultJVMArgs}"
+# for remote debug of job
+# export HADOOP_CLIENT_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8989"
+
+declare -i TIMEOUT=600000
+
+# map/reduce settings see Hadoop Map/Reduce properties
+_MapReduceOpts="-mapreduce.task.io.sort.mb=${_MapInMemSortBufferMBSize} \
+-mapreduce.task.io.sort.factor=${BULK_CHILD_IO_SORT_MB} \
+-compressionType gz \
+-mapreduce.map.output.compression.codec=${BULK_MAP_OUTPUT_COMPRESSION_CODEC} \
+-mapreduce.map.output.compress=${BULK_MAP_OUTPUT_COMPRESS} \
+-mapreduce.output.fileoutputformat.compress=true \
+-mapreduce.output.fileoutputformat.compress.type=${BULK_MAP_OUTPUT_COMPRESSION_TYPE} \
+-bulk.ingest.mapper.threads=0 \
+-bulk.ingest.mapper.workqueue.size=10000 \
+-io.file.buffer.size=1048576 \
+-dfs.bytes-per-checksum=4096 \
+-io.sort.record.percent=.10 \
+-mapreduce.map.sort.spill.percent=.50 \
+-mapreduce.job.reduces=${_NumReducers} \
+-mapreduce.task.timeout=${TIMEOUT} \
+-markerFileReducePercentage 0.33 \
+-context.writer.max.cache.size=2500 \
+-mapreduce.job.queuename=bulkIngestQueue \
+-sequenceialLocationPartitioner \
+-dfs.replication=3 \
+"
+
+BATCHWRITER_OPTS="-AccumuloOutputFormat.WriteOpts.BatchWriterConfig=    11#maxMemory=100000000,maxWriteThreads=4"
+
+# define the location of the jar files for map/reduce job
+declare -r _jarCacheDir=${WAREHOUSE_NAME_BASE_DIR}${JOB_CACHE_DIR}
+
+declare -r _hdfsDir=${WAREHOUSE_NAME_BASE_DIR:-${WAREHOUSE_HDFS_NAME_NODE}}
+
+
+# define job ingest parmeters
+declare -r _IngestOpts="\
+-cacheBaseDir ${_jarCacheDir} \
+-cacheJars ${LIBJARS} \
+-user $USERNAME \
+-pass $PASSWORD \
+-instance ${WAREHOUSE_INSTANCE_NAME} \
+-zookeepers ${WAREHOUSE_ZOOKEEPERS} \
+-workDir ${_workDir} \
+-flagFileDir ${FLAG_DIR} \
+-flagFilePattern '.*_(indexstats)_*\.flag' \
+-srcHdfs ${_hdfsDir} \
+-destHdfs ${_hdfsDir} \
+-skipMarkerFileGeneration \
+-writeDirectlyToDest \
+-tableCounters \
+-contextWriterCounters \
+-disableRefreshSplits \
+-distCpConfDir ${WAREHOUSE_HADOOP_CONF} \
+-datawave-ingest.splits.cache.dir=${WAREHOUSE_NAME_BASE_DIR}/data/splitsCache \
+-BulkInputFormat.working.dir=${WAREHOUSE_NAME_BASE_DIR}/tmp/shardStats \
+-ingestMetricsDisabled \
+"
+# replace -disableRefreshSplits for local testing or run generate-splits-file.sh
+# -jobGeneratedSplits \
+
+
+set -x
+${WAREHOUSE_HADOOP_HOME}/bin/hadoop --config ${WAREHOUSE_HADOOP_CONF} \
+jar ${DATAWAVE_INDEX_STATS_JAR} datawave.mapreduce.shardStats.StatsJob \
+-jt ${WAREHOUSE_JOBTRACKER_NODE} ${_argDate} \
+${_IngestConfig[@]} \
+-mapreduce.map.java.opts="${_ChildMapOpts}" \
+-mapreduce.reduce.java.opts="${_ChildReduceOpts}" \
+${_IngestOpts} "${BATCHWRITER_OPTS}" ${_MapReduceOpts} ${_StatsOpts}
+declare _rc=$?
+set +x
+cleanup $_rc
