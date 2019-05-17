0ea72ef7c55a49d6b6fc830339dd65a01999cbc5
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2019-05-17 14:58:53 +0000

    Refactored QueryIteratorIT.

--- warehouse/query-core/src/test/java/datawave/query/tld/TLDQueryIteratorIT.java
+++ warehouse/query-core/src/test/java/datawave/query/tld/TLDQueryIteratorIT.java
@@ -27,7 +27,7 @@ public class TLDQueryIteratorIT extends QueryIteratorIT {
     }
     
     /**
-     * Document specific range given expected from RangeParitioner and EVENT_FIELD1 index lookup. ExceededValueThreshold TF FIELD triggers TF index lookup for
+     * Document specific range given expected from RangePartitioner and EVENT_FIELD1 index lookup. ExceededValueThreshold TF FIELD triggers TF index lookup for
      * evaluation.
      *
      * document specific range, tf via TermFrequencyAggregator
@@ -37,20 +37,19 @@ public class TLDQueryIteratorIT extends QueryIteratorIT {
     @Test
     public void tf_exceededValue_leadingWildcard_documentSpecific_tld_test() throws IOException {
         // build the seek range for a document specific pull
-        Range seekRange = getDocumentRange("row", "dataType1", "123.345.456");
-        
+        Range seekRange = getDocumentRange("123.345.456");
+        String query = "EVENT_FIELD1 =='a' && ((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ '.*r'))";
         Map.Entry<Key,Map<String,List<String>>> expectedDocument = getBaseExpectedEvent("123.345.456");
         List<String> tfField1Hits = new ArrayList<>();
         tfField1Hits.add("a b c");
         tfField1Hits.add("r");
         expectedDocument.getValue().put("TF_FIELD1", tfField1Hits);
         
-        tf_test(seekRange, "EVENT_FIELD1 =='a' && ((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ '.*r'))", expectedDocument,
-                        configureTLDTestData(11), Collections.EMPTY_LIST);
+        tf_test(seekRange, query, expectedDocument, configureTLDTestData(11), Collections.EMPTY_LIST);
     }
     
     /**
-     * Shard range given expected from RangeParitioner and EVENT_FIELD1 index lookup. ExceededValueThreshold TF FIELD triggers TF index lookup for evaluation.
+     * Shard range given expected from RangePartitioner and EVENT_FIELD1 index lookup. ExceededValueThreshold TF FIELD triggers TF index lookup for evaluation.
      *
      * fiFullySatisfies query, Create an ivarator for tf
      * 
@@ -59,59 +58,59 @@ public class TLDQueryIteratorIT extends QueryIteratorIT {
     @Test
     public void tf_exceededValue_leadingWildcard_shardRange_tld_test() throws IOException {
         // build the seek range for a document specific pull
-        Range seekRange = getDocumentRange("row", "dataType1", null);
+        Range seekRange = getShardRange();
+        String query = "EVENT_FIELD1 =='a' && ((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ '.*r'))";
         Map.Entry<Key,Map<String,List<String>>> expectedDocument = getBaseExpectedEvent("123.345.456");
         List<String> tfField1Hits = new ArrayList<>();
         tfField1Hits.add("a b c");
         tfField1Hits.add("r");
         expectedDocument.getValue().put("TF_FIELD1", tfField1Hits);
         
-        tf_test(seekRange, "EVENT_FIELD1 =='a' && ((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ '.*r'))", expectedDocument,
-                        configureTLDTestData(11), Collections.EMPTY_LIST);
+        tf_test(seekRange, query, expectedDocument, configureTLDTestData(11), Collections.EMPTY_LIST);
     }
     
     @Test
     public void tf_exceededValue_negated_leadingWildcard_documentSpecific_tld_test() throws IOException {
         // build the seek range for a document specific pull
-        Range seekRange = getDocumentRange("row", "dataType1", "123.345.456");
-        tf_test(seekRange, "EVENT_FIELD1 =='a' && !((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ '.*z'))",
-                        getBaseExpectedEvent("123.345.456"), configureTLDTestData(11), Collections.EMPTY_LIST);
+        Range seekRange = getDocumentRange("123.345.456");
+        String query = "EVENT_FIELD1 =='a' && !((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ '.*z'))";
+        tf_test(seekRange, query, getBaseExpectedEvent("123.345.456"), configureTLDTestData(11), Collections.EMPTY_LIST);
     }
     
     @Test
     public void tf_exceededValue_negated_leadingWildcard_shardRange_tld_test() throws IOException {
         // build the seek range for a document specific pull
-        Range seekRange = getDocumentRange("row", "dataType1", null);
-        tf_test(seekRange, "EVENT_FIELD1 =='a' && !((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ '.*z'))",
-                        getBaseExpectedEvent("123.345.456"), configureTLDTestData(11), Collections.EMPTY_LIST);
+        Range seekRange = getShardRange();
+        String query = "EVENT_FIELD1 =='a' && !((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ '.*z'))";
+        tf_test(seekRange, query, getBaseExpectedEvent("123.345.456"), configureTLDTestData(11), Collections.EMPTY_LIST);
     }
     
     @Test
     public void tf_exceededValue_trailingWildcard_documentSpecific_test() throws IOException {
         // build the seek range for a document specific pull
-        Range seekRange = getDocumentRange("row", "dataType1", "123.345.456");
+        Range seekRange = getDocumentRange("123.345.456");
+        String query = "EVENT_FIELD1 =='a' && ((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ 'r.*'))";
         Map.Entry<Key,Map<String,List<String>>> expectedDocument = getBaseExpectedEvent("123.345.456");
         List<String> tfField1Hits = new ArrayList<>();
         tfField1Hits.add("a b c");
         tfField1Hits.add("r");
         expectedDocument.getValue().put("TF_FIELD1", tfField1Hits);
         
-        tf_test(seekRange, "EVENT_FIELD1 =='a' && ((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ 'r.*'))", expectedDocument,
-                        configureTLDTestData(11), Collections.EMPTY_LIST);
+        tf_test(seekRange, query, expectedDocument, configureTLDTestData(11), Collections.EMPTY_LIST);
     }
     
     @Test
     public void tf_exceededValue_trailingWildcard_shardRange_test() throws IOException {
         // build the seek range for a document specific pull
-        Range seekRange = getDocumentRange("row", "dataType1", null);
+        Range seekRange = getShardRange();
+        String query = "EVENT_FIELD1 =='a' && ((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ 'r.*'))";
         Map.Entry<Key,Map<String,List<String>>> expectedDocument = getBaseExpectedEvent("123.345.456");
         List<String> tfField1Hits = new ArrayList<>();
         tfField1Hits.add("a b c");
         tfField1Hits.add("r");
         expectedDocument.getValue().put("TF_FIELD1", tfField1Hits);
         
-        tf_test(seekRange, "EVENT_FIELD1 =='a' && ((ExceededValueThresholdMarkerJexlNode = true) && (TF_FIELD1 =~ 'r.*'))", expectedDocument,
-                        configureTLDTestData(11), Collections.EMPTY_LIST);
+        tf_test(seekRange, query, expectedDocument, configureTLDTestData(11), Collections.EMPTY_LIST);
     }
     
     @Override
@@ -120,33 +119,34 @@ public class TLDQueryIteratorIT extends QueryIteratorIT {
         if (uid == null) {
             // get the shard range from the super
             return super.getDocumentRange(row, dataType, uid);
-        } else {
-            return new Range(new Key("row", "dataType1" + Constants.NULL + uid), true, new Key("row", "dataType1" + Constants.NULL + uid
-                            + Constants.MAX_UNICODE_STRING), true);
         }
+        
+        Key startKey = new Key(row, dataType + Constants.NULL + uid);
+        Key endKey = new Key(row, dataType + Constants.NULL + uid + Constants.MAX_UNICODE_STRING);
+        return new Range(startKey, true, endKey, true);
     }
     
     private List<Map.Entry<Key,Value>> configureTLDTestData(long eventTime) {
         List<Map.Entry<Key,Value>> listSource = super.configureTestData(eventTime);
         
         // add some indexed TF fields in a child
-        listSource.add(new AbstractMap.SimpleEntry<>(getEvent("row", "TF_FIELD1", "q r s", "dataType1", "123.345.456.1", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getFI("row", "TF_FIELD1", "q r s", "dataType1", "123.345.456.1", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getFI("row", "TF_FIELD1", "q", "dataType1", "123.345.456.1", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getFI("row", "TF_FIELD1", "r", "dataType1", "123.345.456.1", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getFI("row", "TF_FIELD1", "s", "dataType1", "123.345.456.1", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getTF("row", "TF_FIELD1", "q", "dataType1", "123.345.456.1", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getTF("row", "TF_FIELD1", "r", "dataType1", "123.345.456.1", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getTF("row", "TF_FIELD1", "s", "dataType1", "123.345.456.1", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getEvent("TF_FIELD1", "q r s", "123.345.456.1", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getFI("TF_FIELD1", "q r s", "123.345.456.1", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getFI("TF_FIELD1", "q", "123.345.456.1", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getFI("TF_FIELD1", "r", "123.345.456.1", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getFI("TF_FIELD1", "s", "123.345.456.1", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getTF("TF_FIELD1", "q", "123.345.456.1", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getTF("TF_FIELD1", "r", "123.345.456.1", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getTF("TF_FIELD1", "s", "123.345.456.1", eventTime), new Value()));
         
-        listSource.add(new AbstractMap.SimpleEntry<>(getEvent("row", "TF_FIELD2", "d e f", "dataType1", "123.345.456.2", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getFI("row", "TF_FIELD2", "d e f", "dataType1", "123.345.456.2", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getFI("row", "TF_FIELD2", "d", "dataType1", "123.345.456.2", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getFI("row", "TF_FIELD2", "e", "dataType1", "123.345.456.2", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getFI("row", "TF_FIELD2", "f", "dataType1", "123.345.456.2", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getTF("row", "TF_FIELD2", "d", "dataType1", "123.345.456.2", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getTF("row", "TF_FIELD2", "e", "dataType1", "123.345.456.2", eventTime), new Value()));
-        listSource.add(new AbstractMap.SimpleEntry<>(getTF("row", "TF_FIELD2", "f", "dataType1", "123.345.456.2", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getEvent("TF_FIELD2", "d e f", "123.345.456.2", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getFI("TF_FIELD2", "d e f", "123.345.456.2", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getFI("TF_FIELD2", "d", "123.345.456.2", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getFI("TF_FIELD2", "e", "123.345.456.2", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getFI("TF_FIELD2", "f", "123.345.456.2", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getTF("TF_FIELD2", "d", "123.345.456.2", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getTF("TF_FIELD2", "e", "123.345.456.2", eventTime), new Value()));
+        listSource.add(new AbstractMap.SimpleEntry<>(getTF("TF_FIELD2", "f", "123.345.456.2", eventTime), new Value()));
         
         return listSource;
     }
