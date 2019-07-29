b5267fe211ccffeeefe806b43a45fa3f3d6aaf87
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2019-07-29 15:14:00 +0000

    Added test for AllTermsIndexedVisitor.

---
+++ warehouse/query-core/src/test/java/datawave/query/jexl/visitors/AllTermsIndexedVisitorTest.java
@@ -0,0 +1,172 @@
+package datawave.query.jexl.visitors;
+
+import com.google.common.collect.Sets;
+import datawave.query.config.ShardQueryConfiguration;
+import datawave.query.exceptions.DatawaveFatalQueryException;
+import datawave.query.jexl.JexlASTHelper;
+import datawave.query.util.MockMetadataHelper;
+import org.apache.commons.jexl2.parser.ASTJexlScript;
+import org.apache.commons.jexl2.parser.ParseException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Set;
+
+public class AllTermsIndexedVisitorTest {
+    
+    private static MockMetadataHelper helper;
+    private static ShardQueryConfiguration config;
+    
+    private static Set<String> indexedFields = Sets.newHashSet("FOO", "FOO2", "FOO3");
+    
+    @BeforeClass
+    public static void beforeClass() {
+        helper = new MockMetadataHelper();
+        helper.setIndexedFields(indexedFields);
+        
+        config = new ShardQueryConfiguration();
+    }
+    
+    @Test
+    public void testIndexedSingleTerm() throws ParseException {
+        String query = "FOO == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    @Test
+    public void testIndexedConjunction() throws ParseException {
+        String query = "FOO == 'bar' || FOO2 == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    @Test
+    public void testIndexedDisjunction() throws ParseException {
+        String query = "FOO == 'bar' && FOO2 == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testNonIndexedSingleTerm() throws ParseException {
+        String query = "BAR == 'foo'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testNonIndexedConjunction() throws ParseException {
+        String query = "BAR == 'foo' && BAR2 == 'foo'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testNonIndexedDisjunction() throws ParseException {
+        String query = "FOO == 'bar' || BAR == 'foo'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testNonIndexedDoubleDisjunction() throws ParseException {
+        String query = "BAR == 'foo' || BAR2 == 'foo'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testNonIndexedTripleDisjunction() throws ParseException {
+        String query = "FOO == 'bar' || BAR == 'foo' || BAR2 == 'foo'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testFunction() throws ParseException {
+        String query = "FOO == 'bar' || content:phrase(termOffsetMap, 'bar', 'too')";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testOnlyFunction() throws ParseException {
+        String query = "content:phrase(termOffsetMap, 'bar', 'too')";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testFunctionOverIndexedField() throws ParseException {
+        String query = "content:phrase(termOffsetMap, FOO, 'bar', 'too')";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testRegex() throws ParseException {
+        String query = "FOO =~ 'bar.*'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testRegexWithExtraTerm() throws ParseException {
+        String query = "FOO =~ 'bar.*' || FOO == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testNegatedRegex() throws ParseException {
+        String query = "FOO !~ 'bar.*'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testNegatedRegexWithExtraTerm() throws ParseException {
+        String query = "FOO !~ 'bar.*' || FOO == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testLessThan() throws ParseException {
+        String query = "FOO < '+aE1'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testLessThanWithExtraTerm() throws ParseException {
+        String query = "FOO < '+aE1' || FOO == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testLessThanEquals() throws ParseException {
+        String query = "FOO <= '+aE1'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testLessThanEqualsWithExtraTerm() throws ParseException {
+        String query = "FOO <= '+aE1' || FOO == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testGreaterThan() throws ParseException {
+        String query = "FOO > '+aE1'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testGreaterThanWithExtraTerm() throws ParseException {
+        String query = "FOO > '+aE1' || FOO == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testGreaterThanEquals() throws ParseException {
+        String query = "FOO >= '+aE1'";
+        testIsIndexed(query);
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testGreaterThanEqualsWithExtraTerm() throws ParseException {
+        String query = "FOO >= '+aE1' || FOO == 'bar'";
+        testIsIndexed(query);
+    }
+    
+    private void testIsIndexed(String query) throws ParseException {
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
+        AllTermsIndexedVisitor.isIndexed(script, config, helper);
+    }
+}
