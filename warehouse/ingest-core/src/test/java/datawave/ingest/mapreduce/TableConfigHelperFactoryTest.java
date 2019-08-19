9a970610a89b935ebe2315a168bb42bdd4a3d196
Mike Miller <mmiller@apache.org>
2019-08-19 14:27:18 -0400

    Replace accumulo MockInstance with MAC

--- warehouse/ingest-core/src/test/java/datawave/ingest/mapreduce/TableConfigHelperFactoryTest.java
+++ warehouse/ingest-core/src/test/java/datawave/ingest/mapreduce/TableConfigHelperFactoryTest.java
@@ -6,29 +6,48 @@ import datawave.ingest.table.config.TableConfigHelper;
 import datawave.util.TableName;
 import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.admin.TableOperations;
-import org.apache.accumulo.core.client.mock.MockInstance;
-import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+import org.apache.accumulo.minicluster.MiniAccumuloCluster;
+import org.apache.accumulo.minicluster.MiniAccumuloConfig;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
+import java.io.File;
+
+/**
+ * Each Test starts and stops a mini accumulo cluster. Files are stored in
+ * warehouse/ingest-core/target/mac/datawave.ingest.mapreduce.TableConfigHelperFactoryTest-testname
+ */
 public class TableConfigHelperFactoryTest {
     private static final Logger logger = Logger.getLogger(TableConfigHelperFactoryTest.class);
+    @Rule
+    public TestName name = new TestName();
     
     private Configuration conf;
-    private MockInstance instance;
+    private MiniAccumuloCluster mac;
     private Connector connector;
     private TableOperations tops;
     
     private static final String TEST_SHARD_TABLE_NAME = "testShard";
     
+    public void startCluster() throws Exception {
+        File macDir = new File(System.getProperty("user.dir") + "/target/mac/" + TableConfigHelperFactoryTest.class.getName() + "-" + name.getMethodName());
+        macDir.mkdirs();
+        mac = new MiniAccumuloCluster(new MiniAccumuloConfig(macDir, "pass"));
+        mac.start();
+    }
+    
     @Before
     public void setup() throws Exception {
+        startCluster();
         conf = new Configuration();
         
         conf.set("shard.table.name", TableName.SHARD);
@@ -38,12 +57,16 @@ public class TableConfigHelperFactoryTest {
         conf.set("testShard.table.config.class", ShardTableConfigHelper.class.getName());
         conf.set("testShard.table.config.prefix", "test");
         
-        instance = new MockInstance();
-        connector = instance.getConnector("root", new PasswordToken(new byte[0]));
+        connector = mac.getConnector("root", "pass");
         tops = connector.tableOperations();
         
         recreateTable(tops, TableName.SHARD);
         recreateTable(tops, TEST_SHARD_TABLE_NAME);
+    }
+    
+    @After
+    public void shutdown() throws Exception {
+        mac.stop();
     }
     
     private void recreateTable(TableOperations tops, String table) throws Exception {
