6038c13b34ef401db808d37c8b8f35a4e51e15ae
Charles Williams <cwilliams.work@gmail.com>
2019-09-12 13:08:44 +0000

    fixes #565 Add better javadoc

--- warehouse/query-core/src/main/java/datawave/query/predicate/PeekingPredicate.java
+++ warehouse/query-core/src/main/java/datawave/query/predicate/PeekingPredicate.java
@@ -1,13 +1,18 @@
 package datawave.query.predicate;
 
 import com.google.common.base.Predicate;
+import org.apache.accumulo.core.data.Key;
 
 import javax.annotation.Nullable;
 
 public interface PeekingPredicate<T> extends Predicate<T> {
     /**
-     * peek at the result of applying an input without actually applying it
-     * 
+     * Peek at the result of applying an input without actually applying it. This method is equivalent to {@link #apply} without making any state updates that
+     * {@code apply()} may have made. This is necessary for cases where a predicate may be evaluated multiple times for different reasons in the lifecycle.
+     * Calling this method multiple times with the same value should always yield the same result, whereas calling {@code apply()} has no such guarantee
+     *
+     * @see datawave.query.predicate.TLDEventDataFilter#keep(Key)
+     *
      * @param input
      * @return
      */
