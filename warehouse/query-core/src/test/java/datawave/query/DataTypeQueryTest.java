62aa6ce284188234462e930a00fe3b6dfc5b41c3
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2019-09-12 15:18:43 +0000

    Prove that setting threshold properties has no effect on test results.

--- warehouse/query-core/src/test/java/datawave/query/DataTypeQueryTest.java
+++ warehouse/query-core/src/test/java/datawave/query/DataTypeQueryTest.java
@@ -132,6 +132,10 @@ public class DataTypeQueryTest extends AbstractFunctionalQuery {
         String query = CityField.STATE.name() + EQ_OP + "'missouri'";
         String expect = "(" + query + ")" + AND_OP + BaseRawData.EVENT_DATATYPE + EQ_OP + "'" + CityEntry.generic.getDataType() + "'";
         runTest(query, expect, qOptions);
+        
+        // Prove that setting this property has no effect on test results.
+        this.logic.setEventPerDayThreshold(Integer.MAX_VALUE);
+        runTest(query, expect, qOptions);
     }
     
     @Test
@@ -148,6 +152,11 @@ public class DataTypeQueryTest extends AbstractFunctionalQuery {
         
         String query = CityField.STATE.name() + EQ_OP + "'missouri'";
         String expect = "(" + query + ")" + AND_OP + BaseRawData.EVENT_DATATYPE + EQ_OP + "'" + CityEntry.generic.getDataType() + "'";
+        runTest(query, expect, qOptions);
+        
+        // Prove that setting this property has no effect on test results.
+        this.logic.setEventPerDayThreshold(Integer.MAX_VALUE);
+        this.logic.setShardsPerDayThreshold(Integer.MAX_VALUE);
         runTest(query, expect, qOptions);
     }
     
