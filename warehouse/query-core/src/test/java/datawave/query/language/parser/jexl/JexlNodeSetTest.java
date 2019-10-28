4c36e628dfa755015970842aec8384582b6bba93
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2019-10-28 12:09:49 +0000

    IndexMatch now uses JexlNodeSet.

--- warehouse/query-core/src/test/java/datawave/query/language/parser/jexl/JexlNodeSetTest.java
+++ warehouse/query-core/src/test/java/datawave/query/language/parser/jexl/JexlNodeSetTest.java
@@ -43,22 +43,22 @@ public class JexlNodeSetTest {
     
     @Test
     public void testContains() {
-        JexlNode node1 = JexlNodeFactory.buildEQNode("FOO", "bar");
-        JexlNodeSet jexlNodeSet = new JexlNodeSet();
-        jexlNodeSet.add(node1);
-        assertTrue(jexlNodeSet.contains(node1));
+        JexlNode node = JexlNodeFactory.buildEQNode("FOO", "bar");
+        JexlNodeSet nodeSet = new JexlNodeSet();
+        nodeSet.add(node);
+        assertTrue(nodeSet.contains(node));
         
         // Nope, it won't.
-        assertFalse(jexlNodeSet.contains("a string?!?!"));
+        assertFalse(nodeSet.contains("a string?!?!"));
     }
     
     @Test
     public void testIterator() {
         JexlNode node = JexlNodeFactory.buildEQNode("FOO", "bar");
-        JexlNodeSet jexlNodeSet = new JexlNodeSet();
-        jexlNodeSet.add(node);
-        
-        Iterator<JexlNode> iter = jexlNodeSet.iterator();
+        JexlNodeSet nodeSet = new JexlNodeSet();
+        nodeSet.add(node);
+
+        Iterator<JexlNode> iter = nodeSet.iterator();
         assertTrue(iter.hasNext());
         JexlNode next = iter.next();
         assertEquals(JexlASTHelper.nodeToKey(node), JexlASTHelper.nodeToKey(next));
@@ -95,7 +95,7 @@ public class JexlNodeSetTest {
         assertTrue(nodeSet.contains(node1));
         assertTrue(nodeSet.contains(node2));
         assertFalse(nodeSet.contains(node3));
-        
+
         assertTrue(nodeSet.add(node3));
         assertFalse(nodeSet.add(node3));
         assertFalse(nodeSet.add(node3));
@@ -260,7 +260,7 @@ public class JexlNodeSetTest {
         assertTrue(nodeSet.contains(node2));
         assertTrue(nodeSet.contains(node3));
         assertFalse(nodeSet.isEmpty());
-        
+
         nodeSet.clear();
         assertFalse(nodeSet.contains(node1));
         assertFalse(nodeSet.contains(node2));
