0b48f0aa1083e6e9eb9b5f87a44e26a2b900331e
Christopher Whitt cjwhi12 <cjwhi12@ip-10-113-15-96.evoforge.org>
2019-12-11 17:07:03 +0000

    issue #678: Added unit tests for getting Query Plan with different exceptions. Removed unused imports.

--- warehouse/query-core/src/test/java/datawave/query/QueryPlanTest.java
+++ warehouse/query-core/src/test/java/datawave/query/QueryPlanTest.java
@@ -11,14 +11,15 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collection;
 
+
 import static datawave.query.testframework.RawDataManager.RE_OP;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 
 public class QueryPlanTest extends AbstractFunctionalQuery {
-    
+
     private static final Logger log = Logger.getLogger(AnyFieldQueryTest.class);
-    
+
     @BeforeClass
     public static void filterSetup() throws Exception {
         Collection<DataTypeHadoopConfig> dataTypes = new ArrayList<>();
@@ -26,46 +27,48 @@ public class QueryPlanTest extends AbstractFunctionalQuery {
         generic.addReverseIndexField(CitiesDataType.CityField.STATE.name());
         generic.addReverseIndexField(CitiesDataType.CityField.CONTINENT.name());
         dataTypes.add(new CitiesDataType(CitiesDataType.CityEntry.generic, generic));
-        
+
         final AccumuloSetupHelper helper = new AccumuloSetupHelper(dataTypes);
         connector = helper.loadTables(log);
     }
-    
+
     public QueryPlanTest() {
         super(CitiesDataType.getManager());
     }
-    
+
     @Test
     public void getPlanAfterFullTableScanDisallowedException() throws Exception {
         for (final TestCities city : TestCities.values()) {
             String cityPhrase = " != " + "'" + city.name() + "'";
             String query = Constants.ANY_FIELD + cityPhrase;
-            // Test list of cities for each plan
+            //Test list of cities for each plan
             try {
                 GenericQueryConfiguration config = setupConfig(query);
                 fail("Expected FullTableScanDisallowedException.");
             } catch (FullTableScansDisallowedException e) {
-                // assure that Query Plan is not null
-                assertNotEquals(null, "No Query Plan was set.", this.logic.getQueryPlan());
+                // assure that Query Plan is not default value
+                assertNotEquals("There is no plan.", "No Query Plan was set.", this.logic.getQueryPlan());
             }
         }
     }
-    
+
     @Test
     public void getPlanAfterDatawaveFatalQueryException() throws Exception {
         String phrase = RE_OP + "'.*iss.*'";
         String query = Constants.ANY_FIELD + phrase;
-        
+
         // Test the plan
         try {
             GenericQueryConfiguration config = setupConfig(query);
-            fail("Expected DatawaveFatalQueryException.");
+            fail("Expected DatawaveFatalQueryException but got plan: " + this.logic.getQueryPlan());
         } catch (DatawaveFatalQueryException e) {
-            // assure that Query Plan is not null
-            assertNotEquals(null, "No Query Plan was set.", this.logic.getQueryPlan());
+            // assure that Query Plan is not default value
+            assertNotEquals("There is no plan.", "No Query Plan was set.", this.logic.getQueryPlan());
         }
     }
-    
+
+
+
     // ============================================
     // implemented abstract methods
     protected void testInit() {
