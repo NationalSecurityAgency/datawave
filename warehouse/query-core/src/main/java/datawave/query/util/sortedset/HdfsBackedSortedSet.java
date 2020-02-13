36a813e58c00da9537bc63edf7b006b176caddbe
Adam Lerman <alerman@gmail.com>
2020-02-13 10:51:53 +0000

    Add deprecated annotation to signal users to update

--- warehouse/query-core/src/main/java/datawave/query/util/sortedset/HdfsBackedSortedSet.java
+++ warehouse/query-core/src/main/java/datawave/query/util/sortedset/HdfsBackedSortedSet.java
@@ -31,6 +31,7 @@ public class HdfsBackedSortedSet<E> extends BufferedFileBackedSortedSet<E> imple
         this(comparator, 10000, fs, uniqueDir, maxOpenFiles, setFactory);
     }
     
+    @Deprecated
     public HdfsBackedSortedSet(Comparator<? super E> comparator, int bufferPersistThreshold, FileSystem fs, Path uniqueDir, int maxOpenFiles)
                     throws IOException {
         this(comparator, bufferPersistThreshold, fs, uniqueDir, maxOpenFiles, new FileSerializableSortedSet.Factory());
