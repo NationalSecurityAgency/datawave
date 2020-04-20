b904ff634d4213dcff5465c921039c0ceae8f064
palindrome <31748527+hlgp@users.noreply.github.com>
2020-04-20 14:31:53 -0400

    Addresses #721.  Set groundwork for internally generated evaluation only fields (#800)

--- warehouse/query-core/src/main/java/datawave/query/planner/DefaultQueryPlanner.java
+++ warehouse/query-core/src/main/java/datawave/query/planner/DefaultQueryPlanner.java
@@ -969,8 +969,10 @@ public class DefaultQueryPlanner extends QueryPlanner {
             
             // Verify that the query does not contain fields we've never seen
             // before
+            Set<String> specialFields = Sets.newHashSet(QueryOptions.DEFAULT_DATATYPE_FIELDNAME, Constants.ANY_FIELD, Constants.NO_FIELD);
+            specialFields.addAll(config.getEvaluationOnlyFields());
             Set<String> nonexistentFields = FieldMissingFromSchemaVisitor.getNonExistentFields(metadataHelper, queryTree, config.getDatatypeFilter(),
-                            Sets.newHashSet(QueryOptions.DEFAULT_DATATYPE_FIELDNAME, Constants.ANY_FIELD, Constants.NO_FIELD));
+                            specialFields);
             if (log.isDebugEnabled()) {
                 log.debug("Testing for non-existent fields, found: " + nonexistentFields.size());
             }
