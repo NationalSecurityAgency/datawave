998d92c5c93d11dca2323a429e56aeea4452bb35
Jeffrey L. Zeiberg <jzeiberg@gmail.com>
2020-06-08 16:21:37 -0400

    Integrating with the new metadata-utils code in another project

--- warehouse/ingest-core/src/main/java/datawave/iterators/FrequencyColumnIterator.java
+++ warehouse/ingest-core/src/main/java/datawave/iterators/FrequencyColumnIterator.java
@@ -1,6 +1,7 @@
 package datawave.iterators;
 
 import datawave.query.util.FrequencyFamilyCounter;
+import datawave.query.util.MetadataHelper;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.PartialKey;
 import org.apache.accumulo.core.data.Value;
@@ -16,7 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FrequencyColumnIterator extends TransformingIterator {
-    public static String COL_QUAL_PREFIX = "compressed-";
+    // public static String COL_QUAL_PREFIX = "compressed-";
     public static String COL_QUAL_TOTAL = "total";
     private HashMap<String,Long> qualifierToFrequencyValueMap = new HashMap<>();
     private FrequencyFamilyCounter frequencyFamilyCounter = new FrequencyFamilyCounter();
@@ -53,7 +54,7 @@ public class FrequencyColumnIterator extends TransformingIterator {
             Key oldKey = sortedKeyValueIterator.getTopKey();
             Value oldValue = sortedKeyValueIterator.getTopValue();
             
-            if (cq.toString().startsWith(COL_QUAL_PREFIX)) {
+            if (cq.toString().startsWith(MetadataHelper.COL_QUAL_PREFIX)) {
                 log.info("Aggregate key is " + oldKey);
                 newKey = oldKey;
                 frequencyFamilyCounter.deserializeCompressedValue(oldValue);
@@ -63,7 +64,7 @@ public class FrequencyColumnIterator extends TransformingIterator {
                     frequencyFamilyCounter.insertIntoMap(cq.toString(), oldValue.toString());
                 
                 if (newKey == null)
-                    newKey = new Key(oldKey.getRow(), oldKey.getColumnFamily(), new Text(COL_QUAL_PREFIX + cq.toString().substring(0, 3)));
+                    newKey = new Key(oldKey.getRow(), oldKey.getColumnFamily(), new Text(MetadataHelper.COL_QUAL_PREFIX + cq.toString().substring(0, 3)));
                 
             }
             
@@ -72,12 +73,12 @@ public class FrequencyColumnIterator extends TransformingIterator {
         
         if (numRecords > 1) {
             kvBuffer.append(newKey, frequencyFamilyCounter.serialize());
-            if (newKey.getColumnQualifier().toString().startsWith(COL_QUAL_PREFIX))
+            if (newKey.getColumnQualifier().toString().startsWith(MetadataHelper.COL_QUAL_PREFIX))
                 kvBuffer.append(new Key(newKey.getRow(), newKey.getColumnFamily(), new Text(COL_QUAL_TOTAL)),
                                 new Value(Long.toString(frequencyFamilyCounter.getTotal())));
         } else if (numRecords == 1) {
             kvBuffer.append(topKey, topValue);
-            if (topKey.getColumnQualifier().toString().startsWith(COL_QUAL_PREFIX))
+            if (topKey.getColumnQualifier().toString().startsWith(MetadataHelper.COL_QUAL_PREFIX))
                 kvBuffer.append(new Key(topKey.getRow(), topKey.getColumnFamily(), new Text(COL_QUAL_TOTAL)),
                                 new Value(Long.toString(frequencyFamilyCounter.getTotal())));
             log.info("Range did not need to be transformed  (ran identity transform");
