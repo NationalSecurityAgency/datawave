924956aa43f2c18e82da3bd95e605b95e29ed159
Ivan Bella <ivan@bella.name>
2020-08-27 16:30:53 +0000

    fixes #907 Implement removeIf on HdfsBackedSortedSet * Implemented removeIf on the supper class BufferedFileBackedSortedSet

--- warehouse/query-core/src/main/java/datawave/query/util/sortedset/FileSortedSet.java
+++ warehouse/query-core/src/main/java/datawave/query/util/sortedset/FileSortedSet.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.function.Predicate;
 
 /**
  * A sorted set that can be persisted into a file and still be read in its persisted state. The set can always be re-loaded and then all operations will work as
@@ -494,7 +495,16 @@ public abstract class FileSortedSet<E> implements SortedSet<E>, Cloneable {
             return set.removeAll(c);
         }
     }
-    
+
+    @Override
+    public boolean removeIf(Predicate<? super E> filter) {
+        if (persisted) {
+            throw new IllegalStateException("Unable to remove from a persisted FileSortedSet.  Please call load() first.");
+        } else {
+            return set.removeIf(filter);
+        }
+    }
+
     @Override
     public void clear() {
         if (persisted) {
@@ -723,7 +733,7 @@ public abstract class FileSortedSet<E> implements SortedSet<E>, Cloneable {
         
         @Override
         public void remove() {
-            throw new UnsupportedOperationException("Cannot remove elements from a persisted file.  Please call load() first.");
+            throw new UnsupportedOperationException("Iterator.remove() not supported.");
         }
         
         @Override
