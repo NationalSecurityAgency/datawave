ca7a8cbce93302e1beaca50209fc4c0293071496
Jeffrey L. Zeiberg <jzeiberg@gmail.com>
2020-09-29 14:39:57 -0400

    Stubbed out tests for NestedIteratorTest.java

---
+++ warehouse/query-core/src/test/java/datawave/core/iterators/NestedIteratorsTest.java
@@ -0,0 +1,88 @@
+package datawave.core.iterators;
+
+import com.google.common.io.Files;
+import datawave.query.Constants;
+import datawave.query.iterator.CachingIterator;
+import datawave.query.iterator.builder.AndIteratorBuilder;
+import datawave.query.iterator.ivarator.IvaratorCacheDir;
+import datawave.query.iterator.ivarator.IvaratorCacheDirConfig;
+import datawave.query.iterator.logic.AndIterator;
+import datawave.query.iterator.logic.DocumentAggregatingIterator;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Range;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.yarn.webapp.hamlet.Hamlet;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NestedIteratorsTest {
+    
+    @Test
+    public void DocumentIteratorTest() {
+        
+    }
+    
+    @Test
+    public void AndIteratorTest() {
+        
+    }
+    
+    @Test
+    public void OrIteratorTest() {
+        
+    }
+    
+    @Test
+    public void DocumentAggregatingIteratorTest() {
+        
+    }
+    
+    @Test
+    public void IndexIteratorTest() {
+        
+    }
+    
+    @Test
+    public void IndexIteratorBridgeTest() {
+        
+    }
+    
+    @Test
+    public void ArrayIteratorTest() {
+        
+    }
+    
+    @Test
+    public void DocumentSpecificNestedIterator() {
+        
+    }
+    
+    @Test
+    public void EmptyTreeIterableTest() {
+        
+    }
+    
+    @Test
+    public void EvaluationTrackingNestedIterator() {
+        
+    }
+    
+    @Test
+    public void EventDataScanNestedIterator() {
+        
+    }
+    
+    @Test
+    public void NestedQueryIteratorTest() {
+        
+    }
+    
+    @Test
+    void SeekableNestedIteratorTest() {
+        
+    }
+}
