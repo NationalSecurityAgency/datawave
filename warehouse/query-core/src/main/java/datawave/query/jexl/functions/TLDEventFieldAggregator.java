7d3d25df5154fc8738b9901ea09c77e3eca2602a
Charles Williams <cwilliams.work@gmail.com>
2021-03-11 16:49:10 +0000

    fixes #858: Use UID to parse base UID

--- warehouse/query-core/src/main/java/datawave/query/jexl/functions/TLDEventFieldAggregator.java
+++ warehouse/query-core/src/main/java/datawave/query/jexl/functions/TLDEventFieldAggregator.java
@@ -1,8 +1,10 @@
 package datawave.query.jexl.functions;
 
+import datawave.data.hash.UID;
 import datawave.query.predicate.EventDataQueryFilter;
 import datawave.query.tld.TLD;
 import datawave.query.util.TypeMetadata;
+import org.apache.accumulo.core.data.ArrayByteSequence;
 import org.apache.accumulo.core.data.ByteSequence;
 
 import java.util.ArrayList;
@@ -17,18 +19,15 @@ public class TLDEventFieldAggregator extends EventFieldAggregator {
         // find the null between the dataType and Uid
         ArrayList<Integer> nulls = TLD.instancesOf(0, columnFamily, 1);
         final int start = nulls.get(0) + 1;
-        
         // uid is from the null byte to the end of the cf
-        ByteSequence uid = columnFamily.subSequence(start, columnFamily.length());
+        ByteSequence uidByteSequence = columnFamily.subSequence(start, columnFamily.length());
         
-        // find the end of the tld if it exists
-        ArrayList<Integer> dots = TLD.instancesOf('.', uid);
-        if (dots.size() > 2) {
-            // reduce to the TLD
-            return columnFamily.subSequence(0, start + dots.get(2));
-        } else {
-            // no reduction necessary, already tld
-            return columnFamily;
+        UID uid = UID.parseBase(uidByteSequence.toString());
+        if (uid.getBaseUid().length() != uidByteSequence.length()) {
+            return new ArrayByteSequence(columnFamily.subSequence(0, start) + uid.getBaseUid());
         }
+        
+        // no reduction necessary, already tld
+        return columnFamily;
     }
 }
