a11f58b6dfba7d25f5d1b792008a9a5c0a239b28
Laura Schanno <lbschanno@gmail.com>
2021-06-22 13:40:51 -0400

    Fix code formatting

--- warehouse/query-core/src/main/java/datawave/query/transformer/UniqueTransform.java
+++ warehouse/query-core/src/main/java/datawave/query/transformer/UniqueTransform.java
@@ -59,8 +59,11 @@ public class UniqueTransform extends DocumentTransform.DefaultDocumentTransform 
     
     /**
      * Create a new {@link UniqueTransform} that will capture the reverse field mapping defined within the model being used by the logic (if present).
-     * @param logic the logic
-     * @param uniqueFields the set of fields to find unique values for
+     * 
+     * @param logic
+     *            the logic
+     * @param uniqueFields
+     *            the set of fields to find unique values for
      */
     public UniqueTransform(BaseQueryLogic<Entry<Key,Value>> logic, UniqueFields uniqueFields) {
         this(uniqueFields);
