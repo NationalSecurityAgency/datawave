1ce0b0bf70d408db47adf4f6540e56b86c4ad52c
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2021-07-21 10:29:20 +0000

    Update for rebase onto integration from feature/accumulo2.0

--- warehouse/query-core/src/test/java/datawave/query/postprocessing/tf/TermFrequencyHitFunctionTest.java
+++ warehouse/query-core/src/test/java/datawave/query/postprocessing/tf/TermFrequencyHitFunctionTest.java
@@ -6,15 +6,20 @@ import datawave.query.attributes.Document;
 import datawave.query.attributes.PreNormalizedAttribute;
 import datawave.query.iterator.SortedListKeyValueIterator;
 import datawave.query.jexl.JexlASTHelper;
+import org.apache.accumulo.core.client.sample.SamplerConfiguration;
+import org.apache.accumulo.core.conf.AccumuloConfiguration;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.iterators.IteratorEnvironment;
+import org.apache.accumulo.core.iterators.IteratorUtil;
 import org.apache.accumulo.core.iterators.SortedKeyValueIterator;
+import org.apache.accumulo.core.security.Authorizations;
 import org.apache.commons.jexl2.parser.ASTJexlScript;
 import org.apache.commons.jexl2.parser.ParseException;
 import org.apache.hadoop.io.Text;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -621,7 +626,52 @@ public class TermFrequencyHitFunctionTest {
         assertEquals(expected, hits);
     }
     
-    public static class TestIteratorEnvironment implements IteratorEnvironment {}
+    public static class TestIteratorEnvironment implements IteratorEnvironment {
+        @Override
+        public SortedKeyValueIterator<Key,Value> reserveMapFileReader(String s) throws IOException {
+            return null;
+        }
+        
+        @Override
+        public AccumuloConfiguration getConfig() {
+            return null;
+        }
+        
+        @Override
+        public IteratorUtil.IteratorScope getIteratorScope() {
+            return null;
+        }
+        
+        @Override
+        public boolean isFullMajorCompaction() {
+            return false;
+        }
+        
+        @Override
+        public void registerSideChannel(SortedKeyValueIterator<Key,Value> sortedKeyValueIterator) {
+            
+        }
+        
+        @Override
+        public Authorizations getAuthorizations() {
+            return null;
+        }
+        
+        @Override
+        public IteratorEnvironment cloneWithSamplingEnabled() {
+            return null;
+        }
+        
+        @Override
+        public boolean isSamplingEnabled() {
+            return false;
+        }
+        
+        @Override
+        public SamplerConfiguration getSamplerConfiguration() {
+            return null;
+        }
+    }
     
     // blue, red, yellow are positive terms
     // purple, orange, green are negative terms (i.e., will only appear in the FI source).
