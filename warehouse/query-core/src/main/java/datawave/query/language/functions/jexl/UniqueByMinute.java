36e4af7d305e933fa673e46b35fe35a362e2d18e
Laura Schanno <lbschanno@gmail.com>
2021-08-09 17:11:14 -0400

    Add support for further granularity in #UNIQUE (#1197)
    * Add support for further granularity in #UNIQUE
    
    Begin to add support for specifying sub-critiera for the #UNIQUE
    function such that a user may specify
    
    * #UNIQUE(#DAY(datetime_field))
    * #UNIQUE(#HOUR(datetime_field))
    * #UNIQUE(#MINUTE(datetime_field))
    
    in order to get increased granularity by the day, hour, and minute of a
    given field.
    
    Work for #711
    
    * Make UniqueFields extend Serializable
    
    * Add documentation for UniqueFields.from()
    
    * Move UniqueFields and ValueTransformer to attributes package
    
    * Make UniqueFields.from() support case-insensitivity
    
    * Simplify UniqueFields and rename ValueTransformer
    
    * Add Lucene support
    
    * Fix code formatting
    
    * Delete obsolete constructors
    
    * Add class-level comments for new classes
    
    * Explicity reference this.uniqueFields
    
    * Use apache.commons StringUtils instead of datawave's
    
    * Fix code formatting
    
    * Simplify logic for UniqueFields.from()
    
    * Create UniqueByFunction super class
    
    Co-authored-by: Moriarty <22225248+apmoriarty@users.noreply.github.com>
    Co-authored-by: hgklohr <hgklohr@gmail.com>

---
+++ warehouse/query-core/src/main/java/datawave/query/language/functions/jexl/UniqueByMinute.java
@@ -0,0 +1,24 @@
+package datawave.query.language.functions.jexl;
+
+import datawave.query.jexl.functions.QueryFunctions;
+import datawave.query.language.functions.QueryFunction;
+import datawave.webservice.query.exception.BadRequestQueryException;
+import datawave.webservice.query.exception.DatawaveErrorCode;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+
+/**
+ * Function to return a unique result for every minute of the hour for a given list of fields. This function is equivalent to {@code #unique(field[MINUTE])}.
+ */
+public class UniqueByMinute extends UniqueByFunction {
+    
+    public UniqueByMinute() {
+        super(QueryFunctions.UNIQUE_BY_MINUTE_FUNCTION, new ArrayList<>());
+    }
+    
+    @Override
+    public QueryFunction duplicate() {
+        return new UniqueByMinute();
+    }
+}
