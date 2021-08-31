cba67c2604f8bc3f5f658ebd36f1d3b54a8daf08
Ivan Bella <ivan@bella.name>
2021-08-31 13:05:29 +0000

    Revert "Address extra parens introduced by several visitors. ASTDelayedPredicate detects if a node is already delayed. (#1165)"
    This reverts commit ae691b44a638a64cb5421760e53e8d4bfa27e619.

--- warehouse/query-core/src/main/java/datawave/query/planner/DefaultQueryPlanner.java
+++ warehouse/query-core/src/main/java/datawave/query/planner/DefaultQueryPlanner.java
@@ -1248,9 +1248,6 @@ public class DefaultQueryPlanner extends QueryPlanner implements Cloneable {
                 ParallelIndexExpansion regexExpansion = new ParallelIndexExpansion(config, scannerFactory, metadataHelper, expansionFields,
                                 config.isExpandFields(), config.isExpandValues(), config.isExpandUnfieldedNegations());
                 queryTree = (ASTJexlScript) regexExpansion.visit(queryTree, null);
-                
-                // regex expansion picks up an extra set of parens, so quickly fix that here
-                queryTree = (ASTJexlScript) TreeFlatteningRebuildingVisitor.flatten(queryTree);
                 if (log.isDebugEnabled()) {
                     logQuery(queryTree, "Query after expanding regex:");
                 }
