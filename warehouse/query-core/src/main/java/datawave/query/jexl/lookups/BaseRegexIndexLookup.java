7d9c74ea2b48a0f05ce348fef01f31152ac05cb7
Whitney O'Meara <james.w.omeara@gmail.com>
2021-10-08 14:47:44 -0400

    Updated the unfielded field index lookups to be asynchronous.

--- warehouse/query-core/src/main/java/datawave/query/jexl/lookups/BaseRegexIndexLookup.java
+++ warehouse/query-core/src/main/java/datawave/query/jexl/lookups/BaseRegexIndexLookup.java
@@ -5,12 +5,13 @@ import datawave.query.config.ShardQueryConfiguration;
 import datawave.query.tables.ScannerFactory;
 
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
 
 public abstract class BaseRegexIndexLookup extends AsyncIndexLookup {
     public static final Set<String> DISALLOWED_PATTERNS = Sets.newHashSet(".*", ".*?");
     
-    public BaseRegexIndexLookup(ShardQueryConfiguration config, ScannerFactory scannerFactory) {
-        super(config, scannerFactory);
+    public BaseRegexIndexLookup(ShardQueryConfiguration config, ScannerFactory scannerFactory, boolean unfieldedLookup, ExecutorService execService) {
+        super(config, scannerFactory, unfieldedLookup, execService);
     }
     
     public static boolean isAcceptedPattern(String pattern) {
