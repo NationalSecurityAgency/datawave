d1adb5a3c87a5f1bff2cb355fc0386626066abcf
Jeffrey Schmidt <jschmidt10@github.com>
2021-11-09 17:38:07 +0000

    Kill YARN jobs with a retry loop

--- warehouse/ingest-scripts/src/main/resources/bin/ingest/stop-ingesters.sh
+++ warehouse/ingest-scripts/src/main/resources/bin/ingest/stop-ingesters.sh
@@ -19,6 +19,56 @@ fi
 # lock out any startups
 touch ${LOCK_FILE_DIR}/INGEST_STARTUP.LCK
 
+INGEST_JOB_POSTKILL_DELAY_SECS="${INGEST_JOB_POSTKILL_DELAY_SECS}"
+INGEST_JOB_KILL_MAX_RETRIES="${INGEST_JOB_KILL_MAX_RETRIES}"
+INGEST_JOB_KILL_LOOP_DELAY_SECS="${INGEST_JOB_KILL_LOOP_DELAY_SECS}"
+
+[[ -n "${INGEST_JOB_POSTKILL_DELAY_SECS}" ]] || { echo "Must define INGEST_JOB_POSTKILL_DELAY_SECS"; exit 1; }
+[[ -n "${INGEST_JOB_KILL_MAX_RETRIES}" ]] || { echo "Must define INGEST_JOB_KILL_MAX_RETRIES"; exit 1; }
+[[ -n "${INGEST_JOB_KILL_LOOP_DELAY_SECS}" ]] || { echo "Must define INGEST_JOB_KILL_LOOP_DELAY_SECS"; exit 1; }
+
+function getNumRunningJobs() {
+  echo "$(jps -lm | grep -c datawave.ingest.mapreduce.job.IngestJob)"
+}
+
+function killYarnJobsAndWait() {
+  echo "Terminating YARN IngestJobs"
+  ./kill-jobs-regex.sh '.*IngestJob.*'
+
+  echo "Waiting ${INGEST_JOB_POSTKILL_DELAY_SECS} seconds for jobs to cleanup"
+  # Grace period to let IngestJob's finish cleanup
+  sleep "${INGEST_JOB_POSTKILL_DELAY_SECS}"
+}
+
+function startYarnKillLoop() {
+  killYarnJobsAndWait
+
+  numRunningJobs=$(getNumRunningJobs)
+  retryNum=1
+
+  while [[ ${numRunningJobs} > 0 && ${retryNum} < ${INGEST_JOB_KILL_MAX_RETRIES} ]]; do
+    echo "Retry ${retryNum}/${INGEST_JOB_KILL_MAX_RETRIES}: ${numRunningJobs} jobs still running"
+    killYarnJobsAndWait
+
+    numRunningJobs=$(getNumRunningJobs)
+
+    if [[ ${numRunningJobs} > 0 ]]; then
+      echo "${numRunningJobs} jobs still running. Waiting for ${INGEST_JOB_KILL_LOOP_DELAY_SECS} seconds before issuing the next yarn kill."
+      sleep "${INGEST_JOB_KILL_LOOP_DELAY_SECS}"
+    fi
+
+    numRunningJobs=$(getNumRunningJobs)
+    retryNum=$(( retryNum + 1 ))
+  done
+
+  numRunningJobs=$(getNumRunningJobs)
+  if [[ ${numRunningJobs} > 0 ]]; then
+    echo "ERROR: Out of retries but ${numRunningJobs} jobs still running. Manual intervention is needed. Terminate rogue jobs and try again."
+    exit 1
+  else
+    echo "All jobs killed."
+  fi
+}
 
 # pull out the signal if supplied
 SIGNAL=""
@@ -37,7 +87,7 @@ STOP_INGEST_SERVERS_CMD=$THIS_DIR/stop-ingest-servers.sh
 
 $STOP_INGEST_SERVERS_CMD -type all -signal $SIGNAL
 
-./kill-jobs-regex.sh '.*IngestJob.*'
+startYarnKillLoop
 
 for f in `shopt -s extglob; find ${FLAG_DIR} -regextype posix-egrep -regex ".*\.flag\..*\.marker" 2>/dev/null`; do
    flag_file=$(flagBasename $f).flag
