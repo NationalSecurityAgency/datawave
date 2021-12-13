4efaca2c75afeece4f1c35a1af4cc7377d4208b7
Matthew Peterson <mjpete3@evoforge.org>
2021-12-13 17:22:40 +0000

    Added comments

--- warehouse/ingest-scripts/src/main/resources/bin/ingest/ingest-env.sh
+++ warehouse/ingest-scripts/src/main/resources/bin/ingest/ingest-env.sh
@@ -181,31 +181,43 @@ MAP_LOADER_HDFS_NAME_NODES_CONFIG="${MAP_LOADER_HDFS_NAME_NODES_CONFIG}"
 # If replacement property MAP_LOADER_HDFS_NAME_NODES_CONFIG is non-empty, override legacy properties:
 # MAP_LOADER_HDFS_NAME_NODES and NUM_MAP_LOADERS
 if [[ ! -z ${MAP_LOADER_HDFS_NAME_NODES_CONFIG} ]]; then
-  IFS=";"MAP_LOADER_HDFS_NAME_NODES=($MAP_LOADER_HDFS_NAME_NODES_CONFIG)
+  IFS=";" MAP_LOADER_HDFS_NAME_NODES=($MAP_LOADER_HDFS_NAME_NODES_CONFIG)
   MAP_LOADER_HDFS_DIRS=($MAP_LOADER_HDFS_NAME_NODES_CONFIG)
   INDEX=0
 
   for MAP_LOADER_HDFS_NAME in ${MAP_LOADER_HDFS_NAME_NODES[@]}; do
-    NUM_MAP_LOADERS[$INDEX]=$(echo $MAP_LOADER_HDFS_NAME_NODE |cut -d \, -f 2)
-    MAP_LOADER_HDFS_NAME_NODES[$INDEX]=$(echo $MAP_LOADER_HDFS_NAME_NODE |cut -d \, -f 1|cut -d \,-f 1  | cut -d /-f 1-3)
-    #for viewfs, add the last / back on
+
+    # Number of loaders for current entry
+    NUM_MAP_LOADERS[$INDEX]=$(echo $MAP_LOADER_HDFS_NAME_NODE |cut -d, -f2)
+
+    # Namenode (without directory) for current entry
+    MAP_LOADER_HDFS_NAME_NODES[$INDEX]=$(echo $MAP_LOADER_HDFS_NAME_NODE | cut -d, -f1 | cut -d/ -f1-3)
+
+    # For viewfs, add the last / back on
     case $MAP_LOADER_HDFS_NAME_NODES[$INDEX] IN viewfs:*)
       MAP_LOADER_HDFS_NAME_NODES[$INDEX]="${MAP_LOADER_HDFS_NAME_NODES[$INDEX]}/"
     esac
-    MAP_LOADER_HDFS_DIRS[$INDEX]=$(echo $MAP_LOADER_HDFS_NAME_NODE |cut -d \, -f 1 |cut -d / -f 4- | sed's/^/\//')
-    if[[ "${MAP_LOADER_HDFS_DIRS[$INDEX]}" == "/" ]]; then
+
+    # Directory (without namenode) for current entry.  If undefined, use base work directory.
+    MAP_LOADER_HDFS_DIRS[$INDEX]=$(echo $MAP_LOADER_HDFS_NAME_NODE | cut -d, -f1 | cut -d/ -f4- | sed 's/^/\//')
+    if [[ "${MAP_LOADER_HDFS_DIRS[$INDEX]}" == "/" ]]; then
       MAP_LOADER_HDFS_DIRS[$INDEX]=${BASE_WORK_DIR}
     fi
+
+    # Insist on numerical format for NUM_MAP_LOADERS entries
     if ! [[ ${NUM_MAP_LOADERS[INDEX]} = ~ '^[0-9]+$' ]]; then
-      echo "Invalid format for MAP_LOADER_HDFS_NAME_NODES_CONFIG. eg. MAP_LOADER_HDFS_NAME_NODES_CONFIG=hdfs://namenode-1:1;hdfs://namenode-2:3;"
-      echo "exiting..."
+      echo "Invalid format for MAP_LOADER_HDFS_NAME_NODES_CONFIG."
+      echo "Example configuration: MAP_LOADER_HDFS_NAME_NODES_CONFIG=hdfs://namenode-1:1;hdfs://namenode-2:3;"
+      echo "Exiting..."
       exit -1
     fi
 
     INDEX=$((INDEX+1))
   done
-fi
 
+  export NUM_MAP_LOADERS
+  export MAP_LOADER_HDFS_NAME_NODES
+fi
 
 ZOOKEEPER_HOME="${ZOOKEEPER_HOME}"
 
