69ddf2d5ce718f16b99516722f7e8e4fcdace954
Miner <andrew.lf.miner@gmail.com>
2022-03-14 03:49:44 +0000

    More formatting

--- warehouse/query-core/src/main/java/datawave/query/util/FuzzyAttributeComparator.java
+++ warehouse/query-core/src/main/java/datawave/query/util/FuzzyAttributeComparator.java
@@ -4,38 +4,35 @@ import datawave.query.attributes.Attribute;
 import datawave.query.attributes.Attributes;
 
 public final class FuzzyAttributeComparator {
-
+    
     // this is a utility and should never be instantiated
     private FuzzyAttributeComparator() {
         throw new UnsupportedOperationException();
     }
-
+    
     public static boolean singleToSingle(Attribute existingAttribute, Attribute newAttribute) {
         return existingAttribute.getData().equals(newAttribute.getData());
     }
-
+    
     public static boolean singleToMultiple(Attributes multipleAttributes, Attribute singleAttribute) {
         return multipleAttributes.getAttributes().stream().anyMatch(existingAttribute -> {
             return existingAttribute.getData().equals(singleAttribute.getData());
         });
     }
-
+    
     public static boolean multipleToMultiple(Attributes existingAttributes, Attributes newAttributes) {
         boolean containsMatch = false;
         for (Attribute<? extends Comparable<?>> newAttr : newAttributes.getAttributes()) {
-            boolean tempMatch = existingAttributes
-                    .getAttributes()
-                    .stream()
-                    .anyMatch(existingAttribute -> {
-                        return existingAttribute.getData().equals(newAttr.getData());
-                    });
+            boolean tempMatch = existingAttributes.getAttributes().stream().anyMatch(existingAttribute -> {
+                return existingAttribute.getData().equals(newAttr.getData());
+            });
             if (tempMatch) {
                 containsMatch = true;
             }
         }
         return containsMatch;
     }
-
+    
     public static Attribute combineAttributes(Attribute prenormalizedAttribute, Attribute contentAttribute) {
         return null;
     }
