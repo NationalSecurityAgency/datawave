7bb21efda6d908f7d985a2c913b584deb63b8d54
phrocker <phrocker@apache.org>
2022-03-23 10:22:59 +0000

    upate deser

--- warehouse/query-core/src/main/java/datawave/query/function/serializer/JsonDocumentSerializer.java
+++ warehouse/query-core/src/main/java/datawave/query/function/serializer/JsonDocumentSerializer.java
@@ -1,24 +1,28 @@
 package datawave.query.function.serializer;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import datawave.data.type.NumberType;
+import datawave.query.attributes.Attribute;
+import datawave.query.attributes.Attributes;
 import datawave.query.attributes.Document;
+import datawave.query.attributes.TypeAttribute;
 import datawave.query.function.json.deser.JsonDeser;
-import org.apache.log4j.Logger;
 
 import java.io.StringWriter;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.util.Map;
 
 /**
  * Json Document Serializer converts the POJO using gsonBuilder to a json string.
  */
 public class JsonDocumentSerializer extends DocumentSerializer {
 
-    private static final Logger log = Logger.getLogger(JsonDocumentSerializer.class);
-
     static GsonBuilder gsonBuilder = new GsonBuilder();
-
+    static Gson gson;
     static{
         gsonBuilder.registerTypeAdapter(Document.class,new JsonDeser());
+        gson = gsonBuilder.create();
     }
 
     public JsonDocumentSerializer(boolean reducedResponse) {
@@ -27,11 +31,9 @@ public class JsonDocumentSerializer extends DocumentSerializer {
 
     @Override
     public byte[] serialize(Document doc) {
-        log.warn("will serialize "+doc.toString());
-        final Gson gson = gsonBuilder.create();
+
         final StringWriter writer = new StringWriter();
         gson.toJson(doc,writer);
-        log.warn("serialized to "+writer.toString());
         return writer.toString().getBytes();
     }
 
