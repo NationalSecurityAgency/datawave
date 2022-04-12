7e6154e0ade17800cf3cc8aee095246719673ed5
Miner <andrew.lf.miner@gmail.com>
2022-04-12 18:16:55 +0000

    Updating based on team feedback

--- warehouse/query-core/src/main/java/datawave/query/jexl/visitors/FieldMissingFromSchemaVisitor.java
+++ warehouse/query-core/src/main/java/datawave/query/jexl/visitors/FieldMissingFromSchemaVisitor.java
@@ -5,59 +5,22 @@ import datawave.query.jexl.functions.JexlFunctionArgumentDescriptorFactory;
 import datawave.query.jexl.functions.arguments.JexlArgumentDescriptor;
 import datawave.query.util.MetadataHelper;
 import org.apache.accumulo.core.client.TableNotFoundException;
-import org.apache.commons.jexl2.parser.ASTAdditiveNode;
-import org.apache.commons.jexl2.parser.ASTAdditiveOperator;
-import org.apache.commons.jexl2.parser.ASTAmbiguous;
 import org.apache.commons.jexl2.parser.ASTAndNode;
-import org.apache.commons.jexl2.parser.ASTArrayAccess;
-import org.apache.commons.jexl2.parser.ASTArrayLiteral;
-import org.apache.commons.jexl2.parser.ASTAssignment;
-import org.apache.commons.jexl2.parser.ASTBitwiseAndNode;
-import org.apache.commons.jexl2.parser.ASTBitwiseComplNode;
-import org.apache.commons.jexl2.parser.ASTBitwiseOrNode;
-import org.apache.commons.jexl2.parser.ASTBitwiseXorNode;
-import org.apache.commons.jexl2.parser.ASTBlock;
-import org.apache.commons.jexl2.parser.ASTConstructorNode;
-import org.apache.commons.jexl2.parser.ASTDivNode;
 import org.apache.commons.jexl2.parser.ASTEQNode;
 import org.apache.commons.jexl2.parser.ASTERNode;
-import org.apache.commons.jexl2.parser.ASTEmptyFunction;
-import org.apache.commons.jexl2.parser.ASTFalseNode;
-import org.apache.commons.jexl2.parser.ASTFloatLiteral;
-import org.apache.commons.jexl2.parser.ASTForeachStatement;
 import org.apache.commons.jexl2.parser.ASTFunctionNode;
 import org.apache.commons.jexl2.parser.ASTGENode;
 import org.apache.commons.jexl2.parser.ASTGTNode;
 import org.apache.commons.jexl2.parser.ASTIdentifier;
-import org.apache.commons.jexl2.parser.ASTIfStatement;
-import org.apache.commons.jexl2.parser.ASTIntegerLiteral;
 import org.apache.commons.jexl2.parser.ASTJexlScript;
 import org.apache.commons.jexl2.parser.ASTLENode;
 import org.apache.commons.jexl2.parser.ASTLTNode;
-import org.apache.commons.jexl2.parser.ASTMapEntry;
-import org.apache.commons.jexl2.parser.ASTMapLiteral;
-import org.apache.commons.jexl2.parser.ASTMethodNode;
-import org.apache.commons.jexl2.parser.ASTModNode;
-import org.apache.commons.jexl2.parser.ASTMulNode;
 import org.apache.commons.jexl2.parser.ASTNENode;
 import org.apache.commons.jexl2.parser.ASTNRNode;
 import org.apache.commons.jexl2.parser.ASTNotNode;
-import org.apache.commons.jexl2.parser.ASTNullLiteral;
-import org.apache.commons.jexl2.parser.ASTNumberLiteral;
-import org.apache.commons.jexl2.parser.ASTOrNode;
 import org.apache.commons.jexl2.parser.ASTReference;
 import org.apache.commons.jexl2.parser.ASTReferenceExpression;
-import org.apache.commons.jexl2.parser.ASTReturnStatement;
-import org.apache.commons.jexl2.parser.ASTSizeFunction;
-import org.apache.commons.jexl2.parser.ASTSizeMethod;
-import org.apache.commons.jexl2.parser.ASTStringLiteral;
-import org.apache.commons.jexl2.parser.ASTTernaryNode;
-import org.apache.commons.jexl2.parser.ASTTrueNode;
-import org.apache.commons.jexl2.parser.ASTUnaryMinusNode;
-import org.apache.commons.jexl2.parser.ASTVar;
-import org.apache.commons.jexl2.parser.ASTWhileStatement;
 import org.apache.commons.jexl2.parser.JexlNode;
-import org.apache.commons.jexl2.parser.SimpleNode;
 import org.apache.log4j.Logger;
 
 import java.util.Collections;
@@ -74,7 +37,7 @@ import java.util.Set;
  * 2. If a datatype filter is NOT specified (null or empty), this implies ALL datatypes.
  * </pre>
  */
-public class FieldMissingFromSchemaVisitor extends BaseVisitor {
+public class FieldMissingFromSchemaVisitor extends ShortCircuitBaseVisitor {
     
     private static final Logger log = Logger.getLogger(FieldMissingFromSchemaVisitor.class);
     
@@ -197,7 +160,7 @@ public class FieldMissingFromSchemaVisitor extends BaseVisitor {
         return nonExistentFieldNames;
     }
     
-    // Do not descend through these nodes
+    // Descend through these nodes
     @Override
     public Object visit(ASTJexlScript node, Object data) {
         node.childrenAccept(this, data);
@@ -225,197 +188,6 @@ public class FieldMissingFromSchemaVisitor extends BaseVisitor {
     @Override
     public Object visit(ASTReferenceExpression node, Object data) {
         node.childrenAccept(this, data);
-        return data;
-    }
-    
-    // Short Circuit at these nodes
-    @Override
-    public Object visit(SimpleNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTBlock node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTAmbiguous node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTIfStatement node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTWhileStatement node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTForeachStatement node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTAssignment node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTTernaryNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTOrNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTBitwiseOrNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTBitwiseXorNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTBitwiseAndNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTAdditiveNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTAdditiveOperator node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTMulNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTDivNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTModNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTUnaryMinusNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTBitwiseComplNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTIdentifier node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTNullLiteral node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTTrueNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTFalseNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTIntegerLiteral node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTFloatLiteral node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTStringLiteral node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTArrayLiteral node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTMapLiteral node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTMapEntry node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTEmptyFunction node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTSizeFunction node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTMethodNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTSizeMethod node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTConstructorNode node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTArrayAccess node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTReturnStatement node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTVar node, Object data) {
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTNumberLiteral node, Object data) {
         return data;
     }
     
