d29a9edd4e3f5c0d107828ca39e5f0aad74ea288
Laura Schanno <lbschanno@gmail.com>
2022-04-22 15:49:46 -0400

    Move includeText to QueryFunctions
    The filter:includeText function is not truly evaluation-only, and as
    such, does not truly belong in EvaluationPhaseFilterFunctions. Move it
    to QueryFunctions.

--- warehouse/query-core/src/test/java/datawave/query/jexl/functions/QueryFunctionsTest.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/functions/QueryFunctionsTest.java
@@ -16,8 +16,8 @@ import java.util.function.Function;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Due to the expansive nature of {@link QueryFunctions}, tests for individual methods are encapsulated within their own test suites represented by the nested
- * classes found herein. The {@link Enclosed} runner will run all tests within these test suites.
+ * Due to the expansive nature of {@link QueryFunctions}, tests for individual methods are encapsulated within their own test suites represented
+ * by the nested classes found herein. The {@link Enclosed} runner will run all tests within these test suites.
  */
 @RunWith(Enclosed.class)
 public class QueryFunctionsTest {
@@ -85,6 +85,56 @@ public class QueryFunctionsTest {
         
         private FunctionalSet<ValueTuple> result() {
             return QueryFunctions.matchRegex(fieldValue, regex);
+        }
+    }
+    
+    
+    /**
+     * Tests for {@link QueryFunctions#includeText(Object, String)}.
+     */
+    public static class IncludeTextSingularValue {
+        
+        private Object fieldValue;
+        private String valueToMatch;
+        
+        // Verify that a null field value results in an empty set.
+        @Test
+        public void testNullValue() {
+            givenFieldValue(null);
+            givenValueToMatch("text");
+            
+            assertThat(result()).isEmpty();
+        }
+        
+        // Verify that a set containing the hit term is returned when the text exactly matches the non-normalized value.
+        @Test
+        public void testMatch() {
+            ValueTuple value = toValueTuple("FOO.1,BAR,boom");
+            givenFieldValue(value);
+            givenValueToMatch("BAR");
+            
+            assertThat(result()).containsExactly(value);
+        }
+        
+        // Verify that an empty set is returned when the text does not exactly match the non-normalized value.
+        @Test
+        public void testNoMatch() {
+            givenFieldValue(toValueTuple("FOO.1,BAR,boom"));
+            givenValueToMatch("bar");
+            
+            assertThat(result()).isEmpty();
+        }
+        
+        public void givenFieldValue(Object fieldValue) {
+            this.fieldValue = fieldValue;
+        }
+        
+        public void givenValueToMatch(String valueToMatch) {
+            this.valueToMatch = valueToMatch;
+        }
+        
+        public FunctionalSet<ValueTuple> result() {
+            return QueryFunctions.includeText(fieldValue, valueToMatch);
         }
     }
     
@@ -159,6 +209,50 @@ public class QueryFunctionsTest {
         
         private FunctionalSet<ValueTuple> result() {
             return QueryFunctions.matchRegex(fieldValues, regex);
+        }
+    }
+    
+    
+    /**
+     * Tests for {@link QueryFunctions#includeText(Iterable, String)}.
+     */
+    public static class IncludeTextIterableValue {
+        
+        private Iterable<Object> fieldValues;
+        private String valueToMatch;
+        
+        // Verify that a null iterable results in an empty set.
+        @Test
+        public void testNullIterable() {
+            fieldValues = null;
+            givenValueToMatch("FOO");
+            
+            assertThat(result()).isEmpty();
+        }
+        
+        // Verify that a set is returned only with hit terms where the non-normalized value exactly match the text.
+        @Test
+        public void testNonNullIterable() {
+            ValueTuple first = toValueTuple("FOO.1,BAR,boom");
+            ValueTuple second = toValueTuple("FOO.1,boom,BAR");
+            ValueTuple third = toValueTuple("BAR.1,foo,boom");
+            ValueTuple fourth = toValueTuple("FOO.2,BAR,bar"); // Although this is a match, only the first match should be returned.
+            givenFieldValues(first, second, third, fourth);
+            givenValueToMatch("BAR");
+            
+            assertThat(result()).containsExactly(first);
+        }
+        
+        public void givenFieldValues(Object... values) {
+            this.fieldValues = Lists.newArrayList(values);
+        }
+        
+        public void givenValueToMatch(String valueToMatch) {
+            this.valueToMatch = valueToMatch;
+        }
+        
+        public FunctionalSet<ValueTuple> result() {
+            return QueryFunctions.includeText(fieldValues, valueToMatch);
         }
     }
     
