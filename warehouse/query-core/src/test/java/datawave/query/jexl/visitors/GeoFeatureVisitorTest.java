6e4a74e040ee1aa20d32a67011b0cc0da27d1194
Miner <andrew.lf.miner@gmail.com>
2022-05-04 15:28:29 +0000

    Adding more unit tests

--- warehouse/query-core/src/test/java/datawave/query/jexl/visitors/GeoFeatureVisitorTest.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/visitors/GeoFeatureVisitorTest.java
@@ -9,6 +9,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GeoFeatureVisitorTest {
     private static final String GEO_FIELD_QUERY = "(((GEO_FIELD >= '0208' && GEO_FIELD <= '020d') || GEO_FIELD == '0202') && (geowave:intersects(GEO_FIELD, 'POLYGON((10 10, -10 10, -10 -10, 10 -10, 10 10 ))')))";
@@ -19,65 +20,74 @@ public class GeoFeatureVisitorTest {
     private static final String ALL_GEO_FIELDS_QUERY = "((" + GEO_FIELD_QUERY + " || " + GEOM_FIELD_QUERY + ")" + " && " + POINT_FIELD_QUERY + ")";
     private static final String ALL_OTHER_FIELDS_QUERY = "(" + EQ_FIELD_QUERY + " || " + ER_FIELD_QUERY + ")";
     private static final String ALL_COMBINED_FIELDS_QUERY = "(((" + GEO_FIELD_QUERY + " || " + GEOM_FIELD_QUERY + ")" + " && " + POINT_FIELD_QUERY + ") || "
-                    + "(" + EQ_FIELD_QUERY + " || " + ER_FIELD_QUERY + "))";
-    
+            + "(" + EQ_FIELD_QUERY + " || " + ER_FIELD_QUERY + "))";
+
     @Test
     public void testGeoQueryOnly() throws Exception {
         Set<QueryGeometry> geoFeatures = new LinkedHashSet<>();
-        
+
         JexlNode queryNode = JexlASTHelper.parseAndFlattenJexlQuery(GEO_FIELD_QUERY);
         geoFeatures.addAll(GeoFeatureVisitor.getGeoFeatures(queryNode));
-        
+
         assertEquals(1, geoFeatures.size());
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(GEO_FIELD, 'POLYGON((10 10, -10 10, -10 -10, 10 -10, 10 10 ))')", "{\"type\":\"Polygon\",\"coordinates\":[[[10,10],[-10,10],[-10,-10],[10,-10],[10,10]]]}")));
     }
-    
+
     @Test
     public void testGeomQueryOnly() throws Exception {
         Set<QueryGeometry> geoFeatures = new LinkedHashSet<>();
-        
+
         JexlNode queryNode = JexlASTHelper.parseAndFlattenJexlQuery(GEOM_FIELD_QUERY);
         geoFeatures.addAll(GeoFeatureVisitor.getGeoFeatures(queryNode));
-        
+
         assertEquals(1, geoFeatures.size());
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(GEOM_FIELD, 'POLYGON((-90 -90, 90 -90, 90 90, -90 90, -90 -90))')", "{\"type\":\"Polygon\",\"coordinates\":[[[-90,-90],[90,-90],[90,90],[-90,90],[-90,-90]]]}")));
     }
-    
+
     @Test
     public void testPointQueryMixed() throws Exception {
         Set<QueryGeometry> geoFeatures = new LinkedHashSet<>();
-        
+
         JexlNode queryNode = JexlASTHelper.parseAndFlattenJexlQuery(POINT_FIELD_QUERY);
         geoFeatures.addAll(GeoFeatureVisitor.getGeoFeatures(queryNode));
-        
+
         assertEquals(1, geoFeatures.size());
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(POINT_FIELD, 'POINT(0 0)')", "{\"type\":\"Point\",\"coordinates\":[0.0,0.0]}")));
     }
-    
+
     @Test
     public void testGeoQueriesOnlyMixed() throws Exception {
         Set<QueryGeometry> geoFeatures = new LinkedHashSet<>();
-        
+
         JexlNode queryNode = JexlASTHelper.parseAndFlattenJexlQuery(ALL_GEO_FIELDS_QUERY);
         geoFeatures.addAll(GeoFeatureVisitor.getGeoFeatures(queryNode));
-        
+
         assertEquals(3, geoFeatures.size());
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(GEO_FIELD, 'POLYGON((10 10, -10 10, -10 -10, 10 -10, 10 10 ))')", "{\"type\":\"Polygon\",\"coordinates\":[[[10,10],[-10,10],[-10,-10],[10,-10],[10,10]]]}")));
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(GEOM_FIELD, 'POLYGON((-90 -90, 90 -90, 90 90, -90 90, -90 -90))')", "{\"type\":\"Polygon\",\"coordinates\":[[[-90,-90],[90,-90],[90,90],[-90,90],[-90,-90]]]}")));
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(POINT_FIELD, 'POINT(0 0)')", "{\"type\":\"Point\",\"coordinates\":[0.0,0.0]}")));
     }
-    
+
     @Test
     public void testNoGeoQueries() throws Exception {
         Set<QueryGeometry> geoFeatures = new LinkedHashSet<>();
-        
+
         JexlNode queryNode = JexlASTHelper.parseAndFlattenJexlQuery(ALL_OTHER_FIELDS_QUERY);
         geoFeatures.addAll(GeoFeatureVisitor.getGeoFeatures(queryNode));
-        
+
         assertEquals(0, geoFeatures.size());
     }
-    
+
     @Test
     public void testMultipleMixedQueryTypes() throws Exception {
         Set<QueryGeometry> geoFeatures = new LinkedHashSet<>();
-        
+
         JexlNode queryNode = JexlASTHelper.parseAndFlattenJexlQuery(ALL_COMBINED_FIELDS_QUERY);
         geoFeatures.addAll(GeoFeatureVisitor.getGeoFeatures(queryNode));
-        
+
         assertEquals(3, geoFeatures.size());
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(GEO_FIELD, 'POLYGON((10 10, -10 10, -10 -10, 10 -10, 10 10 ))')", "{\"type\":\"Polygon\",\"coordinates\":[[[10,10],[-10,10],[-10,-10],[10,-10],[10,10]]]}")));
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(GEOM_FIELD, 'POLYGON((-90 -90, 90 -90, 90 90, -90 90, -90 -90))')", "{\"type\":\"Polygon\",\"coordinates\":[[[-90,-90],[90,-90],[90,90],[-90,90],[-90,-90]]]}")));
+        assertTrue(geoFeatures.contains(new QueryGeometry("geowave:intersects(POINT_FIELD, 'POINT(0 0)')", "{\"type\":\"Point\",\"coordinates\":[0.0,0.0]}")));
     }
 }
