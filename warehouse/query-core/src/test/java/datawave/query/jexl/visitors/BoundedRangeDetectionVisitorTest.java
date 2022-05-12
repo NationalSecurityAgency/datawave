4975d8ffc9a73f4367091babf3238ce684bfa65b
Miner <andrew.lf.miner@gmail.com>
2022-05-12 16:09:22 +0000

    Adding short circuiting and more unit tests

--- warehouse/query-core/src/test/java/datawave/query/jexl/visitors/BoundedRangeDetectionVisitorTest.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/visitors/BoundedRangeDetectionVisitorTest.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Multimap;
 import datawave.data.type.StringType;
 import datawave.data.type.Type;
 import datawave.query.config.ShardQueryConfiguration;
+import datawave.query.exceptions.DatawaveFatalQueryException;
 import datawave.query.jexl.JexlASTHelper;
 import datawave.query.util.MockMetadataHelper;
 import org.apache.commons.jexl2.parser.ASTJexlScript;
@@ -12,31 +13,44 @@ import org.apache.commons.jexl2.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class BoundedRangeDetectionVisitorTest {
     private ShardQueryConfiguration config;
+    private MockMetadataHelper helper;
     
     @Before
     public void init() throws Exception {
         this.config = ShardQueryConfiguration.create();
-    }
-    
-    @Test
-    public void testBoundedRange() throws ParseException {
         Multimap<String,Type<?>> queryFieldsDatatypes = HashMultimap.create();
         queryFieldsDatatypes.put("foo", new StringType());
         
         config.setQueryFieldsDatatypes(queryFieldsDatatypes);
         
-        MockMetadataHelper helper = new MockMetadataHelper();
-        helper.setNonEventFields(queryFieldsDatatypes.keySet());
-        
+        this.helper = new MockMetadataHelper();
+        this.helper.setNonEventFields(queryFieldsDatatypes.keySet());
+    }
+    
+    @Test
+    public void testBoundedRange() throws ParseException {
         String queryString = "((_Bounded_ = true) && (foo >= '1' && foo <= '10'))";
         ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
-        boolean foo = BoundedRangeDetectionVisitor.mustExpandBoundedRange(config, helper, script);
-        
-        assertTrue(foo);
+        assertTrue(BoundedRangeDetectionVisitor.mustExpandBoundedRange(config, helper, script));
+    }
+    
+    @Test
+    public void testUnboundedRange() throws ParseException {
+        String queryString = "(foo == '1' && foo == '10')";
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
+        assertFalse(BoundedRangeDetectionVisitor.mustExpandBoundedRange(config, helper, script));
+    }
+    
+    @Test(expected = DatawaveFatalQueryException.class)
+    public void testBoundedRangeMalformed() throws ParseException {
+        String queryString = "((_Bounded_ = true) && (foo == '1' && foo == '10'))";
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
+        BoundedRangeDetectionVisitor.mustExpandBoundedRange(config, helper, script);
     }
     
 }
