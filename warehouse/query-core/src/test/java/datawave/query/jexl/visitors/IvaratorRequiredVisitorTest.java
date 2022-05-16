0cf8658552d8eeb202e2429435648fb9de08cb29
Miner <andrew.lf.miner@gmail.com>
2022-05-16 16:30:48 +0000

    Adding unit tests

---
+++ warehouse/query-core/src/test/java/datawave/query/jexl/visitors/IvaratorRequiredVisitorTest.java
@@ -0,0 +1,40 @@
+package datawave.query.jexl.visitors;
+
+import datawave.query.jexl.JexlASTHelper;
+import org.apache.commons.jexl2.parser.ASTJexlScript;
+import org.apache.commons.jexl2.parser.ParseException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class IvaratorRequiredVisitorTest {
+
+    @Test
+    public void testExceededOrThresholdTrue() throws ParseException {
+        String query = "((_List_ = true) && (FOO == 1 && FOO == 3))";
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
+        assertTrue(IvaratorRequiredVisitor.isIvaratorRequired(script));
+    }
+
+    @Test
+    public void testExceededOrThresholdFalse() throws ParseException {
+        String query = "(FOO == 1 && FOO == 3)";
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
+        assertFalse(IvaratorRequiredVisitor.isIvaratorRequired(script));
+    }
+
+    @Test
+    public void testExceededValueThresholdTrue() throws ParseException {
+        String query = "((_Value_ = true) && (FOO == 1 && FOO == 3))";
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
+        assertTrue(IvaratorRequiredVisitor.isIvaratorRequired(script));
+    }
+
+    @Test
+    public void testExceededValueThresholdFalse() throws ParseException {
+        String query = "(FOO == 1 && FOO == 3)";
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
+        assertFalse(IvaratorRequiredVisitor.isIvaratorRequired(script));
+    }
+}
