e1ae1f4a2a722a716a699a55dadf423ead682315
Whitney O'Meara <james.w.omeara@gmail.com>
2022-05-25 13:55:23 -0400

    removed the query starter

--- microservices/starters/query/src/main/java/datawave/microservice/query/messaging/config/QueryMessagingConfig.java
+++
@@ -1,8 +0,0 @@
-package datawave.microservice.query.messaging.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(MessagingProperties.class)
-public class QueryMessagingConfig {}
