e28bc32c488c3a17ee33fc88e0032ab8a176108c
Miner <andrew.lf.miner@gmail.com>
2022-05-26 17:47:50 +0000

    Removing unnecessary unit test

--- warehouse/query-core/src/test/java/datawave/query/jexl/visitors/IvaratorRequiredVisitorTest.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/visitors/IvaratorRequiredVisitorTest.java
@@ -9,32 +9,26 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class IvaratorRequiredVisitorTest {
-    
+
     @Test
     public void testExceededOrThresholdTrue() throws ParseException {
         String query = "((_List_ = true) && (FOO == 1 && FOO == 3))";
         ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
         assertTrue(IvaratorRequiredVisitor.isIvaratorRequired(script));
     }
-    
+
     @Test
-    public void testExceededOrThresholdFalse() throws ParseException {
+    public void testExceededThresholdFalse() throws ParseException {
         String query = "(FOO == 1 && FOO == 3)";
         ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
         assertFalse(IvaratorRequiredVisitor.isIvaratorRequired(script));
     }
-    
+
     @Test
     public void testExceededValueThresholdTrue() throws ParseException {
-        String query = "((_Value_ = true) && (FOO == 1 && FOO == 3))";
+        String query = "((_Value_ = true) && (FOO == 1 || FOO == 3))";
         ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
         assertTrue(IvaratorRequiredVisitor.isIvaratorRequired(script));
     }
     
-    @Test
-    public void testExceededValueThresholdFalse() throws ParseException {
-        String query = "(FOO == 1 && FOO == 3)";
-        ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
-        assertFalse(IvaratorRequiredVisitor.isIvaratorRequired(script));
-    }
 }
