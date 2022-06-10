933a7e67ea13120fbd817c00461fa01f67b75d8b
Ivan Bella <ivan@bella.name>
2022-06-10 19:29:01 +0000

    Updated to use docker compose (without the dash)

--- docker/restart.sh
+++ docker/restart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 services=$@
 for service in $services; do
-  docker-compose stop $service
+  docker compose stop $service
 done
-docker-compose rm -f
-docker-compose up -d
+docker compose rm -f
+docker compose up -d
