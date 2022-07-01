15966911096febd4643ab020c125693fe3260f69
Kevin Rathbun <kevinrr888@gmail.com>
2022-07-01 15:55:53 -0400

    Removed sed commands from script

--- warehouse/ingest-scripts/src/main/resources/bin/util/prettyPrintQuery.sh
+++ warehouse/ingest-scripts/src/main/resources/bin/util/prettyPrintQuery.sh
@@ -10,12 +10,6 @@ findJars () {
     ls -1 ../../lib/$1-*.jar | sort | paste -sd ':' -
 }
 
-# Delete line 4 of ./ingest-libs.sh: ". ../ingest/ingest-env.sh"
-sed -i '4d' ./ingest-libs.sh
-
 . ./ingest-libs.sh
-
-# Add ". ../ingest/ingest-env.sh" back on line 4 of ./ingest-libs.sh
-sed -i '4 i . ../ingest/ingest-env.sh' ./ingest-libs.sh
 
 java -cp $CLASSPATH:$(findJars jackson) datawave.query.jexl.visitors.JexlFormattedStringBuildingVisitor
