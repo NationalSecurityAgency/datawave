627b01cc2e7745fabbb49d8185ee99e2a02d0084
Miner <andrew.lf.miner@gmail.com>
2022-08-03 20:02:19 +0000

    Updating based on team feedback

--- warehouse/query-core/src/main/java/datawave/query/jexl/visitors/ValidPatternVisitor.java
+++ warehouse/query-core/src/main/java/datawave/query/jexl/visitors/ValidPatternVisitor.java
@@ -5,12 +5,9 @@ import datawave.query.jexl.JexlASTHelper;
 import datawave.query.jexl.functions.FunctionJexlNodeVisitor;
 import datawave.query.jexl.functions.JexlFunctionArgumentDescriptorFactory;
 import datawave.query.jexl.functions.arguments.JexlArgumentDescriptor;
-import org.apache.commons.jexl2.parser.ASTAndNode;
 import org.apache.commons.jexl2.parser.ASTERNode;
 import org.apache.commons.jexl2.parser.ASTFunctionNode;
-import org.apache.commons.jexl2.parser.ASTJexlScript;
 import org.apache.commons.jexl2.parser.ASTNRNode;
-import org.apache.commons.jexl2.parser.ASTReference;
 import org.apache.commons.jexl2.parser.ASTStringLiteral;
 import org.apache.commons.jexl2.parser.JexlNode;
 
@@ -127,25 +124,6 @@ public class ValidPatternVisitor extends ShortCircuitBaseVisitor {
                 throw new PatternSyntaxException(errMsg, e.getPattern(), e.getIndex());
             }
         }
-    }
-    
-    // Descend through these nodes
-    @Override
-    public Object visit(ASTJexlScript node, Object data) {
-        node.childrenAccept(this, data);
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTAndNode node, Object data) {
-        node.childrenAccept(this, data);
-        return data;
-    }
-    
-    @Override
-    public Object visit(ASTReference node, Object data) {
-        node.childrenAccept(this, data);
-        return data;
     }
     
 }
