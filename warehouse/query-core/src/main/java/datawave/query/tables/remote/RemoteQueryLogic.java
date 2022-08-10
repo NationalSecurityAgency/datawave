614931cae52408be98da6943cd847d750a445840
Ivan Bella <ivan@bella.name>
2022-08-10 21:10:50 +0000

    re #1658: Added a remote query service * Added a remote query service to allow the webserver to call through to   the query microservices * Added a remote query logic that can invoke a remote query service using   an injected RemoteQueryService.

---
+++ warehouse/query-core/src/main/java/datawave/query/tables/remote/RemoteQueryLogic.java
@@ -0,0 +1,7 @@
+package datawave.query.tables.remote;
+
+import datawave.webservice.common.remote.RemoteQueryService;
+
+public interface RemoteQueryLogic {
+    public void setRemoteQueryService(RemoteQueryService service);
+}
---
+++ web-services/common/src/main/java/datawave/webservice/common/remote/RemoteQueryService.java
@@ -0,0 +1,21 @@
+package datawave.webservice.common.remote;
+
+import datawave.webservice.result.BaseQueryResponse;
+import datawave.webservice.result.GenericResponse;
+import datawave.webservice.result.VoidResponse;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RemoteQueryService {
+    
+    public GenericResponse<String> createQuery(String queryLogicName, Map<String,List<String>> queryParameters, Object callerObject);
+    
+    public BaseQueryResponse next(String id, Object callerObject);
+    
+    public VoidResponse close(String id, Object callerObject);
+    
+    public GenericResponse<String> planQuery(String queryLogicName, Map<String,List<String>> queryParameters, Object callerObject);
+    
+    public GenericResponse<String> planQuery(String id, Object callerObject);
+}
