0070d3d9fddae03af11fa8f5557afdefb1ba29d4
Miner <andrew.lf.miner@gmail.com>
2022-08-29 14:13:03 +0000

    Refactoring unit tests

--- warehouse/query-core/src/test/java/datawave/query/jexl/visitors/LiteralNodeSubsetVisitorTest.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/visitors/LiteralNodeSubsetVisitorTest.java
@@ -1,5 +1,6 @@
 package datawave.query.jexl.visitors;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 import datawave.query.jexl.JexlASTHelper;
 import org.apache.commons.jexl2.parser.ASTJexlScript;
@@ -7,106 +8,112 @@ import org.apache.commons.jexl2.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LiteralNodeSubsetVisitorTest {
     
-    private final Set<String> expectedLiterals = new HashSet<>(Arrays.asList("FOO", "BAR", "BAZ", "ABCD", "1234", "1b3d"));
-    
     @Test
     public void testSingleLiteralWithMatch() throws ParseException {
         String queryString = "FOO == '1234'";
-        Multimap<String,String> literals = getLiterals(queryString);
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(1, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
+        literals.put("FOO", "1234");
+        test(queryString, literals);
     }
     
     @Test
     public void testSingleLiteralWithoutMatch() throws ParseException {
         String queryString = "RANDOMFIELD == '10000000'";
-        Set<String> expectedLiterals = new HashSet<>();
-        ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
-        Multimap<String,String> literals = LiteralNodeSubsetVisitor.getLiterals(expectedLiterals, script);
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(0, literals.size());
+        test(queryString, literals);
     }
     
     @Test
     public void testMultipleLiteralsAllMatch() throws ParseException {
-        String queryString = "FOO == '1234' || BAR == 'ABCD' && BAZ == '1b3d'";
-        Multimap<String,String> literals = getLiterals(queryString);
+        String queryString = "FOO == '1234' || BAR == 'abcd' && BAZ == '1b3d'";
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(3, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
-        Assert.assertTrue(literals.containsEntry("BAR", "ABCD"));
-        Assert.assertTrue(literals.containsEntry("BAZ", "1b3d"));
+        literals.put("FOO", "1234");
+        literals.put("BAR", "abcd");
+        literals.put("BAZ", "1b3d");
+        test(queryString, literals);
     }
     
     @Test
     public void testMultipleLiteralsMixedMatch() throws ParseException {
         String queryString = "FOO == '1234' || FLARP == '9001' && BAZ == '1b3d'";
-        Multimap<String,String> literals = getLiterals(queryString);
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(2, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
-        Assert.assertTrue(literals.containsEntry("BAZ", "1b3d"));
+        literals.put("FOO", "1234");
+        literals.put("BAZ", "1b3d");
+        test(queryString, literals);
     }
     
     @Test
     public void testMultipleLiteralsNoMatch() throws ParseException {
-        String queryString = "YO == 'HO' || YO == 'HO' && A == 'PIRATESLIFEFORME'";
+        String queryString = "YO == 'ho' || YO == 'ho' && A == 'pirateslifeforme'";
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(0, getLiterals(queryString).size());
+        test(queryString, literals);
     }
     
     @Test
     public void testSingleLiteralWithMatchAndRegex() throws ParseException {
         String queryString = "FOO == '1234' || BAR =~ 'abcd.*\\d'";
-        Multimap<String,String> literals = getLiterals(queryString);
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(1, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
+        literals.put("FOO", "1234");
+        test(queryString, literals);
     }
     
     @Test
     public void testSingleLiteralWithNoMatchAndRegex() throws ParseException {
         String queryString = "DIFFERENTFIELD == '1000000' || BAR =~ 'abcd.*\\d'";
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(0, getLiterals(queryString).size());
+        test(queryString, literals);
     }
     
     @Test
     public void testMultipleLiteralsAllMatchAndRegex() throws ParseException {
         String queryString = "FOO == '1234' && BAR == 'abcd' || BAZ =~ 'abcd.*\\d'";
-        Multimap<String,String> literals = getLiterals(queryString);
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(2, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
-        Assert.assertTrue(literals.containsEntry("BAR", "abcd"));
+        literals.put("FOO", "1234");
+        literals.put("BAR", "abcd");
+        test(queryString, literals);
     }
     
     @Test
     public void testMultipleLiteralsMixedMatchAndRegex() throws ParseException {
-        String queryString = "FOO == '1234' && FLIM == 'FLAM' || BAZ =~ 'abcd.*\\d'";
+        String queryString = "FOO == '1234' && FLIM == 'flam' || BAZ =~ 'abcd.*\\d'";
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(1, getLiterals(queryString).size());
-        Assert.assertTrue(getLiterals(queryString).containsEntry("FOO", "1234"));
+        literals.put("FOO", "1234");
+        test(queryString, literals);
     }
     
     @Test
     public void testMultipleLiteralsNoMatchAndRegex() throws ParseException {
         String queryString = "FLIM == 'flam' && PARA == 'diddle' || CHEESE =~ 'ka.*\\d'";
+        Multimap<String,String> literals = ArrayListMultimap.create();
         
-        Assert.assertEquals(0, getLiterals(queryString).size());
+        test(queryString, literals);
     }
     
-    private Multimap<String,String> getLiterals(String queryString) throws ParseException {
-        ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
+    private void test(String query, Multimap<String,String> expected) throws ParseException {
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
+        Set<String> expectedLiterals = new HashSet<>(expected.keySet());
+        expectedLiterals.addAll(expected.values());
+        Multimap<String,String> actualLiterals = LiteralNodeSubsetVisitor.getLiterals(expectedLiterals, script);
         
-        return LiteralNodeSubsetVisitor.getLiterals(expectedLiterals, script);
+        Assert.assertEquals(expected.size(), actualLiterals.size());
+        for (Map.Entry<String,String> entry : expected.entries()) {
+            Assert.assertTrue(actualLiterals.containsEntry(entry.getKey(), entry.getValue()));
+        }
     }
     
 }
--- warehouse/query-core/src/test/java/datawave/query/jexl/visitors/LiteralNodeVisitorTest.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/visitors/LiteralNodeVisitorTest.java
@@ -1,5 +1,6 @@
 package datawave.query.jexl.visitors;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 import datawave.query.jexl.JexlASTHelper;
 import org.apache.commons.jexl2.parser.ASTJexlScript;
@@ -7,49 +8,61 @@ import org.apache.commons.jexl2.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class LiteralNodeVisitorTest {
-    
+
     @Test
     public void testSingleLiteral() throws ParseException {
         String queryString = "FOO == '1234'";
-        ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
-        Multimap<String,String> literals = LiteralNodeVisitor.getLiterals(script);
-        
-        Assert.assertEquals(1L, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
+        Multimap<String, String> literals = ArrayListMultimap.create();
+
+        literals.put("FOO", "1234");
+        test(queryString, literals);
     }
-    
+
     @Test
     public void testMultipleLiterals() throws ParseException {
         String queryString = "FOO == '1234' || BAR == 'ABCD' && BAZ == '1b3d'";
-        ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
-        Multimap<String,String> literals = LiteralNodeVisitor.getLiterals(script);
-        
-        Assert.assertEquals(3L, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
-        Assert.assertTrue(literals.containsEntry("BAR", "ABCD"));
-        Assert.assertTrue(literals.containsEntry("BAZ", "1b3d"));
+        Multimap<String, String> literals = ArrayListMultimap.create();
+
+        literals.put("FOO", "1234");
+        literals.put("BAR", "abcd");
+        literals.put("BAZ", "1b3d");
+        test(queryString, literals);
     }
-    
+
     @Test
     public void testSingleLiteralAndRegex() throws ParseException {
         String queryString = "FOO == '1234' || BAR =~ 'abcd.*\\d'";
-        ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
-        Multimap<String,String> literals = LiteralNodeVisitor.getLiterals(script);
-        
-        Assert.assertEquals(1L, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
+        Multimap<String, String> literals = ArrayListMultimap.create();
+
+        literals.put("FOO", "1234");
+        test(queryString, literals);
     }
-    
+
     @Test
     public void testMultipleLiteralsAndRegex() throws ParseException {
         String queryString = "FOO == '1234' && BAR == 'abcd' || BAZ =~ 'abcd.*\\d'";
-        ASTJexlScript script = JexlASTHelper.parseJexlQuery(queryString);
-        Multimap<String,String> literals = LiteralNodeVisitor.getLiterals(script);
-        
-        Assert.assertEquals(2L, literals.size());
-        Assert.assertTrue(literals.containsEntry("FOO", "1234"));
-        Assert.assertTrue(literals.containsEntry("BAR", "abcd"));
+        Multimap<String, String> literals = ArrayListMultimap.create();
+
+        literals.put("FOO", "1234");
+        literals.put("BAR", "abcd");
+        test(queryString, literals);
     }
-    
+
+    private void test(String query, Multimap<String, String> expected) throws ParseException {
+        ASTJexlScript script = JexlASTHelper.parseJexlQuery(query);
+        Set<String> expectedLiterals = new HashSet<>(expected.keySet());
+        expectedLiterals.addAll(expected.values());
+        Multimap<String, String> actualLiterals = LiteralNodeVisitor.getLiterals(script);
+
+        Assert.assertEquals(expected.size(), actualLiterals.size());
+        for (Map.Entry<String, String> entry : expected.entries()) {
+            Assert.assertTrue(actualLiterals.containsEntry(entry.getKey(), entry.getValue()));
+        }
+    }
+
 }
