d2411017b841882eaef7391ba359f97a60e7bd1e
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2022-11-18 15:36:44 +0000

    detect and log invalid tf keys

--- warehouse/query-core/src/main/java/datawave/core/iterators/key/TFKey.java
+++ warehouse/query-core/src/main/java/datawave/core/iterators/key/TFKey.java
@@ -1,6 +1,5 @@
 package datawave.core.iterators.key;
 
-import org.apache.accumulo.core.data.ArrayByteSequence;
 import org.apache.accumulo.core.data.Key;
 
 import java.util.ArrayList;
@@ -18,7 +17,7 @@ public class TFKey {
     private String uidAndValue;
     
     private byte[] backing;
-    private ArrayList<Integer> nulls;
+    private final ArrayList<Integer> nulls = new ArrayList<>();
     
     public void parse(Key k) {
         this.datatype = null;
@@ -27,7 +26,7 @@ public class TFKey {
         this.field = null;
         this.uidAndValue = null;
         
-        this.nulls = new ArrayList<>();
+        this.nulls.clear();
         this.backing = k.getColumnQualifierData().getBackingArray();
         
         // Find all possible split points
@@ -38,11 +37,9 @@ public class TFKey {
     }
     
     public String getDatatype() {
-        if (datatype == null) {
-            if (nulls.size() > 1) {
-                int stop = nulls.get(0);
-                datatype = new String(backing, 0, stop);
-            }
+        if (datatype == null && nulls.size() > 1) {
+            int stop = nulls.get(0);
+            datatype = new String(backing, 0, stop);
         }
         return datatype;
     }
@@ -62,30 +59,30 @@ public class TFKey {
     }
     
     public String getValue() {
-        if (value == null) {
-            if (nulls.size() >= 3) {
-                int start = nulls.get(1) + 1;
-                int stop = nulls.get(nulls.size() - 1);
-                value = new String(backing, start, (stop - start));
-            }
+        if (value == null && nulls.size() >= 3) {
+            int start = nulls.get(1) + 1;
+            int stop = nulls.get(nulls.size() - 1);
+            value = new String(backing, start, (stop - start));
         }
         return value;
     }
     
     public String getField() {
-        if (field == null) {
-            if (nulls.size() >= 3) {
-                int start = nulls.get(nulls.size() - 1) + 1;
-                field = new String(backing, start, (backing.length - start));
-            }
+        if (field == null && nulls.size() >= 3) {
+            int start = nulls.get(nulls.size() - 1) + 1;
+            field = new String(backing, start, (backing.length - start));
         }
         return field;
     }
     
     public String getUidAndValue() {
-        if (uidAndValue == null) {
+        if (uidAndValue == null && getUid() != null && getValue() != null) {
             uidAndValue = getUid() + '\u0000' + getValue();
         }
         return uidAndValue;
+    }
+    
+    public boolean isValid() {
+        return backing != null && nulls.size() >= 3;
     }
 }
