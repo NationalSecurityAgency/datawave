160a554500ebdaf099cb46ed579c02feff3af98f
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2022-12-14 15:30:39 +0000

    Expand tests to include child uids and global index keys

--- warehouse/query-core/src/test/java/datawave/query/data/parsers/DatawaveKeyTest.java
+++ warehouse/query-core/src/test/java/datawave/query/data/parsers/DatawaveKeyTest.java
@@ -22,7 +22,7 @@ public class DatawaveKeyTest {
     private DatawaveKey parser;
     
     @Test
-    public void parseShardDocKey() {
+    public void testEventKey() {
         Key key = new Key(row, "datatype\u0000uid", "fieldName\u0000fieldValue", cv);
         parse(key);
         
@@ -36,7 +36,21 @@ public class DatawaveKeyTest {
     }
     
     @Test
-    public void parseShardDocKeyWithNull() {
+    public void testEventKeyWithChildUid() {
+        Key key = new Key(row, "datatype\u0000uid.1", "fieldName\u0000fieldValue", cv);
+        parse(key);
+        
+        assertInvalid(false);
+        assertType(EVENT);
+        assertRow(row);
+        assertDataType(datatype);
+        assertUid("uid.1");
+        assertFieldName(fieldName);
+        assertFieldValue(fieldValue);
+    }
+    
+    @Test
+    public void testShardKeyWithExtraNulls() {
         Key key = new Key(row, "datatype\u0000uid", "fieldName\u0000blah\u0000fieldValue", cv);
         parse(key);
         
@@ -50,7 +64,7 @@ public class DatawaveKeyTest {
     }
     
     @Test
-    public void parseShardFiKeyWithNull() {
+    public void testFiKeyWithExtraNulls() {
         Key key = new Key(row, "fi\u0000fieldName", "fieldValue\u0000blah\u0000datatype\u0000uid", cv);
         parse(key);
         
@@ -64,7 +78,7 @@ public class DatawaveKeyTest {
     }
     
     @Test
-    public void parseShardFiKey() {
+    public void testFiKey() {
         Key key = new Key(row, "fi\u0000fieldName", "fieldValue\u0000datatype\u0000uid", cv);
         parse(key);
         
@@ -78,14 +92,28 @@ public class DatawaveKeyTest {
     }
     
     @Test
-    public void parseShardFiKeyInvalid() {
+    public void testFiKeyWithChildUid() {
+        Key key = new Key(row, "fi\u0000fieldName", "fieldValue\u0000datatype\u0000uid.1", cv);
+        parse(key);
+        
+        assertInvalid(false);
+        assertType(INDEX_EVENT);
+        assertRow(row);
+        assertDataType(datatype);
+        assertUid("uid.1");
+        assertFieldName(fieldName);
+        assertFieldValue(fieldValue);
+    }
+    
+    @Test
+    public void testInvalidFiKey() {
         Key key = new Key(row, "fi\u0000fieldName", "fieldValue\u0000datatype", cv);
         parse(key);
         assertInvalid(true);
     }
     
     @Test
-    public void parseShardTfKey() {
+    public void testTermFrequencyKey() {
         Key key = new Key(row, "tf", "datatype\u0000uid\u0000fieldValue\u0000fieldName", cv);
         parse(key);
         
@@ -99,7 +127,21 @@ public class DatawaveKeyTest {
     }
     
     @Test
-    public void parseShardTfKey_valueHasNulls() {
+    public void testTermFrequencyKeyWithChildUid() {
+        Key key = new Key(row, "tf", "datatype\u0000uid.1\u0000fieldValue\u0000fieldName", cv);
+        parse(key);
+        
+        assertInvalid(false);
+        assertType(TERM_OFFSETS);
+        assertRow(row);
+        assertDataType(datatype);
+        assertUid("uid.1");
+        assertFieldName(fieldName);
+        assertFieldValue(fieldValue);
+    }
+    
+    @Test
+    public void testTermFrequencyKeyWithExtraNulls() {
         Key key = new Key(row, "tf", "datatype\u0000uid\u0000fi\u0000eldVa\u0000lue\u0000fieldName", cv);
         parse(key);
         
@@ -110,6 +152,34 @@ public class DatawaveKeyTest {
         assertUid(uid);
         assertFieldName(fieldName);
         assertFieldValue("fi\0eldVa\0lue");
+    }
+    
+    @Test
+    public void testGlobalIndexKey() {
+        Key key = new Key("fieldValue", "fieldName", "row\0datatype");
+        parse(key);
+        
+        assertInvalid(false);
+        assertType(KeyType.INDEX);
+        assertRow(fieldValue);
+        assertDataType(datatype);
+        assertUid(null);
+        assertFieldName(fieldName);
+        assertFieldValue(fieldValue);
+    }
+    
+    @Test
+    public void testGlobalIndexKeyWithNulls() {
+        Key key = new Key("fie\0ldVa\0lue", "fieldName", "row\0datatype");
+        parse(key);
+        
+        assertInvalid(false);
+        assertType(KeyType.INDEX);
+        assertRow("fie\0ldVa\0lue");
+        assertDataType(datatype);
+        assertUid(null);
+        assertFieldName(fieldName);
+        assertFieldValue("fie\0ldVa\0lue");
     }
     
     private void parse(Key key) {
