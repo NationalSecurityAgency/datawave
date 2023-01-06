616f26b0cd5051fff3459ee4f9821d3980c972dd
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2023-01-06 09:35:05 -0500

    remove instances of junit.framework, prefer static access instead of qualified asserts (#1807)

--- warehouse/query-core/src/test/java/datawave/query/jexl/NodeTypeCountTest.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/NodeTypeCountTest.java
@@ -10,9 +10,9 @@ import org.junit.Test;
 
 import java.util.Set;
 
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class NodeTypeCountTest {
     
