e8b862161c035cdd55bbf1f6422174cef427fb44
Adam Lerman awlerma <awlerma@uwe.nsa.gov>
2023-03-08 06:34:10 -0500

    Needed changes and update raw data

--- minikube_helm/wipe_and_restart.sh
+++
@@ -1,4 +0,0 @@
-minikube delete --all --purge && \
-minikube start --cpus 8 --memory 30960 --disk-size 20480 --insecure-registry="containeryard.evoforge.org" && \
-minikube image load busybox:1.28
-minikube addons enable ingress 
