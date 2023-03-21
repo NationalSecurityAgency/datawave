20e62587375923d0c0d8f1a8af6332892d2802ab
Bill Oley <bill.oley@gmail.com>
2023-03-21 09:22:18 -0400

    re #1894 Reduce log level in RemoteHttpService.execute() call

--- web-services/common/src/main/java/datawave/webservice/common/remote/RemoteHttpService.java
+++ web-services/common/src/main/java/datawave/webservice/common/remote/RemoteHttpService.java
@@ -116,7 +116,9 @@ public abstract class RemoteHttpService {
     }
     
     protected <T> T execute(HttpRequestBase request, IOFunction<T> resultConverter, Supplier<String> errorSupplier) throws IOException {
-        log.info("Executing " + request.getClass().getSimpleName() + " against " + request.getURI());
+        if (log.isTraceEnabled()) {
+            log.trace("Executing " + request.getClass().getSimpleName() + " against " + request.getURI());
+        }
         try {
             activeExecutions.incrementAndGet();
             return client.execute(
