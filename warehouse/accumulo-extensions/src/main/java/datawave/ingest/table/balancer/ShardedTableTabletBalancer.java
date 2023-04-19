fad2a870c4f57d6c429e1e5b2323c3619e2f8aaf
Christopher Gross cogross <christopher.gross@red-drum.com>
2023-04-19 18:04:34 +0000

    fixes from merge

--- warehouse/accumulo-extensions/src/main/java/datawave/ingest/table/balancer/ShardedTableTabletBalancer.java
+++ warehouse/accumulo-extensions/src/main/java/datawave/ingest/table/balancer/ShardedTableTabletBalancer.java
@@ -82,15 +82,16 @@ public class ShardedTableTabletBalancer extends GroupBalancer {
     @Override
     protected int getMaxMigrations() {
         int maxMigrations = MAX_MIGRATIONS_DEFAULT;
-        String maxMigrationsProp = getTableConfiguration().get(SHARDED_MAX_MIGRATIONS);
         try {
+            String maxMigrationsProp = getTableConfiguration().get(SHARDED_MAX_MIGRATIONS);
             if (maxMigrationsProp != null && !maxMigrationsProp.isEmpty()) {
-                maxMigrations = Integer.parseInt(maxMigrationsProp);
+                try {
+                    maxMigrations = Integer.parseInt(maxMigrationsProp);
+                } catch (Exception e) {
+                    log.error("Unable to parse " + SHARDED_MAX_MIGRATIONS + " value (" + maxMigrationsProp + ") as an integer.  Defaulting to " + maxMigrations);
+                }
             }
         } catch (Exception e) {
-            if (e instanceof NumberFormatException) {
-                log.error("Unable to parse " + SHARDED_MAX_MIGRATIONS + " value (" + maxMigrationsProp + ") as an integer.");
-            }
             log.warn("Failed to get " + SHARDED_MAX_MIGRATIONS + ".  Defaulting to " + maxMigrations, e);
         }
         return maxMigrations;
