169fbe8d94e477bd126761a1ffdd66fbf8b6e551
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2023-04-24 14:02:24 +0000

    updates from pr feedback

--- warehouse/ingest-core/src/main/java/datawave/ingest/table/aggregator/KeepCountOnlyNoUidAggregator.java
+++ warehouse/ingest-core/src/main/java/datawave/ingest/table/aggregator/KeepCountOnlyNoUidAggregator.java
@@ -1,25 +1,13 @@
 package datawave.ingest.table.aggregator;
 
 /**
- * This is like the {@link KeepCountOnlyUidAggregator} except it never preserves the uid list.
+ * This is similar to {@link KeepCountOnlyUidAggregator} except it never preserves the uid list.
  * <p>
- * An extension of {@link GlobalIndexUidAggregator} that will always preserve "count only" Uid.List protobufs, even when their count goes below 1. A count only
- * protobuf is one where we have dropped the actual uid list due to exceeding the max list size. These protobufs will have their 'ignore' flag set.
- * <p>
- * The intent of this iterator is to prevent accidental deletion of count only protobufs in case extra deletions occur.
+ * Practically this version does not require additional configuration.
  */
-public class KeepCountOnlyNoUidAggregator extends GlobalIndexUidAggregator {
-    
-    public KeepCountOnlyNoUidAggregator(int max) {
-        super(0);
-    }
+public class KeepCountOnlyNoUidAggregator extends KeepCountOnlyUidAggregator {
     
     public KeepCountOnlyNoUidAggregator() {
         super(0);
-    }
-    
-    @Override
-    public boolean propogateKey() {
-        return isSeenIgnore() || super.propogateKey();
     }
 }
