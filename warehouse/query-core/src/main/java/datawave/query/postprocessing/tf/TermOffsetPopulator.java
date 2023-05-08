e0c1151784c56e2cfb331e02a21343c24b8c2550
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2023-05-08 11:07:45 +0000

    correct location of variable declaration, remove unused nested class

--- warehouse/query-core/src/main/java/datawave/query/postprocessing/tf/TermOffsetPopulator.java
+++ warehouse/query-core/src/main/java/datawave/query/postprocessing/tf/TermOffsetPopulator.java
@@ -18,7 +18,6 @@ import datawave.query.Constants;
 import datawave.query.attributes.Content;
 import datawave.query.attributes.Document;
 import datawave.query.jexl.visitors.LiteralNodeSubsetVisitor;
-import datawave.util.StringUtils;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.PartialKey;
 import org.apache.accumulo.core.data.Range;
@@ -155,31 +154,26 @@ public class TermOffsetPopulator {
             evaluationFilter.startNewDocument(docKey);
         }
 
-        Key key;
-        Content attr;
         TermFrequencyKey parser = new TermFrequencyKey();
-        TermWeight.Info twInfo;
-        TermFrequencyList.Zone twZone;
         TermWeightPosition.Builder position = new TermWeightPosition.Builder();
         Map<String,TermFrequencyList> termOffsetMap = Maps.newHashMap();
 
         while (tfSource.hasTop()) {
-            key = tfSource.getTopKey();
+            Key key = tfSource.getTopKey();
             parser.parse(key);
             
             // add the zone and term to our internal document.
-            // TODO: lift the keep logic from the filter
-            attr = new Content(parser.getValue(), source.getTopKey(), evaluationFilter == null || evaluationFilter.keep(key));
+            Content attr = new Content(parser.getValue(), source.getTopKey(), evaluationFilter == null || evaluationFilter.keep(key));
 
             this.document.put(parser.getField(), attr);
             
             TreeMultimap<TermFrequencyList.Zone,TermWeightPosition> offsets = TreeMultimap.create();
             try {
-                twInfo = TermWeight.Info.parseFrom(tfSource.getTopValue().get());
+                TermWeight.Info twInfo = TermWeight.Info.parseFrom(tfSource.getTopValue().get());
                 
                 // if no content expansion fields then assume every field is permitted for unfielded content functions
-                boolean b = contentExpansionFields == null || contentExpansionFields.isEmpty() || contentExpansionFields.contains(parser.getField());
-                twZone = new TermFrequencyList.Zone(parser.getField(), b, TermFrequencyList.getEventId(key));
+                boolean isContentExpansionField = contentExpansionFields == null || contentExpansionFields.contains(parser.getField());
+                TermFrequencyList.Zone twZone = new TermFrequencyList.Zone(parser.getField(), isContentExpansionField, TermFrequencyList.getEventId(key));
 
                 for (int i = 0; i < twInfo.getTermOffsetCount(); i++) {
                     position.setTermWeightOffsetInfo(twInfo, i);
@@ -398,131 +392,5 @@ public class TermOffsetPopulator {
             }
         }
         return termFrequencyFieldValues;
-    }
-    
-    /**
-     * A field name and value which is sorted on {@code <value>\0<name>}
-     */
-    public static class FieldValue implements Comparable<FieldValue> {
-        private int nullOffset;
-        private String valueField;
-        
-        public FieldValue(String field, String value) {
-            this.nullOffset = value.length();
-            this.valueField = value + '\0' + field;
-        }
-        
-        /**
-         * A distance between this field value and another. Here we want a distance that correlates with the number of keys between here and there for the same.
-         * Essentially we want the inverse of the number of bytes that match. document.
-         *
-         * @param fv
-         *            a field value
-         * @return a distance between here and there (negative means there is before here)
-         */
-        public double distance(FieldValue fv) {
-            byte[] s1 = getValueField().getBytes();
-            byte[] s2 = fv.getValueField().getBytes();
-            int len = Math.min(s1.length, s2.length);
-            
-            int matches = 0;
-            int lastCharDiff = 0;
-            
-            for (int i = 0; i <= len; i++) {
-                lastCharDiff = getValue(s2, i) - getValue(s1, i);
-                if (lastCharDiff == 0) {
-                    matches++;
-                } else {
-                    break;
-                }
-            }
-            
-            return Math.copySign(1.0d / (matches + 1), lastCharDiff);
-        }
-        
-        private int getValue(byte[] bytes, int index) {
-            if (index >= bytes.length) {
-                return 0;
-            } else {
-                return bytes[index];
-            }
-        }
-        
-        public String getValueField() {
-            return valueField;
-        }
-        
-        public String getField() {
-            return valueField.substring(nullOffset + 1);
-        }
-        
-        public String getValue() {
-            return valueField.substring(0, nullOffset);
-        }
-        
-        @Override
-        public int compareTo(FieldValue o) {
-            return valueField.compareTo(o.valueField);
-        }
-        
-        @Override
-        public int hashCode() {
-            return valueField.hashCode();
-        }
-        
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof FieldValue) {
-                return valueField.equals(((FieldValue) obj).valueField);
-            }
-            return false;
-        }
-        
-        @Override
-        public String toString() {
-            return getField() + " -> " + getValue();
-        }
-        
-        public static FieldValue getFieldValue(Key key) {
-            return getFieldValue(key.getColumnQualifier());
-        }
-        
-        public static FieldValue getFieldValue(Text cqText) {
-            if (cqText == null) {
-                return null;
-            }
-            return getFieldValue(cqText.toString());
-        }
-        
-        public static FieldValue getFieldValue(String cq) {
-            if (cq == null) {
-                return null;
-            }
-            
-            // pull apart the cq
-            String[] cqParts = StringUtils.split(cq, '\0');
-            
-            // if we do not even have the first datatype\0uid, then lets find it
-            if (cqParts.length <= 2) {
-                return null;
-            }
-            
-            // get the value and field
-            String value = "";
-            String field = "";
-            if (cqParts.length >= 4) {
-                field = cqParts[cqParts.length - 1];
-                value = cqParts[2];
-                // in case the value had null characters therein
-                for (int i = 3; i < (cqParts.length - 1); i++) {
-                    value = value + '\0' + cqParts[i];
-                }
-            } else if (cqParts.length == 3) {
-                value = cqParts[2];
-            }
-            
-            return new FieldValue(field, value);
-        }
-        
     }
 }
