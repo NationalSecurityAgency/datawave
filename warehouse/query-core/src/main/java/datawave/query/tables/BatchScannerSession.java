8e6fff6403631b2aec9163e49d95a6bdd6153c2d
Keith Ratcliffe <ratcliffe.keith@gmail.com>
2023-05-15 16:25:41 -0400

    Closes #1961 - Use MoreExecutors.newDirectExecutorService() for ListenableFuture callbacks (#1962)

--- warehouse/query-core/src/main/java/datawave/query/tables/BatchScannerSession.java
+++ warehouse/query-core/src/main/java/datawave/query/tables/BatchScannerSession.java
@@ -373,7 +373,7 @@ public class BatchScannerSession extends ScannerSession implements Iterator<Entr
                 scan = new SpeculativeScan(localTableName, localAuths, chunk, delegatorReference, delegatedResourceInitializer, resultQueue, listenerService);
                 
                 scan.setVisitors(visitorFunctions);
-                
+
                 Scan childScan = new Scan(localTableName, localAuths, new ScannerChunk(chunk), delegatorReference, BatchResource.class,
                                 ((SpeculativeScan) scan).getQueue(), listenerService);
                 
@@ -459,7 +459,7 @@ public class BatchScannerSession extends ScannerSession implements Iterator<Entr
         ListenableFuture<Scan> future = (ListenableFuture<Scan>) service.submit(scan);
         if (increment)
             runnableCount.incrementAndGet();
-        Futures.addCallback(future, this, service);
+        Futures.addCallback(future, this, MoreExecutors.newDirectExecutorService());
     }
     
     /**
