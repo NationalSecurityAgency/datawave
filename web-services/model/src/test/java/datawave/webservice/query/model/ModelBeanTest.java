85369894945a292572cf5890147dae8fa5b29a6d
keith-ratcliffe <ratcliffe.keith@gmail.com>
2023-06-12 15:55:16 +0000

    Closes #1994 - Update webservice XMLNS

--- web-services/model/src/test/java/datawave/webservice/query/model/ModelBeanTest.java
+++ web-services/model/src/test/java/datawave/webservice/query/model/ModelBeanTest.java
@@ -96,7 +96,7 @@ public class ModelBeanTest {
          * In jdk 9+, java.lang.Package.getPackageInfo() may fail to load a package-info.class,
          * which is needed to resolve our JAXB package-level annotations. Here, this results in:
          *
-         * "javax.xml.bind.UnmarshalException: unexpected element (uri:"http://webservice.datawave/v1", local:"Model").
+         * "javax.xml.bind.UnmarshalException: unexpected element (uri:"http://webservice.datawave.nsa/v1", local:"Model").
          *    Expected elements are <{}Model>"
          *
          * As a workaround, we can force it to load here in the same manner as in jdk 8's Package.getPackageInfo(),
