524cab31ed4881866bc803d4787c197a5e576ac3
Miner <andrew.lf.miner@gmail.com>
2023-06-21 17:13:42 +0000

    Testing formatter changes for ease of PR reviews

--- warehouse/query-core/src/main/java/datawave/core/iterators/filter/GlobalIndexDataTypeFilter.java
+++ warehouse/query-core/src/main/java/datawave/core/iterators/filter/GlobalIndexDataTypeFilter.java
@@ -16,15 +16,15 @@ import java.util.Set;
  * The iterator skips entries in the global index for entries not in the specified set of data types
  */
 public class GlobalIndexDataTypeFilter extends Filter {
-    
+
     protected static final Logger log = Logger.getLogger(GlobalIndexDataTypeFilter.class);
     public static final String DATA_TYPE = "data.type.";
     private Set<String> dataTypes = null;
-    
+
     @Override
     public void init(SortedKeyValueIterator<Key,Value> source, Map<String,String> options, IteratorEnvironment env) throws IOException {
         super.init(source, options, env);
-        
+
         int i = 1;
         if (options.containsKey(DATA_TYPE + i)) {
             dataTypes = new HashSet<String>();
@@ -41,21 +41,21 @@ public class GlobalIndexDataTypeFilter extends Filter {
             }
         }
     }
-    
+
     @Override
     public boolean accept(Key k, Value v) {
         if (dataTypes == null) {
             return true;
         }
-        
+
         if (dataTypes.isEmpty()) { // accept none of the things
             return false;
         }
-        
+
         // The column qualifier contains the shard id and datatype separated by a null byte.
         String dataType = k.getColumnQualifier().toString();
         dataType = dataType.substring(dataType.indexOf('\0') + 1);
         return dataTypes.contains(dataType); // accept only specified datatypes
     }
-    
+
 }
