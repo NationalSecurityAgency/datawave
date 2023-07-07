468ae0ad6903d446da1bc74571240edc5dfbf442
Ivan Bella <ivan@bella.name>
2023-07-07 15:16:50 +0000

    sendQueryMetric whenever the metric is updated

--- web-services/query/src/main/java/datawave/webservice/query/metric/QueryMetricsBean.java
+++ web-services/query/src/main/java/datawave/webservice/query/metric/QueryMetricsBean.java
@@ -89,24 +89,21 @@ public class QueryMetricsBean {
      */
     @PermitAll
     public void updateMetric(BaseQueryMetric metric) throws Exception {
-        DatawavePrincipal dp = getPrincipal();
+        try {
+            DatawavePrincipal dp = getPrincipal();
 
-        if (metric.getLastWrittenHash() != metric.hashCode()) {
-            metric.setLastWrittenHash(metric.hashCode());
-            try {
-                metric.setLastUpdated(new Date());
-                sendQueryMetric(dp, metric);
-                // PageMetrics now know their own page numbers
-                // this should keep large queries from blowing up the queue
-                // Leave the last page on the list so that interceptors can update it.
-                Iterator<PageMetric> itr = metric.getPageTimes().iterator();
-                while (metric.getPageTimes().size() > 1) {
-                    itr.next();
-                    itr.remove();
-                }
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
+            sendQueryMetric(dp, metric);
+
+            // PageMetrics now know their own page numbers
+            // this should keep large queries from blowing up the queue
+            // Leave the last page on the list so that interceptors can update it.
+            Iterator<PageMetric> itr = metric.getPageTimes().iterator();
+            while (metric.getPageTimes().size() > 1) {
+                itr.next();
+                itr.remove();
             }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
     }
 
