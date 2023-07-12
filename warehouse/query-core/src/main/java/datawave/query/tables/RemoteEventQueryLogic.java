5bc918db0f7e1f72c049e922a9fb5fe686ca669b
hgklohr <hgklohr@gmail.com>
2023-07-12 17:15:37 +0000

    Fixed merge conflict

--- warehouse/query-core/src/main/java/datawave/query/tables/RemoteEventQueryLogic.java
+++ warehouse/query-core/src/main/java/datawave/query/tables/RemoteEventQueryLogic.java
@@ -102,7 +102,7 @@ public class RemoteEventQueryLogic extends BaseQueryLogic<EventBase> implements 
     }
     
     @Override
-    public String getPlan(AccumuloClient connection, Query settings, Set<Authorizations> auths, boolean expandFields, boolean expandValues) throws Exception {
+    public String getPlan(Connector connection, Query settings, Set<Authorizations> auths, boolean expandFields, boolean expandValues) throws Exception {
         GenericResponse<String> planResponse = remoteQueryService.planQuery(getRemoteQueryLogic(), getCallerObject());
         return planResponse.getResult();
     }
