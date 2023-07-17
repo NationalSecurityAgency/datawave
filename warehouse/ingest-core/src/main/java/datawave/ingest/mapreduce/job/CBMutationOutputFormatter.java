e55c0bfc5ea14c086309dda3c13ab0ed42ee7782
keith-ratcliffe <ratcliffe.keith@gmail.com>
2023-07-17 15:17:39 +0000

    Fixes #2039 - Restored ability to configure AccumuloOutputFormat's accumulo client via script - Cleanup of IngestJob's CBMutationOutputFormatter setup for live ingest

--- warehouse/ingest-core/src/main/java/datawave/ingest/mapreduce/job/CBMutationOutputFormatter.java
+++ warehouse/ingest-core/src/main/java/datawave/ingest/mapreduce/job/CBMutationOutputFormatter.java
@@ -1,23 +1,49 @@
 package datawave.ingest.mapreduce.job;
 
 import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.accumulo.core.client.Accumulo;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.hadoop.mapreduce.AccumuloOutputFormat;
+import org.apache.accumulo.hadoop.mapreduce.OutputFormatBuilder;
+import org.apache.accumulo.hadoopImpl.mapreduce.OutputFormatBuilderImpl;
+import org.apache.accumulo.hadoopImpl.mapreduce.lib.OutputConfigurator;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.log4j.Logger;
 
+import datawave.ingest.data.config.ingest.AccumuloHelper;
 import datawave.ingest.mapreduce.handler.shard.ShardedDataTypeHandler;
 
 public class CBMutationOutputFormatter extends AccumuloOutputFormat {
-
     private static final Logger log = Logger.getLogger(CBMutationOutputFormatter.class);
 
     @Override
     public RecordWriter<Text,Mutation> getRecordWriter(TaskAttemptContext attempt) throws IOException {
         return new CBRecordWriter(super.getRecordWriter(attempt), attempt);
+    }
+
+    public static Properties getClientProperties(Configuration conf) {
+        // Get any AccumuloClient property customizations (e.g., batch writer config overrides, etc)
+        Properties clientProps = OutputConfigurator.getClientProperties(AccumuloOutputFormat.class, conf);
+        //@formatter:off
+        // Convert DW's connection conf keys into Accumulo-compatible keys as required
+        clientProps.putAll(Accumulo.newClientProperties()
+                .to(AccumuloHelper.getInstanceName(conf), AccumuloHelper.getZooKeepers(conf))
+                .as(AccumuloHelper.getUsername(conf), new PasswordToken(new String(AccumuloHelper.getPassword(conf))))
+                .build());
+        //@formatter:on
+        return clientProps;
+    }
+
+    public static OutputFormatBuilder.OutputOptions<Job> configure(Configuration conf) {
+        OutputFormatBuilder.ClientParams<Job> clientParams = new OutputFormatBuilderImpl<>(AccumuloOutputFormat.class);
+        return clientParams.clientProperties(getClientProperties(conf));
     }
 
     public static class CBRecordWriter extends RecordWriter<Text,Mutation> {
