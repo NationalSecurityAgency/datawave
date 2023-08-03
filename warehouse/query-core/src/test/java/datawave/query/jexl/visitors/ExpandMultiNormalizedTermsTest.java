edd593e1c66c648522d83ed3ba52e9e151b73b11
Ivan Bella <ivan@bella.name>
2023-08-03 15:35:46 -0400

    re #2008: Make the default normalization expansions as they were before (#2053)
    * Ensure that normalizations expansions remains the same if not lenient or strict.
    * If both strict and lenient, then apply neither with a warning message

--- warehouse/query-core/src/test/java/datawave/query/jexl/visitors/ExpandMultiNormalizedTermsTest.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/visitors/ExpandMultiNormalizedTermsTest.java
@@ -3,15 +3,14 @@ package datawave.query.jexl.visitors;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
-import datawave.data.type.DateType;
 import datawave.data.type.IpAddressType;
 import datawave.data.type.LcNoDiacriticsType;
 import datawave.data.type.LcType;
 import datawave.data.type.NoOpType;
 import datawave.data.type.NumberType;
+import datawave.data.type.PointType;
 import datawave.data.type.StringType;
 import datawave.data.type.TrimLeadingZerosType;
-import datawave.data.type.PointType;
 import datawave.data.type.Type;
 import datawave.query.config.ShardQueryConfiguration;
 import datawave.query.jexl.JexlASTHelper;
@@ -31,6 +30,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -606,6 +606,7 @@ public class ExpandMultiNormalizedTermsTest {
         helper.addTermFrequencyFields(dataTypes.keySet());
 
         config.setQueryFieldsDatatypes(dataTypes);
+        config.setLenientFields(Collections.singleton("FOO"));
 
         // this tests for the successful normalization as a simple number can be normalized as a regex
         String original = "(FOO == 'ab32')";
@@ -639,6 +640,28 @@ public class ExpandMultiNormalizedTermsTest {
         helper.addTermFrequencyFields(dataTypes.keySet());
 
         config.setQueryFieldsDatatypes(dataTypes);
+
+        // this tests for the successful normalization as a simple number can be normalized as a regex
+        String original = "FOO =~ '32' && FOO !~ '42'";
+        String expected = "(FOO =~ '32' || FOO =~ '\\Q+bE3.2\\E') && (FOO !~ '\\Q+bE4.2\\E' && FOO !~ '42')";
+        expandTerms(original, expected);
+
+        // in this case the numeric normalization fails but others succeed (e.g. lcnodiacritics)
+        original = "FOO =~ '3.*2' && FOO !~ '3.*22'";
+        expected = "((_Eval_ = true) && (FOO =~ '3.*2')) && ((_Eval_ = true) && (FOO !~ '3.*22'))";
+        expandTerms(original, expected);
+    }
+
+    @Test
+    public void testLenientFailedRegexNormalizersAndNRNodes() throws ParseException {
+        Multimap<String,Type<?>> dataTypes = HashMultimap.create();
+        dataTypes.putAll("FOO", Sets.newHashSet(new LcNoDiacriticsType(), new LcType(), new NumberType()));
+
+        helper.setIndexedFields(dataTypes.keySet());
+        helper.addTermFrequencyFields(dataTypes.keySet());
+
+        config.setQueryFieldsDatatypes(dataTypes);
+        config.setLenientFields(Collections.singleton("FOO"));
 
         // this tests for the successful normalization as a simple number can be normalized as a regex
         String original = "FOO =~ '32' && FOO !~ '42'";
