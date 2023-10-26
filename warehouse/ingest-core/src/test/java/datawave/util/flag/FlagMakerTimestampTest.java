030d419840bffd85158021fa6fa5cea039dbb239
Matthew Peterson <mjpete3@evoforge.org>
2023-10-26 17:49:04 +0000

    adjusted

--- warehouse/ingest-core/src/test/java/datawave/util/flag/FlagMakerTimestampTest.java
+++ warehouse/ingest-core/src/test/java/datawave/util/flag/FlagMakerTimestampTest.java
@@ -27,6 +27,12 @@ import org.slf4j.LoggerFactory;
 @RunWith(Parameterized.class)
 public class FlagMakerTimestampTest {
     private static final Logger log = LoggerFactory.getLogger(FlagMakerTimestampTest.class);
+    private static final boolean USE_FOLDER_TIME = true;
+    private static final boolean USE_FILE_TIME = false;
+    private static final boolean SET_FLAG_TIME_TO_LAST_FILE = true;
+    private static final boolean SET_FLAG_TIME_TO_CREATED_TIME = false;
+    private static final Boolean UNSET = null;
+    private static final int EXPECTED_NUM_FLAG_FILES = 2;
 
     private final Boolean useFolderTime;
     private final Boolean setFlagFileTimestamp;
@@ -40,15 +46,15 @@ public class FlagMakerTimestampTest {
     public static Collection<Object[]> data() {
         // @formatter:off
 		return Arrays.asList(new Object[][]{
-				{true, true},
-				{true, false},
-				{true, null},
-				{false, true},
-				{false, false},
-				{false, null},
-				{null, true},
-				{null, false},
-				{null, null}});
+				{USE_FOLDER_TIME, SET_FLAG_TIME_TO_LAST_FILE},
+				{USE_FOLDER_TIME, SET_FLAG_TIME_TO_CREATED_TIME},
+				{USE_FOLDER_TIME, UNSET},
+				{USE_FILE_TIME, SET_FLAG_TIME_TO_LAST_FILE},
+				{USE_FILE_TIME, SET_FLAG_TIME_TO_CREATED_TIME},
+				{USE_FILE_TIME, UNSET},
+				{UNSET, SET_FLAG_TIME_TO_LAST_FILE},
+				{UNSET, SET_FLAG_TIME_TO_CREATED_TIME},
+				{UNSET, UNSET}});
 		// @formatter:on
     }
 
@@ -76,29 +82,29 @@ public class FlagMakerTimestampTest {
         createInputFiles();
         configureFlagMakerWithParameters();
         List<File> flagFiles = runFlagMaker();
-        assertEquals("Incorrect number of flagFiles.  Found files: " + FlagFileTestInspector.logFiles(flagFiles), 2, flagFiles.size());
+        assertEquals("Incorrect number of flagFiles.  Found files: " + FlagFileTestInspector.logFiles(flagFiles), EXPECTED_NUM_FLAG_FILES, flagFiles.size());
         assertFlagFilesTimestampInExpectedRange(flagFiles, determineExpectedRange());
     }
 
     private void createInputFiles() throws IOException {
-        // two days, 5 files each day, two folders in getFlagMakerConfig() = 20 flags
+        // Two days, five files each day, two folders in getFlagMakerConfig() = twenty input files
         flagMakerTestSetup.withFilesPerDay(5).withNumDays(2).createTestFiles();
     }
 
     private void configureFlagMakerWithParameters() {
-        // true by default - use latest last modified for input files
+        // true by default - use latest last modified of input files
         if (null != this.setFlagFileTimestamp) {
             flagMakerTestSetup.getFlagMakerConfig().setSetFlagFileTimestamp(this.setFlagFileTimestamp);
         }
-        // false by default - use the folder's timestamp regardless of input
-        // files
+        // true: use the folder's timestamp regardless of input files
+        // false (default): use time of input file
         if (null != this.useFolderTime) {
             flagMakerTestSetup.getFlagMakerConfig().setUseFolderTimestamp(this.useFolderTime);
         }
     }
 
     private List<File> runFlagMaker() throws Exception {
-        FlagMaker flagMaker = new FlagMaker(flagMakerTestSetup.getFlagMakerConfig());
+        FlagMaker flagMaker = new FlagMaker(this.flagMakerTestSetup.getFlagMakerConfig());
         flagMaker.processFlags();
         return FlagFileTestInspector.listFlagFiles(this.flagMakerTestSetup.getFlagMakerConfig());
     }
@@ -106,7 +112,7 @@ public class FlagMakerTimestampTest {
     private LongRange determineExpectedRange() {
         LongRange range;
 
-        if (null != useFolderTime && useFolderTime) {
+        if (null != useFolderTime && useFolderTime == USE_FOLDER_TIME) {
             range = new LongRange(flagMakerTestSetup.getMinFolderTime(), flagMakerTestSetup.getMaxFolderTime());
         } else {
             range = new LongRange(flagMakerTestSetup.getMinLastModified(), flagMakerTestSetup.getMaxLastModified());
@@ -117,7 +123,7 @@ public class FlagMakerTimestampTest {
     private void assertFlagFilesTimestampInExpectedRange(List<File> flagFiles, LongRange expectedTimestampRange) {
         for (File file : flagFiles) {
             if (file.getName().endsWith(".flag")) {
-                if (this.setFlagFileTimestamp == null || this.setFlagFileTimestamp) {
+                if (this.setFlagFileTimestamp == null || SET_FLAG_TIME_TO_LAST_FILE == this.setFlagFileTimestamp) {
                     assertTrue(expectedTimestampRange.containsLong(file.lastModified()));
                 } else {
                     assertFalse(expectedTimestampRange.containsLong(file.lastModified()));
