108c6e69df2756bf368df2f5e998e10fd057b057
austin007008 <undercovera008@gmail.com>
2023-11-02 17:32:49 +0000

    format

--- warehouse/query-core/src/test/java/datawave/query/transformer/SSDeepSimilarityQueryTransformerTest.java
+++ warehouse/query-core/src/test/java/datawave/query/transformer/SSDeepSimilarityQueryTransformerTest.java
@@ -53,7 +53,7 @@ public class SSDeepSimilarityQueryTransformerTest {
 
     public void basicExpects(Key k) {
         EasyMock.expect(mockQuery.getQueryAuthorizations()).andReturn("A,B,C");
-        EasyMock.expect(mockQuery.findParameter("minScore")).andReturn(new QueryImpl.Parameter("minScore",""));
+        EasyMock.expect(mockQuery.findParameter("minScore")).andReturn(new QueryImpl.Parameter("minScore", ""));
         EasyMock.expect(mockResponseFactory.getEventQueryResponse()).andReturn(new DefaultEventQueryResponse());
         EasyMock.expect(mockResponseFactory.getEvent()).andReturn(new DefaultEvent()).times(1);
         EasyMock.expect(mockResponseFactory.getField()).andReturn(new DefaultField()).times(4);
