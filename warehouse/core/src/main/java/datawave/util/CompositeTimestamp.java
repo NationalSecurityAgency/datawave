89671828b1c81e71a94f39827b89614b48c21be7
hlgp <hlgp@users.noreply.github.com>
2023-11-03 01:24:30 +0000

    Laying the gound work for composite timestamps

---
+++ warehouse/core/src/main/java/datawave/util/CompositeTimestamp.java
@@ -0,0 +1,18 @@
+package datawave.util;
+
+public class CompositeTimestamp {
+
+    public static boolean isCompositeTimestamp(long ts) {
+        return false;
+    }
+
+    public static Long getEventDate(long ts) {
+        // TODO
+        return ts;
+    }
+
+    public static Long getAgeOffDate(long ts) {
+        // TODO
+        return ts;
+    }
+}
