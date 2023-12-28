d088422b4f7c8037082e68c53f1cdb81a269574a
Charles Williams <cwilliams.work@gmail.com>
2023-12-28 13:28:29 +0000

    splittable rfile input format fixes and updates

--- warehouse/core/src/main/java/datawave/ingest/mapreduce/job/SplittableRFileInputFormat.java
+++ warehouse/core/src/main/java/datawave/ingest/mapreduce/job/SplittableRFileInputFormat.java
@@ -1,20 +1,20 @@
 package datawave.ingest.mapreduce.job;
 
-import org.apache.accumulo.core.conf.DefaultConfiguration;
+import static org.apache.accumulo.core.conf.Property.TABLE_CRYPTO_PREFIX;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.accumulo.core.crypto.CryptoFactoryLoader;
 import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
-import org.apache.accumulo.core.file.FileOperations;
 import org.apache.accumulo.core.file.FileSKVIterator;
-import org.apache.accumulo.core.file.FileSKVWriter;
 import org.apache.accumulo.core.file.blockfile.impl.CachableBlockFile;
 import org.apache.accumulo.core.file.rfile.RFile;
-import org.apache.accumulo.core.file.rfile.RFileOperations;
 import org.apache.accumulo.core.spi.crypto.CryptoEnvironment;
 import org.apache.accumulo.core.spi.crypto.CryptoService;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.InputSplit;
@@ -22,19 +22,12 @@ import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-import java.io.EOFException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static org.apache.accumulo.core.conf.Property.TABLE_CRYPTO_PREFIX;
-
 public class SplittableRFileInputFormat extends RFileInputFormat {
+    private static final Logger log = Logger.getLogger(SplittableRFileInputFormat.class);
+    public static final String MIN_BLOCKS_PER_SPLIT = SplittableRFileInputFormat.class.getName() + ".minBlocksPerSplit";
+
     @Override
     protected boolean isSplitable(JobContext context, Path filename) {
         return true;
@@ -45,13 +38,19 @@ public class SplittableRFileInputFormat extends RFileInputFormat {
         return new SplittableRFileRecordReader();
     }
 
+    /**
+     * Create rfile splits for the configured job. See <code>FileInputFormat</code>
+     *
+     * @param job
+     * @return
+     * @throws IOException
+     */
     @Override
     public List<InputSplit> getSplits(JobContext job) throws IOException {
         Configuration config = job.getConfiguration();
 
         List<InputSplit> rfileSplits = new ArrayList<>();
-
-
+        log.info("getting splits for job");
         // get the configured directories/files
         for (InputSplit inputSplit : super.getSplits(job)) {
             if (!(inputSplit instanceof FileSplit)) {
@@ -60,13 +59,25 @@ public class SplittableRFileInputFormat extends RFileInputFormat {
 
             // for each file get the index blocks
             FileSplit fileSplit = (FileSplit) inputSplit;
+            log.info("creating splits for: " + fileSplit);
             rfileSplits.addAll(getSplits(config, fileSplit));
         }
+        log.info("total splits: " + rfileSplits.size());
 
         return rfileSplits;
     }
 
+    /**
+     * Create rfile splits for a given FileSplit. Splits will honor the MIN_BLOCKS_PER_SPLIT config option. Splits may extend more than MIN_BLOCKS_PER_SPLIT if
+     * a given Key spans more index blocks.
+     *
+     * @param config
+     * @param fileSplit
+     * @return
+     * @throws IOException
+     */
     public static List<InputSplit> getSplits(Configuration config, FileSplit fileSplit) throws IOException {
+        log.info("getting splits for: " + fileSplit);
         Path rfile = fileSplit.getPath();
         FileSystem fs = rfile.getFileSystem(config);
 
@@ -91,14 +102,17 @@ public class SplittableRFileInputFormat extends RFileInputFormat {
         int blkCount = 0;
         int splitBlocks = 0;
 
+        int minBlocksPerSplit = config.getInt(MIN_BLOCKS_PER_SPLIT, 1);
+        log.info("Blocks per split: " + minBlocksPerSplit);
 
         Key top = null;
         while (iter.hasTop()) {
             splitBlocks++;
             top = iter.getTopKey();
 
-            if (!top.equals(lastSplit)) {
-                splits.add(new RFileSplit(fileSplit, blkCount, splitBlocks));
+            if (!top.equals(lastSplit) && splitBlocks >= minBlocksPerSplit) {
+                splits.add(new RFileSplit(fileSplit.getPath(), fileSplit.getStart(), fileSplit.getLength(), fileSplit.getLocations(), blkCount, splitBlocks,
+                                top));
                 blkCount += splitBlocks;
                 lastSplit = iter.getTopKey();
                 splitBlocks = 0;
@@ -109,7 +123,8 @@ public class SplittableRFileInputFormat extends RFileInputFormat {
         // if there is a gap between the last index key top key and the lastKey in the file or there hasn't been a split created yet
         if (top.equals(lastKey) || splits.isEmpty()) {
             // add the last split
-            splits.add(new RFileSplit(fileSplit, blkCount, splitBlocks + 1));
+            splits.add(new RFileSplit(fileSplit.getPath(), fileSplit.getStart(), fileSplit.getLength(), fileSplit.getLocations(), blkCount, splitBlocks + 1,
+                            top));
         }
 
         return splits;
@@ -118,161 +133,5 @@ public class SplittableRFileInputFormat extends RFileInputFormat {
     // get the splits for the rfile
     public static List<InputSplit> getSplits(Configuration config, Path rfile) throws IOException {
         return getSplits(config, new FileSplit(rfile, 0, 0, new String[0]));
-    }
-
-    public static FileSKVIterator getIterator(Configuration config, RFileSplit split) throws IOException, InterruptedException {
-        return getKeys(config, split.getFileSplit().getPath(), split.getStartBlock(), split.getLength());
-    }
-
-    public static FileSKVIterator getKeys(Configuration config, Path rfile, int startIndexBlock, long numIndexBlocks) throws IOException {
-        FileSystem fs = rfile.getFileSystem(config);
-
-        if (!fs.exists(rfile)) {
-            throw new IllegalArgumentException(rfile + " does not exist");
-        }
-
-        CryptoService cs = CryptoFactoryLoader.getServiceForClient(CryptoEnvironment.Scope.TABLE, config.getPropsWithPrefix(TABLE_CRYPTO_PREFIX.name()));
-        CachableBlockFile.CachableBuilder cb = new CachableBlockFile.CachableBuilder().fsPath(fs, rfile).conf(config).cryptoService(cs);
-        RFile.Reader rfileReader = new RFile.Reader(cb);
-        FileSKVIterator iter = rfileReader.getIndex();
-
-        Key start = null;
-        int blkCount = 1;
-        if (startIndexBlock > 0) {
-            while (iter.hasTop() && blkCount < startIndexBlock) {
-                iter.next();
-                blkCount++;
-            }
-
-            if (blkCount < startIndexBlock) {
-                throw new EOFException("start index " + startIndexBlock + " beyond end of file");
-            }
-
-            start = iter.getTopKey();
-            blkCount = 0;
-        }
-
-        // find the end key
-        while (iter.hasTop() && blkCount < numIndexBlocks) {
-            iter.next();
-            blkCount++;
-        }
-
-        if (blkCount < numIndexBlocks) {
-            throw new EOFException("end index " + startIndexBlock + numIndexBlocks + " beyond end of file");
-        }
-
-        Key end = null;
-        if (iter.hasTop()) {
-            end = iter.getTopKey();
-        }
-
-        rfileReader.seek(new Range(start, true, end, false), Collections.EMPTY_SET, false);
-
-        return rfileReader;
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Logger.getRootLogger().setLevel(Level.WARN);
-        FileSystem fs = FileSystem.getLocal(new Configuration());
-        Path rfile = new Path("/tmp/test.rf");
-
-        boolean buildFile = false;
-
-        if (buildFile && fs.exists(rfile)) {
-            fs.delete(rfile);
-        }
-
-        if (buildFile) {
-            CryptoService cs = CryptoFactoryLoader.getServiceForClient(CryptoEnvironment.Scope.TABLE, new Configuration().getPropsWithPrefix(TABLE_CRYPTO_PREFIX.name()));
-            FileSKVWriter writer = RFileOperations.getInstance().newWriterBuilder().forFile("/tmp/test.rf", FileSystem.getLocal(new Configuration()), new Configuration(), cs).withTableConfiguration(DefaultConfiguration.getInstance()).build();
-            writer.startDefaultLocalityGroup();
-
-
-            int count = 0;
-            for (int i = 0; i < 10000000; i++) {
-                // use hex formatting
-                String key = String.format("%08x", i);
-                for (int j = 0; j < 100; j++) {
-                    writer.append(new Key(key), new Value("" + j));
-                    count++;
-                }
-            }
-
-            System.out.println("total keys: " + count);
-            writer.close();
-        }
-
-        if (fs.exists(new Path("/tmp/keys"))) {
-            fs.delete(new Path("/tmp/keys"));
-        }
-
-        FSDataOutputStream out = fs.create(new Path("/tmp/keys"));
-
-
-        List<InputSplit> splits = SplittableRFileInputFormat.getSplits(new Configuration(), rfile);
-        int writeBlocks = splits.size() + 1;
-        out.write(("total splits: " + splits.size() + "\n").getBytes(StandardCharsets.UTF_8));
-        int totalKeys = 0;
-        for (int i = 0; i < splits.size(); i++) {
-            RFileSplit split = (RFileSplit) splits.get(i);
-
-            FileSKVIterator itr = SplittableRFileInputFormat.getKeys(new Configuration(), split.getFileSplit().getPath(), split.getStartBlock(), split.getLength());
-            out.write((itr.getTopKey() + " " + itr.getTopValue() + "\n").getBytes(StandardCharsets.UTF_8));
-
-            int count = 0;
-            Key last = null;
-            Value lastValue = null;
-            while (itr.hasTop()) {
-                count++;
-                last = itr.getTopKey();
-                lastValue = itr.getTopValue();
-//                out.write((last + " " + lastValue + "\n").getBytes(StandardCharsets.UTF_8));
-                itr.next();
-            }
-//            out.write((last + " " + lastValue + "\n").getBytes(StandardCharsets.UTF_8));
-            out.write(("keys: " + count + "\n").getBytes(StandardCharsets.UTF_8));
-            totalKeys += count;
-            if (i + 1 == writeBlocks) {
-                System.exit(0);
-            }
-        }
-
-        out.write(("total keys: " + totalKeys).getBytes(StandardCharsets.UTF_8));
-
-        out.close();
-    }
-
-    // For a file with N blocks
-    // startBlock 0 - beginning of the file to the first block
-    // startBlock N - last block to the end
-    public static class RFileSplit extends InputSplit {
-        private final FileSplit fileSplit;
-        private final int startBlock;
-        private final int numBlocks;
-
-        public RFileSplit(FileSplit fileSplit, int startBlock, int numBlocks) {
-            this.fileSplit = fileSplit;
-            this.startBlock = startBlock;
-            this.numBlocks = numBlocks;
-        }
-
-        public FileSplit getFileSplit() {
-            return fileSplit;
-        }
-
-        public int getStartBlock() {
-            return startBlock;
-        }
-
-        @Override
-        public long getLength() throws IOException, InterruptedException {
-            return numBlocks;
-        }
-
-        @Override
-        public String[] getLocations() throws IOException, InterruptedException {
-            return fileSplit.getLocations();
-        }
     }
 }
--- warehouse/core/src/main/java/datawave/ingest/mapreduce/job/SplittableRFileRecordReader.java
+++ warehouse/core/src/main/java/datawave/ingest/mapreduce/job/SplittableRFileRecordReader.java
@@ -1,17 +1,95 @@
 package datawave.ingest.mapreduce.job;
 
+import static org.apache.accumulo.core.conf.Property.TABLE_CRYPTO_PREFIX;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.util.Collections;
+
+import org.apache.accumulo.core.crypto.CryptoFactoryLoader;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Range;
+import org.apache.accumulo.core.file.FileSKVIterator;
+import org.apache.accumulo.core.file.blockfile.impl.CachableBlockFile;
+import org.apache.accumulo.core.file.rfile.RFile;
+import org.apache.accumulo.core.spi.crypto.CryptoEnvironment;
+import org.apache.accumulo.core.spi.crypto.CryptoService;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
-
-import java.io.IOException;
 
 public class SplittableRFileRecordReader extends RFileRecordReader {
     @Override
     public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
-        if (split instanceof SplittableRFileInputFormat.RFileSplit) {
-            fileIterator = SplittableRFileInputFormat.getIterator(context.getConfiguration(), (SplittableRFileInputFormat.RFileSplit) split);
+        if (split instanceof RFileSplit) {
+            fileIterator = getIterator(context.getConfiguration(), (RFileSplit) split);
         } else {
             super.initialize(split, context);
         }
+    }
+
+    /**
+     * Open an rfile specified by the split, create an iterator to read the region of the rfile configured in the split by reading the rfile index blocks.
+     * Delegate to the split to get the seek range.
+     *
+     * @param config
+     * @param split
+     * @return
+     * @throws IOException
+     */
+    public static FileSKVIterator getIterator(Configuration config, RFileSplit split) throws IOException {
+        Path rfile = split.getPath();
+        long startIndexBlock = split.getStartBlock();
+        long numIndexBlocks = split.getLength();
+
+        FileSystem fs = rfile.getFileSystem(config);
+
+        if (!fs.exists(rfile)) {
+            throw new IllegalArgumentException(rfile + " does not exist");
+        }
+
+        CryptoService cs = CryptoFactoryLoader.getServiceForClient(CryptoEnvironment.Scope.TABLE, config.getPropsWithPrefix(TABLE_CRYPTO_PREFIX.name()));
+        CachableBlockFile.CachableBuilder cb = new CachableBlockFile.CachableBuilder().fsPath(fs, rfile).conf(config).cryptoService(cs);
+        RFile.Reader rfileReader = new RFile.Reader(cb);
+        FileSKVIterator iter = rfileReader.getIndex();
+
+        Key start = null;
+        int blkCount = 1;
+        if (startIndexBlock > 0) {
+            while (iter.hasTop() && blkCount < startIndexBlock) {
+                iter.next();
+                blkCount++;
+            }
+
+            if (blkCount < startIndexBlock) {
+                throw new EOFException("start index " + startIndexBlock + " beyond end of file");
+            }
+
+            start = iter.getTopKey();
+            blkCount = 0;
+        }
+
+        // find the end key
+        while (iter.hasTop() && blkCount < numIndexBlocks) {
+            iter.next();
+            blkCount++;
+        }
+
+        if (blkCount < numIndexBlocks) {
+            throw new EOFException("end index " + startIndexBlock + numIndexBlocks + " beyond end of file");
+        }
+
+        Key end = null;
+        if (iter.hasTop()) {
+            end = iter.getTopKey();
+        }
+
+        Range seekRange = split.getSeekRange(start, end);
+
+        rfileReader.seek(seekRange, Collections.EMPTY_SET, false);
+
+        return rfileReader;
     }
 }
