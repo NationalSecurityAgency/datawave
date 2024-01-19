9e7ed7b055e16b11a1a7cceb7114d198acd37774
awright27 <ajwrig6@uwe.nsa.gov>
2024-01-19 19:41:41 +0000

    Working on unit tests

--- warehouse/ingest-core/src/main/java/datawave/ingest/input/reader/event/EventCombineSequenceFileInputFormat.java
+++ warehouse/ingest-core/src/main/java/datawave/ingest/input/reader/event/EventCombineSequenceFileInputFormat.java
@@ -1,15 +1,16 @@
 package datawave.ingest.input.reader.event;
 
-import datawave.ingest.data.RawRecordContainer;
+import java.io.IOException;
+
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileRecordReader;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileRecordReaderWrapper;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileSplit;
 import org.apache.hadoop.mapreduce.lib.input.CombineSequenceFileInputFormat;
-import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
-import java.io.IOException;
+import datawave.ingest.data.RawRecordContainer;
 
 /**
  * Specialization of SequenceFileInputFormat that expects an {@link RawRecordContainer} object as the value.
@@ -20,12 +21,12 @@ import java.io.IOException;
  *            key type
  */
 public class EventCombineSequenceFileInputFormat<K> extends CombineSequenceFileInputFormat<K,RawRecordContainer> {
-    
+
     @Override
     public RecordReader<K,RawRecordContainer> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException {
         return new CombineFileRecordReader((CombineFileSplit) split, context, EventSequenceFileRecordReaderWrapper.class);
     }
-    
+
     private static class EventSequenceFileRecordReaderWrapper<K> extends CombineFileRecordReaderWrapper<K,RawRecordContainer> {
         // this constructor signature is required by CombineFileRecordReader
         public EventSequenceFileRecordReaderWrapper(CombineFileSplit split, TaskAttemptContext context, Integer idx) throws IOException, InterruptedException {
--- warehouse/ingest-core/src/test/java/datawave/ingest/input/reader/event/EventCombineSequenceFileInputFormatTest.java
+++ warehouse/ingest-core/src/test/java/datawave/ingest/input/reader/event/EventCombineSequenceFileInputFormatTest.java
@@ -1,109 +1,127 @@
 package datawave.ingest.input.reader.event;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.util.BitSet;
 import java.util.List;
 import java.util.Random;
 
-import datawave.data.hash.UID;
-import datawave.ingest.config.RawRecordContainerImpl;
-import datawave.ingest.data.RawRecordContainer;
-import datawave.ingest.data.Type;
-import datawave.ingest.data.TypeRegistry;
-import datawave.ingest.data.config.ingest.FakeIngestHelper;
-import datawave.ingest.mapreduce.MapReduceTestUtil;
-import datawave.ingest.mapreduce.SimpleDataTypeHandler;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Counters;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.MRJobConfig;
 import org.apache.hadoop.mapreduce.MapContext;
 import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.StatusReporter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.TaskAttemptID;
+import org.apache.hadoop.mapreduce.TaskType;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileRecordReader;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileSplit;
-import org.apache.hadoop.mapreduce.lib.input.CombineSequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.task.MapContextImpl;
+import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EventCombineSequenceFileInputFormatTest {
-    private static final Logger LOG =
-            LoggerFactory.getLogger(EventCombineSequenceFileInputFormatTest.class);
-    private static Configuration conf = new Configuration();
-    private static FileSystem localFs = null;
+import datawave.data.hash.UID;
+import datawave.ingest.config.RawRecordContainerImpl;
+import datawave.ingest.data.RawRecordContainer;
 
-    static {
-        try {
-            conf.set("fs.defaultFS", "file:///");
-            localFs = FileSystem.getLocal(conf);
-        } catch (IOException e) {
-            throw new RuntimeException("init failure", e);
-        }
+public class EventCombineSequenceFileInputFormatTest {
+    private static final Logger LOG = LoggerFactory.getLogger(EventCombineSequenceFileInputFormatTest.class);
+    private Configuration conf;
+    private FileSystem localFs;
+    private Path workDir;
+    private Job job;
+
+    @Before
+    public void setup() throws Exception {
+        conf = new Configuration();
+        conf.set("fs.defaultFS", "file:///");
+        localFs = FileSystem.getLocal(conf);
+        workDir = new Path(new Path(System.getProperty("test.build.data", "."), "data"), "EventCombineSequenceFileInputFormatTest");
+        job = Job.getInstance(conf);
+        FileInputFormat.setInputPaths(job, workDir);
     }
 
-    private static Path workDir =
-            new Path(new Path(System.getProperty("test.build.data", "."), "data"),
-                    "EventCombineSequenceFileInputFormatTest");
+    @After
+    public void teardown() throws Exception {
+        localFs.delete(workDir, true);
+        localFs.close();
+    }
 
-    @Test(timeout=10000)
+    @Test
+    public void testFormatCombinesMultipleFiles() throws Exception {
+        createFile("File1.seq", 1);
+        createFile("File2.seq", 1);
+        TaskAttemptContext context = createDummyMapTaskAttemptContext(conf);
+        // create a combine split for the files
+        InputFormat<IntWritable,RawRecordContainer> format = new EventCombineSequenceFileInputFormat<IntWritable>();
+        List<InputSplit> splits = format.getSplits(job);
+        assertEquals("We got more than one splits!", 1, splits.size());
+        InputSplit split = splits.get(0);
+        RecordReader<IntWritable,RawRecordContainer> reader = format.createRecordReader(split, context);
+        MapContext<IntWritable,RawRecordContainer,IntWritable,RawRecordContainer> mcontext = new MapContextImpl<>(job.getConfiguration(),
+                        context.getTaskAttemptID(), reader, null, null, createDummyReporter(), split);
+        reader.initialize(split, mcontext);
+        Assert.assertEquals("File File1: Test Data 1", reader.getCurrentValue());
+    }
+
+    @Ignore
+    @Test(timeout = 10000)
     public void testFormat() throws IOException, InterruptedException {
-        Job job = Job.getInstance(conf);
 
         Random random = new Random();
         long seed = random.nextLong();
         random.setSeed(seed);
 
-        localFs.delete(workDir, true);
-        FileInputFormat.setInputPaths(job, workDir);
-
         final int length = 10000;
         final int numFiles = 10;
 
         // create files with a variety of lengths
-        createFiles(length, numFiles, random, job);
+        createRandomFiles(length, numFiles, random);
 
-        TaskAttemptContext context = MapReduceTestUtil.createDummyMapTaskAttemptContext(job.getConfiguration());
+        TaskAttemptContext context = createDummyMapTaskAttemptContext(conf);
         // create a combine split for the files
-        InputFormat<IntWritable, RawRecordContainer> format =
-                new EventCombineSequenceFileInputFormat<IntWritable>();
+        InputFormat<IntWritable,RawRecordContainer> format = new EventCombineSequenceFileInputFormat<IntWritable>();
         for (int i = 0; i < 3; i++) {
-            int numSplits =
-                    random.nextInt(length/(SequenceFile.SYNC_INTERVAL/20)) + 1;
+            int numSplits = random.nextInt(length / (SequenceFile.SYNC_INTERVAL / 20)) + 1;
             LOG.info("splitting: requesting = " + numSplits);
             List<InputSplit> splits = format.getSplits(job);
-            LOG.info("splitting: got =        " + splits.size());
+            LOG.info("splitting: got = " + splits.size());
 
             // we should have a single split as the length is comfortably smaller than
             // the block size
             assertEquals("We got more than one splits!", 1, splits.size());
             InputSplit split = splits.get(0);
-            assertEquals("It should be CombineFileSplit",
-                    CombineFileSplit.class, split.getClass());
+            assertEquals("It should be CombineFileSplit", CombineFileSplit.class, split.getClass());
 
             // check the split
             BitSet bits = new BitSet(length);
-            RecordReader<IntWritable,RawRecordContainer> reader =
-                    format.createRecordReader(split, context);
-            MapContext<IntWritable,RawRecordContainer,IntWritable,RawRecordContainer> mcontext =
-                    new MapContextImpl<>(job.getConfiguration(),
-                            context.getTaskAttemptID(), reader, null, null,
-                            MapReduceTestUtil.createDummyReporter(), split);
+            RecordReader<IntWritable,RawRecordContainer> reader = format.createRecordReader(split, context);
+            MapContext<IntWritable,RawRecordContainer,IntWritable,RawRecordContainer> mcontext = new MapContextImpl<>(job.getConfiguration(),
+                            context.getTaskAttemptID(), reader, null, null, createDummyReporter(), split);
             reader.initialize(split, mcontext);
-            assertEquals("reader class is CombineFileRecordReader.",
-                    CombineFileRecordReader.class, reader.getClass());
+            assertEquals("reader class is CombineFileRecordReader.", CombineFileRecordReader.class, reader.getClass());
 
             try {
                 while (reader.nextKeyValue()) {
@@ -122,6 +140,32 @@ public class EventCombineSequenceFileInputFormatTest {
         }
     }
 
+    public TaskAttemptContext createDummyMapTaskAttemptContext(Configuration conf) {
+        TaskAttemptID tid = new TaskAttemptID("jt", 1, TaskType.MAP, 0, 0);
+        conf.set(MRJobConfig.TASK_ATTEMPT_ID, tid.toString());
+        return new TaskAttemptContextImpl(conf, tid);
+    }
+
+    public StatusReporter createDummyReporter() {
+        return new StatusReporter() {
+            public void setStatus(String s) {}
+
+            public void progress() {}
+
+            @Override
+            public float getProgress() {
+                return 0;
+            }
+
+            public Counter getCounter(Enum<?> name) {
+                return new Counters().findCounter(name);
+            }
+
+            public Counter getCounter(String group, String name) {
+                return new Counters().findCounter(group, name);
+            }
+        };
+    }
 
     private static class Range {
         private final int start;
@@ -138,48 +182,58 @@ public class EventCombineSequenceFileInputFormatTest {
         }
     }
 
-    private static Range[] createRanges(int length, int numFiles, Random random) {
+    private Range[] createRanges(int length, int numFiles, Random random) {
         // generate a number of files with various lengths
         Range[] ranges = new Range[numFiles];
         for (int i = 0; i < numFiles; i++) {
-            int start = i == 0 ? 0 : ranges[i-1].end;
-            int end = i == numFiles - 1 ?
-                    length :
-                    (length/numFiles)*(2*i + 1)/2 + random.nextInt(length/numFiles) + 1;
+            int start = i == 0 ? 0 : ranges[i - 1].end;
+            int end = i == numFiles - 1 ? length : (length / numFiles) * (2 * i + 1) / 2 + random.nextInt(length / numFiles) + 1;
             ranges[i] = new Range(start, end);
         }
         return ranges;
     }
 
-    private static RawRecordContainerImpl getEvent(Job job) {
+    private RawRecordContainerImpl getEvent(byte[] data) {
         RawRecordContainerImpl myEvent = new RawRecordContainerImpl();
         myEvent.addSecurityMarking("columnVisibility", "PRIVATE");
         myEvent.setId(UID.builder().newId());
-        myEvent.setConf(job.getConfiguration());
+        myEvent.setConf(conf);
 
         Instant i = Instant.from(DateTimeFormatter.ISO_INSTANT.parse("2016-04-26T01:31:53Z"));
         myEvent.setDate(i.getEpochSecond());
+        // myEvent.setRawData(data);
         return myEvent;
     }
 
-    private static void createFiles(int length, int numFiles, Random random,
-                                    Job job) throws IOException {
+    private void createFile(String filename, int numEvents) throws IOException {
+        Path file = new Path(workDir, filename);
+        // create a file with length entries
+        // @SuppressWarnings("deprecation")
+        try (SequenceFile.Writer writer = SequenceFile.createWriter(localFs, conf, file, IntWritable.class, RawRecordContainerImpl.class)) {
+            for (int i = 0; i < numEvents; i++) {
+                IntWritable key = new IntWritable(i);
+                byte[] data = String.format("File {}: Test Data {}", filename, i).getBytes();
+                RawRecordContainerImpl value = getEvent(data);
+                writer.append(key, value);
+            }
+        }
+    }
+
+    private void createRandomFiles(int length, int numFiles, Random random) throws IOException {
         Range[] ranges = createRanges(length, numFiles, random);
 
         for (int i = 0; i < numFiles; i++) {
             Path file = new Path(workDir, "test_" + i + ".seq");
             // create a file with length entries
             @SuppressWarnings("deprecation")
-            SequenceFile.Writer writer =
-                    SequenceFile.createWriter(localFs, job.getConfiguration(), file,
-                            IntWritable.class, RawRecordContainerImpl.class);
+            SequenceFile.Writer writer = SequenceFile.createWriter(localFs, job.getConfiguration(), file, IntWritable.class, RawRecordContainerImpl.class);
             Range range = ranges[i];
             try {
                 for (int j = range.start; j < range.end; j++) {
                     IntWritable key = new IntWritable(j);
                     byte[] data = new byte[random.nextInt(10)];
                     random.nextBytes(data);
-                    RawRecordContainerImpl value = getEvent(job);
+                    RawRecordContainerImpl value = getEvent(data);
                     writer.append(key, value);
                 }
             } finally {
