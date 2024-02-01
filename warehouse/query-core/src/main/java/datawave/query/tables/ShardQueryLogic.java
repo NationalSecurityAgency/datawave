97c7b60c6a822ef184a426247d678a72ad6d5ae7
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2024-02-01 12:00:27 +0000

    Optionally limit query execution based on the number of indexed equality terms

--- warehouse/query-core/src/main/java/datawave/query/tables/ShardQueryLogic.java
+++ warehouse/query-core/src/main/java/datawave/query/tables/ShardQueryLogic.java
@@ -1545,6 +1545,14 @@ public class ShardQueryLogic extends BaseQueryLogic<Entry<Key,Value>> {
         getConfig().setIntermediateMaxTermThreshold(intermediateMaxTermThreshold);
     }
 
+    public int getIndexedMaxTermThreshold() {
+        return getConfig().getIndexedMaxTermThreshold();
+    }
+
+    public void setIndexedMaxTermThreshold(int indexedMaxTermThreshold) {
+        getConfig().setIndexedMaxTermThreshold(indexedMaxTermThreshold);
+    }
+
     public int getFinalMaxTermThreshold() {
         return getConfig().getFinalMaxTermThreshold();
     }
