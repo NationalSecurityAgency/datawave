7412ed8bd5b851a3a8a965e73e08424802e932af
hlgp <hlgp@users.noreply.github.com>
2024-03-22 16:38:48 +0000

    remove commented out code

--- warehouse/query-core/src/main/java/datawave/query/jexl/visitors/IteratorBuildingVisitor.java
+++ warehouse/query-core/src/main/java/datawave/query/jexl/visitors/IteratorBuildingVisitor.java
@@ -279,35 +279,6 @@ public class IteratorBuildingVisitor extends BaseVisitor {
                 if (subNode instanceof ASTEQNode) {
                     delayedEqNodes.add(subNode);
                 }
-                // else if (subNode instanceof ASTERNode){
-                // JexlNode source = instance.getSource();
-                //
-                // String identifier = JexlASTHelper.getIdentifier(source);
-                // LiteralRange<?> range = buildLiteralRange(source);
-                // limitLookup = true;
-                //
-                // if( !indexOnlyFields.contains(identifier) && termFrequencyFields.contains(identifier)){
-                // NestedIterator<Key> nested = buildExceededFromTermFrequency(identifier, and, source, range, data);
-                // if (null != nested && data instanceof AbstractIteratorBuilder) {
-                //
-                // AbstractIteratorBuilder iterators = (AbstractIteratorBuilder) data;
-                //
-                // iterators.addInclude(nested);
-                //
-                // } else {
-                // if (isQueryFullySatisfied) {
-                // log.warn("Determined that isQueryFullySatisfied should be false, but it was not preset to false in the SatisfactionVisitor");
-                // }
-                // // if there is no parent
-                // if (root == null && data == null) {
-                // // make this nested the root node
-                // root = nested;
-                // }
-                // return nested;
-                //
-                // }
-                // }
-                // }
                 if (isQueryFullySatisfied) {
                     log.warn("Determined that isQueryFullySatisfied should be false, but it was not preset to false in the SatisfactionVisitor");
                 }
