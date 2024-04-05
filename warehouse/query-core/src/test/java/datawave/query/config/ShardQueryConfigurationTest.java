0ae0f5de157b5611af54feee01efd0e9655c5f5b
foster33 <realfosty@gmail.com>
2024-04-05 10:33:00 +0000

    update shardqueryconfigurationtest

--- warehouse/query-core/src/test/java/datawave/query/config/ShardQueryConfigurationTest.java
+++ warehouse/query-core/src/test/java/datawave/query/config/ShardQueryConfigurationTest.java
@@ -207,6 +207,8 @@ public class ShardQueryConfigurationTest {
         updatedValues.put("defaultDateTypeName", "DOCUMENT");
         defaultValues.put("cleanupShardsAndDaysQueryHints", true);
         updatedValues.put("cleanupShardsAndDaysQueryHints", false);
+        defaultValues.put("cachePreviouslyExpandedFields", false);
+        updatedValues.put("cachePreviouslyExpandedFields", true);
         defaultValues.put("numQueryThreads", 8);
         updatedValues.put("numQueryThreads", 80);
         defaultValues.put("numDateIndexThreads", 8);
