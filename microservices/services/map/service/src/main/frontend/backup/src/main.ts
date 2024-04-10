dabda07f24553b1d2c7132a4164dc3a712749be6
Whitney O'Meara <james.w.omeara@gmail.com>
2024-04-10 11:50:28 +0000

    Removed some unnecessary files

--- microservices/services/map/service/src/main/frontend/backup/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-// import router from './router'
-import './style.css'
-
-// createApp(App).use(router).mount('#app')
-createApp(App).mount('#app')
