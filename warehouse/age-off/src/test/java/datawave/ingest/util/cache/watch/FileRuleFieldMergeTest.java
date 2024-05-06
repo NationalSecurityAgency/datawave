7361007e1e4fb4927e8f8444beecdea695cc138a
matthpeterson <matt.peterson@tibertechnologies.com>
2024-05-06 13:42:48 -0400

    Age off modulation and new formatting utility   (#2326)
    * Extracted code and resources only related to age-off to their own module Removed unneeded dependencies and transitive dependencies from poms, added scope and comment for each
    
    * Added CSV to Age off xml configuration file utilities
    
    * Corrected compiler warnings and code inspection findings
    
    * Use pom version variables when reused
    
    * Correct javadoc formatting and remove star exclusions
    
    * Began changes from peer review
    
    * Remove XMLStreamWriter implementation
    
    * MR feedback
    
    * MR feedback
    
    * MR feedback - removed Reader
    
    * MR feedback: improved exception message
    
    * MR feedback: survival food
    
    * MR feedback: remove the remove the redundancy
    
    * Updated poms based on dependency:analyze feedback
    
    * Use Document Transformer to create XML
    
    * Update pom versions in new modules post-rebase
    
    * Remove typo from pom version
    
    * Update pom versions in new modules post-rebase
    
    ---------
    
    Co-authored-by: Matthew Peterson <mjpete3@evoforge.org>
    Co-authored-by: hgklohr <hgklohr@gmail.com>

---
+++ warehouse/age-off/src/test/java/datawave/ingest/util/cache/watch/FileRuleFieldMergeTest.java
@@ -0,0 +1,74 @@
+package datawave.ingest.util.cache.watch;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Before;
+import org.junit.Test;
+
+import datawave.iterators.filter.ageoff.FilterRule;
+
+/**
+ * Tests to verify capability of merging configs that use filters that inherit from {@code FieldAgeOffFilter}
+ */
+public class FileRuleFieldMergeTest {
+    static final String ROOT_FILTER_CONFIGURATION_FILE = "/filter/test-root-field.xml";
+    private static final String CHILD_FILTER_CONFIGURATION_FILE = "/filter/test-customized-field.xml";
+
+    private FileRuleWatcher watcher;
+    private TestFieldFilter parentFilter;
+    // this one inherits defaults from parentFilter
+    private TestFieldFilter childFilter;
+
+    @Before
+    public void before() throws IOException {
+        Path childPath = new Path(this.getClass().getResource(CHILD_FILTER_CONFIGURATION_FILE).toString());
+        Path rootPath = new Path(this.getClass().getResource(ROOT_FILTER_CONFIGURATION_FILE).toString());
+        FileSystem fs = childPath.getFileSystem(new Configuration());
+        watcher = new FileRuleWatcher(fs, childPath, 1);
+        parentFilter = (TestFieldFilter) loadRulesFromFile(watcher, fs, rootPath);
+        childFilter = (TestFieldFilter) loadRulesFromFile(watcher, fs, childPath);
+    }
+
+    @Test
+    public void verifyIsIndexOnlyForChild() throws IOException {
+        assertThat(isIndexTable(parentFilter), is(false));
+
+        assertThat(isIndexTable(childFilter), is(true));
+    }
+
+    @Test
+    public void verifyInheritedParentConfigs() throws IOException {
+        // parent config fields
+        // <fields>alpha,beta,gamma,delta</fields>
+        // since child is index config, field should be in the column family
+        Key key = new Key("row", "alpha", "cq", "vis", 0);
+        assertThat(childFilter.accept(key, new Value()), is(false));
+
+        key = new Key("row", "beta", "cq", "vis", Long.MAX_VALUE);
+        assertThat(childFilter.accept(key, new Value()), is(true));
+    }
+
+    private Boolean isIndexTable(TestFieldFilter filter) {
+        return Boolean.valueOf(filter.options.getOption("isindextable", "false"));
+    }
+
+    private static FilterRule loadRulesFromFile(FileRuleWatcher watcher, FileSystem fs, Path filePath) throws IOException {
+        Collection<FilterRule> rules = watcher.loadContents(fs.open(filePath));
+        // should only have the single rule
+        assertThat(rules.size(), is(1));
+        for (FilterRule rule : rules) {
+            assertEquals(TestFieldFilter.class, rule.getClass());
+        }
+        return rules.iterator().next();
+    }
+}
