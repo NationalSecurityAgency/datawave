013812082449a121cffba34a89b3cc7b71def71a
Laura Schanno <lbschanno@gmail.com>
2024-05-31 09:48:31 -0400

    Ensure CompositeQueryLogic error codes are sent to metrics
    The query metrics API expects QueryExceptions to be found in the cause
    of the original exception passed to BaseQueryMetric.setError(Throwable).
    In the case of CompositeQueryLogic, due to the additional wrapping of
    exceptions passed from its defined internal logics in a
    CompositeLogicException, query exceptions were not in the expected place
    in the stack hierarchy.
    
    Update the CompositeLogicException to identify any QueryExceptions
    present in the stacks of exception arguments, and ensure that they are
    either already a QueryException, or that if they contain a
    QueryException in the stack, that they are subsequently wrapped in a
    CompositeRaisedQueryException with a copy of the desired error code.
    
    Fixes #2340

--- core/query/src/main/java/datawave/core/query/logic/composite/CompositeLogicException.java
+++ core/query/src/main/java/datawave/core/query/logic/composite/CompositeLogicException.java
@@ -7,38 +7,94 @@ import java.util.Map;
 import datawave.webservice.query.exception.QueryException;
 
 public class CompositeLogicException extends RuntimeException {
+
     public CompositeLogicException(String message, String logicName, Exception exception) {
-        super(getMessage(message, Collections.singletonMap(logicName, exception)), exception);
+        super(getMessage(message, Collections.singletonMap(logicName, exception)), getRaisedQueryException(exception));
     }
 
     public CompositeLogicException(String message, Map<String,Exception> exceptions) {
-        super(getMessage(message, exceptions), getQueryException(exceptions.values()));
+        super(getMessage(message, exceptions), getCause(exceptions.values()));
         if (exceptions.size() > 1) {
             exceptions.values().forEach(this::addSuppressed);
         }
     }
 
-    // looking for an exception that has a nested QueryException such that we may return an error code
-    private static Exception getQueryException(Collection<Exception> exceptions) {
+    /**
+     * Return the cause to use, prioritizing the first {@link QueryException} instance that we see. In the case where the {@link QueryException} is found to be
+     * the cause or further nested in the stack of an {@link Exception}, a {@link CompositeRaisedQueryException} will be returned with the query exception's
+     * error code, and the original exception as the cause. This is necessary to ensure the error code is passed to query metrics.
+     */
+    private static Exception getCause(Collection<Exception> exceptions) {
         if (exceptions.size() == 1) {
             return exceptions.iterator().next();
         }
-        Exception e = null;
-        for (Exception test : exceptions) {
-            if (e == null) {
-                e = test;
-            } else if (isQueryException(test)) {
-                e = test;
-            }
-            if (isQueryException(e)) {
-                break;
+        Exception cause = null;
+        for (Exception exception : exceptions) {
+            // Establish the initial cause as the first seen exception.
+            if (cause == null) {
+                cause = getRaisedQueryException(exception);
+                // If the first cause we see is a QueryException, there's nothing further to do.
+                if (cause instanceof QueryException) {
+                    return cause;
+                }
+                // If a subsequent exception is a or contains a QueryException in its stack, return it with the query exception error code available at the root
+                // exception.
+            } else if (hasQueryExceptionInStack(exception)) {
+                return getRaisedQueryException(exception);
             }
         }
-        return e;
+        return cause;
     }
 
-    private static boolean isQueryException(Exception e) {
-        return new QueryException(e).getQueryExceptionsInStack().size() > 1;
+    /**
+     * Return whether the given throwable contains at least one {@link QueryException} in its stack trace (including itself).
+     */
+    private static boolean hasQueryExceptionInStack(Throwable throwable) {
+        return getFirstQueryExceptionInStack(throwable) != null;
+    }
+
+    /**
+     * Return the given exception with query exception's error code (if present) available at the root exception. This means one of the following cases will
+     * occur:
+     * <li>
+     * <ul>
+     * The exception is not a {@link QueryException} and no {@link QueryException} exists in the exception's stack: The exception will be returned.
+     * </ul>
+     * <ul>
+     * The exception is a {@link QueryException}: The exception will be returned.
+     * </ul>
+     * <ul>
+     * The exception is not a {@link QueryException}, but a {@link QueryException} exists in the exception's stack. A {@link CompositeRaisedQueryException} will
+     * be returned with the error code of the first {@link QueryException} found in the stack, and the original exception as its cause.
+     * </ul>
+     * </li>
+     */
+    private static Exception getRaisedQueryException(Exception exception) {
+        if (exception instanceof QueryException) {
+            return exception;
+        } else {
+            // TODO - should we fetch the top-most or bottom-most query exception in the stack?
+            QueryException queryException = getFirstQueryExceptionInStack(exception);
+            if (queryException != null) {
+                return new CompositeRaisedQueryException(exception, queryException.getErrorCode());
+            } else {
+                return exception;
+            }
+        }
+    }
+
+    /**
+     * Return the first {@link QueryException} found in the stack, or null if none were found.
+     */
+    private static QueryException getFirstQueryExceptionInStack(Throwable throwable) {
+        if (throwable != null) {
+            if (throwable instanceof QueryException) {
+                return (QueryException) throwable;
+            } else {
+                return getFirstQueryExceptionInStack(throwable.getCause());
+            }
+        }
+        return null;
     }
 
     private static String getMessage(String message, Map<String,Exception> exceptions) {
