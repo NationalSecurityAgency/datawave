a0de546485ffcfc802d9072ae94aa47e135932b1
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2024-08-23 11:11:00 +0000

    Assert fields after document aggregation

--- warehouse/query-core/src/test/java/datawave/query/function/IndexOnlyKeyToDocumentDataTest.java
+++ warehouse/query-core/src/test/java/datawave/query/function/IndexOnlyKeyToDocumentDataTest.java
@@ -3,7 +3,11 @@ package datawave.query.function;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -16,6 +20,7 @@ import org.apache.accumulo.core.iteratorsImpl.system.SortedMapIterator;
 import org.junit.jupiter.api.Test;
 
 import datawave.query.attributes.Document;
+import datawave.query.data.parsers.EventKey;
 import datawave.query.iterator.aggregation.DocumentData;
 
 /**
@@ -30,16 +35,21 @@ public class IndexOnlyKeyToDocumentDataTest {
 
     private final Value value = new Value();
 
+    private final EventKey parser = new EventKey();
+    private List<Map.Entry<Key,Value>> result = new ArrayList<>();
+
     @Test
     public void testEventData_singleValue() {
         String field = "FIELD_A";
         drive(field, documentKey, getSource(), 1);
+        assertFields(Set.of("FIELD_A"));
     }
 
     @Test
     public void testEventData_multiValue() {
         String field = "FIELD_B";
         drive(field, documentKey, getSource(), 2);
+        assertFields(Set.of("FIELD_B"));
     }
 
     @Test
@@ -52,30 +62,35 @@ public class IndexOnlyKeyToDocumentDataTest {
     public void testTLDData_parent_singleValue() {
         String field = "FIELD_A";
         drive(field, documentKey, getTLDSource(), 1);
+        assertFields(Set.of("FIELD_A"));
     }
 
     @Test
     public void testTLDData_child_singleValue() {
         String field = "FIELD_A";
         drive(field, childKey, getTLDSource(), 1);
+        assertFields(Set.of("FIELD_A"));
     }
 
     @Test
     public void testTLDData_grandchild_singleValue() {
         String field = "FIELD_X";
         drive(field, grandChildKey, getTLDSource(), 1);
+        assertFields(Set.of("FIELD_X"));
     }
 
     @Test
     public void testTLDData_parent_multiValue() {
         String field = "FIELD_B";
         drive(field, documentKey, getTLDSource(), 2);
+        assertFields(Set.of("FIELD_B"));
     }
 
     @Test
     public void testTLDData_child_multiValue() {
         String field = "FIELD_B";
         drive(field, childKey, getTLDSource(), 2);
+        assertFields(Set.of("FIELD_B"));
     }
 
     /**
@@ -91,11 +106,23 @@ public class IndexOnlyKeyToDocumentDataTest {
      *            the expected size post-aggregation
      */
     private void drive(String field, Key documentKey, SortedKeyValueIterator<Key,Value> source, int expectedSize) {
-        IndexOnlyKeyToDocumentData data = new IndexOnlyKeyToDocumentData(getRange(), field, source);
-        Map.Entry<DocumentData,Document> result = data.apply(getEntry(documentKey));
+        result.clear(); // clear any existing data
 
-        int dataSize = result.getKey().getData().size();
-        assertEquals(expectedSize, dataSize);
+        IndexOnlyKeyToDocumentData data = new IndexOnlyKeyToDocumentData(getRange(), field, source);
+        Map.Entry<DocumentData,Document> aggregated = data.apply(getEntry(documentKey));
+
+        result = aggregated.getKey().getData();
+        assertEquals(expectedSize, result.size());
+    }
+
+    private void assertFields(Set<String> fields) {
+        Set<String> resultFields = new HashSet<>();
+        for (Map.Entry<Key,Value> e : result) {
+            parser.parse(e.getKey());
+            resultFields.add(parser.getField());
+        }
+
+        assertEquals(fields, resultFields);
     }
 
     private Map.Entry<Key,Document> getEntry(Key documentKey) {
@@ -126,7 +153,7 @@ public class IndexOnlyKeyToDocumentDataTest {
     }
 
     private SortedMap<Key,Value> getTLDSourceData() {
-        SortedMap<Key,Value> data = getSourceData();
+        SortedMap<Key,Value> data = new TreeMap<>();
         // parent
         data.put(new Key("20230114_17", "tf", "datatype\0uid\0value_1\0FIELD_A"), value);
         data.put(new Key("20230114_17", "tf", "datatype\0uid\0value_2\0FIELD_B"), value);
--- warehouse/query-core/src/test/java/datawave/query/function/KeyToDocumentDataTest.java
+++ warehouse/query-core/src/test/java/datawave/query/function/KeyToDocumentDataTest.java
@@ -3,6 +3,9 @@ package datawave.query.function;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
@@ -16,6 +19,7 @@ import org.apache.accumulo.core.iteratorsImpl.system.SortedMapIterator;
 import org.junit.jupiter.api.Test;
 
 import datawave.query.attributes.Document;
+import datawave.query.data.parsers.EventKey;
 import datawave.query.iterator.aggregation.DocumentData;
 import datawave.query.predicate.EventDataQueryFieldFilter;
 import datawave.query.predicate.EventDataQueryFilter;
@@ -33,10 +37,14 @@ public class KeyToDocumentDataTest {
     private final RangeProvider rangeProvider = new DocumentRangeProvider();
     private final RangeProvider tldRangeProvider = new TLDRangeProvider();
 
+    private final EventKey parser = new EventKey();
+    private List<Map.Entry<Key,Value>> result = new ArrayList<>();
+
     @Test
     public void testEventData_defaultEquality_noFilter() {
         KeyToDocumentData data = new KeyToDocumentData(getSource(), equality, null, false, false).withRangeProvider(rangeProvider);
         drive(data, getEntry(), 9);
+        assertEventFields();
     }
 
     @Test
@@ -45,12 +53,21 @@ public class KeyToDocumentDataTest {
         EventDataQueryFilter filter = new EventDataQueryFieldFilter(projection);
         KeyToDocumentData data = new KeyToDocumentData(getSource(), equality, filter, false, false).withRangeProvider(rangeProvider);
         drive(data, getEntry(), 2);
+        assertFields(Set.of("FIELD_A", "FIELD_B"));
+
+        // exclusive filter should result with nothing
+        projection = new KeyProjection(Set.of("FIELD_Z"), Projection.ProjectionType.INCLUDES);
+        filter = new EventDataQueryFieldFilter(projection);
+        data = new KeyToDocumentData(getSource(), equality, filter, false, false).withRangeProvider(rangeProvider);
+        drive(data, getEntry(), 0);
+        assertFields(Set.of());
     }
 
     @Test
     public void testEventData_TLDEquality_noFilter() {
         KeyToDocumentData data = new KeyToDocumentData(getSource(), tldEquality, null, false, false).withRangeProvider(rangeProvider);
         drive(data, getEntry(), 9);
+        assertEventFields();
     }
 
     @Test
@@ -59,12 +76,15 @@ public class KeyToDocumentDataTest {
         EventDataQueryFilter filter = new EventDataQueryFieldFilter(projection);
         KeyToDocumentData data = new KeyToDocumentData(getSource(), tldEquality, filter, false, false).withRangeProvider(rangeProvider);
         drive(data, getEntry(), 2);
+        assertFields(Set.of("FIELD_A", "FIELD_B"));
     }
 
     @Test
     public void testTLDData_defaultEquality_noFilter() {
         KeyToDocumentData data = new KeyToDocumentData(getTLDSource(), equality, null, false, false).withRangeProvider(tldRangeProvider);
-        drive(data, getEntry(), 9);
+        drive(data, getEntry(), 4);
+        // TLDEquality is required to pick up the x,y,z fields
+        assertFields(Set.of("FIELD_A", "FIELD_B", "FIELD_C"));
     }
 
     @Test
@@ -73,12 +93,14 @@ public class KeyToDocumentDataTest {
         EventDataQueryFilter filter = new EventDataQueryFieldFilter(projection);
         KeyToDocumentData data = new KeyToDocumentData(getTLDSource(), equality, filter, false, false).withRangeProvider(tldRangeProvider);
         drive(data, getEntry(), 2);
+        assertFields(Set.of("FIELD_A", "FIELD_B"));
     }
 
     @Test
     public void testTLDData_TLDEquality_noFilter() {
         KeyToDocumentData data = new KeyToDocumentData(getTLDSource(), tldEquality, null, false, false).withRangeProvider(tldRangeProvider);
-        drive(data, getEntry(), 16);
+        drive(data, getEntry(), 11);
+        assertTLDFields();
     }
 
     @Test
@@ -87,6 +109,7 @@ public class KeyToDocumentDataTest {
         EventDataQueryFilter filter = new EventDataQueryFieldFilter(projection);
         KeyToDocumentData data = new KeyToDocumentData(getTLDSource(), tldEquality, filter, false, false).withRangeProvider(tldRangeProvider);
         drive(data, getEntry(), 4);
+        assertFields(Set.of("FIELD_A", "FIELD_B"));
     }
 
     /**
@@ -100,10 +123,30 @@ public class KeyToDocumentDataTest {
      *            the expected size post-aggregation
      */
     private void drive(KeyToDocumentData data, Map.Entry<Key,Document> entry, int expectedSize) {
-        Map.Entry<DocumentData,Document> result = data.apply(entry);
+        result.clear(); // clear any existing data
 
-        int dataSize = result.getKey().getData().size();
-        assertEquals(expectedSize, dataSize);
+        Map.Entry<DocumentData,Document> aggregated = data.apply(entry);
+
+        result = aggregated.getKey().getData();
+        assertEquals(expectedSize, result.size());
+    }
+
+    private void assertEventFields() {
+        assertFields(Set.of("FIELD_A", "FIELD_B", "FIELD_C", "GROUP_FIELD.0", "GROUP_FIELD.1", "GROUP_FIELD.2", "GROUP_FIELD.3", "GROUP_FIELD.4"));
+    }
+
+    private void assertTLDFields() {
+        assertFields(Set.of("FIELD_A", "FIELD_B", "FIELD_C", "FIELD_D", "FIELD_X", "FIELD_Y", "FIELD_Z"));
+    }
+
+    private void assertFields(Set<String> fields) {
+        Set<String> resultFields = new HashSet<>();
+        for (Map.Entry<Key,Value> e : result) {
+            parser.parse(e.getKey());
+            resultFields.add(parser.getField());
+        }
+
+        assertEquals(fields, resultFields);
     }
 
     private Map.Entry<Key,Document> getEntry() {
@@ -127,7 +170,7 @@ public class KeyToDocumentDataTest {
         data.put(new Key("20230114_17", "datatype\0uid", "FIELD_A\0value_1"), value);
         data.put(new Key("20230114_17", "datatype\0uid", "FIELD_B\0value_2"), value);
         data.put(new Key("20230114_17", "datatype\0uid", "FIELD_C\0value_3"), value);
-        data.put(new Key("20230114_17", "datatype\0uid", "FIELD_D\0value_4"), value);
+        data.put(new Key("20230114_17", "datatype\0uid", "FIELD_C\0value_4"), value);
         data.put(new Key("20230114_17", "datatype\0uid", "GROUP_FIELD.0\0value_0"), value);
         data.put(new Key("20230114_17", "datatype\0uid", "GROUP_FIELD.1\0value_1"), value);
         data.put(new Key("20230114_17", "datatype\0uid", "GROUP_FIELD.2\0value_2"), value);
@@ -137,16 +180,16 @@ public class KeyToDocumentDataTest {
     }
 
     private SortedMap<Key,Value> getTLDSourceData() {
-        SortedMap<Key,Value> data = getSourceData();
+        SortedMap<Key,Value> data = new TreeMap<>();
         // parent
         data.put(new Key("20230114_17", "datatype\0uid", "FIELD_A\0value_1"), value);
         data.put(new Key("20230114_17", "datatype\0uid", "FIELD_B\0value_2"), value);
         data.put(new Key("20230114_17", "datatype\0uid", "FIELD_C\0value_3"), value);
-        data.put(new Key("20230114_17", "datatype\0uid", "FIELD_D\0value_4"), value);
+        data.put(new Key("20230114_17", "datatype\0uid", "FIELD_C\0value_4"), value);
         // child
         data.put(new Key("20230114_17", "datatype\0uid.1", "FIELD_A\0value_1"), value);
         data.put(new Key("20230114_17", "datatype\0uid.1", "FIELD_B\0value_2"), value);
-        data.put(new Key("20230114_17", "datatype\0uid.1", "FIELD_C\0value_3"), value);
+        data.put(new Key("20230114_17", "datatype\0uid.1", "FIELD_D\0value_3"), value);
         data.put(new Key("20230114_17", "datatype\0uid.1", "FIELD_D\0value_4"), value);
         // grandchild
         data.put(new Key("20230114_17", "datatype\0uid.2.7", "FIELD_X\0value_x"), value);
