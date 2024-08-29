05db63f3ed085e6d2d60ff4c7cfaef2785dcbcd4
Seth <smuckeratwork@gmail.com>
2024-08-29 14:04:30 -0400

    Documentation

--- warehouse/core/src/main/java/datawave/iterators/IterationInterruptedException.java
+++ warehouse/core/src/main/java/datawave/iterators/IterationInterruptedException.java
@@ -1,7 +1,9 @@
 package datawave.iterators;
 
 /**
- * Exception thrown if an interrupt flag is detected.
+ * This code was repurposed from org.apache.accumulo.core.iteratorsImpl.system.IterationInterruptedException.
+ * It was not part of the public API, so we've created a DataWave equivalent.
+ * This exception should be used in place of {@link org.apache.accumulo.core.iteratorsImpl.system.IterationInterruptedException} when thrown from an iterator.
  */
 public class IterationInterruptedException extends RuntimeException {
 
