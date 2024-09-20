9861e1f13de0305ee2863b6222a9ec2d8f5152fa
Whitney O'Meara <james.w.omeara@gmail.com>
2024-09-20 11:44:16 +0000

    more testing

--- contrib/datawave-quickstart/bin/services/maven/bootstrap.sh
+++ contrib/datawave-quickstart/bin/services/maven/bootstrap.sh
@@ -14,8 +14,8 @@ function bootstrapEmbeddedMaven() {
         info "JWO Files before:"
         info "$(ls -l ${DW_MAVEN_SERVICE_DIR})"
         DW_MAVEN_DIST="$( { downloadTarball "${DW_MAVEN_DIST_URI}" "${DW_MAVEN_SERVICE_DIR}" || downloadMavenTarball "datawave-parent" "gov.nsa.datawave.quickstart" "maven" "${DW_MAVEN_VERSION}" "${DW_MAVEN_SERVICE_DIR}"; } && echo "${tarball}" )"
-        DW_MAVEN_DOWNLOAD_TARBALL_DIST="$( downloadTarball "${DW_MAVEN_DIST_URI}" "${DW_MAVEN_SERVICE_DIR}" && echo "${tarball}" )"
-        DW_MAVEN_DOWNLOAD_MAVEN_TARBALL_DIST=$( downloadMavenTarball "datawave-parent" "gov.nsa.datawave.quickstart" "maven" "${DW_MAVEN_VERSION}" "${DW_MAVEN_SERVICE_DIR}" && echo "${tarball}" )
+        DW_MAVEN_DOWNLOAD_TARBALL_DIST="$( downloadTarball "${DW_MAVEN_DIST_URI}" "${DW_MAVEN_SERVICE_DIR}" && echo "ret: $? tarball: ${tarball}" )"
+        DW_MAVEN_DOWNLOAD_MAVEN_TARBALL_DIST=$( downloadMavenTarball "datawave-parent" "gov.nsa.datawave.quickstart" "maven" "${DW_MAVEN_VERSION}" "${DW_MAVEN_SERVICE_DIR}" && echo "ret: $? tarball: ${tarball}" )
         info "DW_MAVEN_DIST: ${DW_MAVEN_DIST}"
         info "DW_MAVEN_DOWNLOAD_TARBALL_DIST: ${DW_MAVEN_DOWNLOAD_TARBALL_DIST}"
         info "DW_MAVEN_DOWNLOAD_MAVEN_TARBALL_DIST: ${DW_MAVEN_DOWNLOAD_MAVEN_TARBALL_DIST}"
