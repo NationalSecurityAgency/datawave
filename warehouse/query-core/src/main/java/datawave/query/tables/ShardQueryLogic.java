609c85b96d949f04564007e381c27ead8e457e01
FineAndDandy <cwilliams.work@gmail.com>
2024-09-30 15:17:53 -0400

    Close ScannerSessiosn on timeout, correctly apply timeout fixes #2574 (#2578)
    * Close ScannerSessiosn on timeout, correctly apply timeout fixes #2574
    
    * javadoc updates fixes #2574
    
    * Clear interrupt flags after checking interrupted state fixes #2574
    
    ---------
    
    Co-authored-by: hgklohr <hgklohr@gmail.com>

--- warehouse/query-core/src/main/java/datawave/query/tables/ShardQueryLogic.java
+++ warehouse/query-core/src/main/java/datawave/query/tables/ShardQueryLogic.java
@@ -2432,6 +2432,14 @@ public class ShardQueryLogic extends BaseQueryLogic<Entry<Key,Value>> implements
         getConfig().setMaxIndexScanTimeMillis(maxTime);
     }
 
+    public long getMaxAnyFieldScanTimeMillis() {
+        return getConfig().getMaxAnyFieldScanTimeMillis();
+    }
+
+    public void setMaxAnyFieldScanTimeMillis(long maxAnyFieldScanTimeMillis) {
+        getConfig().setMaxAnyFieldScanTimeMillis(maxAnyFieldScanTimeMillis);
+    }
+
     public Function getQueryMacroFunction() {
         return queryMacroFunction;
     }
