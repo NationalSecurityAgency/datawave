672000e1aa84eeea0859d26de527d747340819bb
Seth <smuckeratwork@gmail.com>
2024-10-16 15:16:11 -0400

    Add comprehensive testing for ValidateNotVisitor

--- warehouse/query-core/src/main/java/datawave/query/lucene/visitors/ValidateNotVisitor.java
+++ warehouse/query-core/src/main/java/datawave/query/lucene/visitors/ValidateNotVisitor.java
@@ -15,17 +15,27 @@ public class ValidateNotVisitor extends BaseVisitor{
 
         boolean modSeen = false;
         for(QueryNode child : node.getChildren()){
-            QueryNodeType type = QueryNodeType.get(child.getClass());
-            switch (type) {
+            QueryNodeType childType = QueryNodeType.get(child.getClass());
+            switch (childType) {
                 case OR:
                 case AND: {
-                    throw new IllegalArgumentException("NotBooleanQueryNode cannot have junction as claus. Add parens");
+                    throw new IllegalArgumentException("NotBooleanQueryNode cannot have junction as a claus. Try wrapping it in parenthesis.");
                 }
                 case MODIFIER: {
                     if(modSeen){
-                        throw new IllegalArgumentException("NotBooleanQueryNode cannot have multiple modifiers");
+                        throw new IllegalArgumentException("NotBooleanQueryNode cannot have multiple modifiers.");
                     }
                     modSeen = true;
+
+                    for(QueryNode grandchild : child.getChildren()) {
+                        QueryNodeType grandchildType = QueryNodeType.get(grandchild.getClass());
+                        switch (grandchildType) {
+                            case OR:
+                            case AND: {
+                                throw new IllegalArgumentException("Modifier cannot have junction as a claus. Try wrapping it in parenthesis.");
+                            }
+                        }
+                    }
                     break;
                 }
             }
