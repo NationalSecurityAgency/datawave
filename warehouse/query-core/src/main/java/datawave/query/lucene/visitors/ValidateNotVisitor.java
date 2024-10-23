9c1249c69d650c88a8f40819f3d184dbb6457570
Seth <smuckeratwork@gmail.com>
2024-10-23 10:55:52 -0400

    Clean up ValidateNotVisitor & Tests:
    # Conflicts:
    #	warehouse/query-core/src/main/java/datawave/query/lucene/visitors/ValidateNotVisitor.java
    #	warehouse/query-core/src/test/java/datawave/query/lucene/visitors/ValidateNotVisitorTest.java

--- warehouse/query-core/src/main/java/datawave/query/lucene/visitors/ValidateNotVisitor.java
+++ warehouse/query-core/src/main/java/datawave/query/lucene/visitors/ValidateNotVisitor.java
@@ -3,22 +3,30 @@ package datawave.query.lucene.visitors;
 import org.apache.lucene.queryparser.flexible.core.nodes.NotBooleanQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
 
+/**
+ * Validates that NOT junctions are unambiguous.
+ */
 public class ValidateNotVisitor extends BaseVisitor{
+
+    public static void validate(QueryNode node) {
+        ValidateNotVisitor visitor = new ValidateNotVisitor();
+        visitor.visit(node, null);
+    }
 
     @Override
     public Object visit(NotBooleanQueryNode node, Object data) {
 
         boolean modSeen = false;
         for(QueryNode child : node.getChildren()){
-            QueryNodeType childType = QueryNodeType.get(child.getClass());
-            switch (childType) {
-                case OR: //NotBooleanQueryNodes cannot have junctions as children
+            QueryNodeType type = QueryNodeType.get(child.getClass());
+            switch (type) {
+                case OR:
                 case AND: {
-                    throw new IllegalArgumentException("NotBooleanQueryNode cannot have junction as a claus. Try wrapping it in parenthesis.");
+                    throw new IllegalArgumentException("NOT junction's terms are ambiguous. Try adding parentheses around its terms.");
                 }
-                case MODIFIER: { //NotBooleanQueryNodes cannot have more than one modifier as children
+                case MODIFIER: {
                     if(modSeen){
-                        throw new IllegalArgumentException("NotBooleanQueryNode cannot have multiple modifiers.");
+                        throw new IllegalArgumentException("NOT junction's terms are ambiguous. Try adding parentheses around its terms.");
                     }
                     modSeen = true;
                     break;
@@ -27,6 +35,6 @@ public class ValidateNotVisitor extends BaseVisitor{
         }
 
         visitChildren(node, data);
-        return data;
+        return null;
     }
 }
