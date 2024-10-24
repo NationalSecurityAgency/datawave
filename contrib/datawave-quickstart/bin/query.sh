607d9196aa1e14b0386b345cac0cbc243df83a6c
skyler-kv <vangkskyler@gmail.com>
2024-10-24 15:57:29 +0000

    closed parenthesis

--- contrib/datawave-quickstart/bin/query.sh
+++ contrib/datawave-quickstart/bin/query.sh
@@ -131,7 +131,7 @@ function setQueryIdFromResponse() {
 }
 
 function prettyPrintJson() {
-    PY_CMD='"from __future__ import print_function; import sys,json; data=json.loads(sys.stdin.read()); print(json.dumps(data, indent=2, sort_keys=True)"'
+    PY_CMD='from __future__ import print_function; import sys,json; data=json.loads(sys.stdin.read()); print(json.dumps(data, indent=2, sort_keys=True))'
     echo "${1}" | ( python3 -c "${PY_CMD}" 2>/dev/null || python2 -c "${PY_CMD}" 2>/dev/null ) || ( warn "Python encountered error. Printed response without formatting" && printRawResponse "${1}" )
 }
 
