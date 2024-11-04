01f12a320697a12c85b7a089e88491a8a015601d
dtspence <33552925+dtspence@users.noreply.github.com>
2024-11-04 21:21:04 +0000

    Rename unit test for main class name

--- warehouse/ingest-core/src/test/java/datawave/ingest/data/config/CachedFieldConfigHelperTest.java
+++ warehouse/ingest-core/src/test/java/datawave/ingest/data/config/CachedFieldConfigHelperTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class CachingFieldConfigHelperTest {
+public class CachedFieldConfigHelperTest {
     @Test
     public void testCachingBehaviorWillCallBaseMethods() {
         String fieldName = "test";
