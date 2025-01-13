59aa7ba61c686cfcb81e91eec2823e54b139a9c6
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2025-01-13 14:35:37 +0000

    Update class level documentation for SeekableIterator interface

--- warehouse/query-core/src/main/java/datawave/query/iterator/SeekableIterator.java
+++ warehouse/query-core/src/main/java/datawave/query/iterator/SeekableIterator.java
@@ -7,7 +7,7 @@ import org.apache.accumulo.core.data.ByteSequence;
 import org.apache.accumulo.core.data.Range;
 
 /**
- *
+ * See {@link NestedIterator#seek(Range, Collection, boolean)} for examples of how this interface was previously used.
  */
 @Deprecated(forRemoval = true, since = "7.13.0")
 public interface SeekableIterator {
