6c77441adfd918db52255306a237407728d38af5
Laura Schanno <lbschanno@gmail.com>
2025-01-17 13:48:54 -0500

    Add documentation for ShiftOnDay

--- warehouse/ingest-core/src/main/java/datawave/ingest/mapreduce/handler/shard/ShiftOnDay.java
+++ warehouse/ingest-core/src/main/java/datawave/ingest/mapreduce/handler/shard/ShiftOnDay.java
@@ -12,12 +12,26 @@ import datawave.ingest.data.RawRecordContainer;
 import datawave.util.time.DateHelper;
 
 /**
- * Implementation of {@link ShardIdGenerator} that will
+ * Implementation of {@link ShardIdGenerator} that will shift the partition of a baseline shard ID for a record by the max number of shards. Given some shard id
+ * {@code 20240110_2}, and a max shards of 10, the shard id will become {@code 20240110_12}. A {@link ShiftOnDay} can be configured with a set of data types, a
+ * beginning date (inclusive), and an end date (inclusive). These attributes will be used by {@link ShiftOnDay#isApplicable(RawRecordContainer)} to determine if
+ * the default shard id for that particular record should be replaced by {@link ShiftOnDay#getShardId(RawRecordContainer, String, int)}.
  */
 public class ShiftOnDay implements ShardIdGenerator {
 
+    /**
+     * The configuration property for the comma-delimited datatypes of a {@link ShiftOnDay}.
+     */
     public static final String DATATYPES = "datatypes";
+
+    /**
+     * The configuration property for the begin date of a {@link ShiftOnDay}.
+     */
     public static final String BEGIN = "begin";
+
+    /**
+     * The configuration property for the end date of a {@link ShiftOnDay}.
+     */
     public static final String END = "end";
 
     private final Set<String> dataTypes;
@@ -46,6 +60,14 @@ public class ShiftOnDay implements ShardIdGenerator {
         validateDateRange(begin, end);
     }
 
+    /**
+     * Throws an {@link IllegalArgumentException} if both the begin and end are non-null, and the begin date is after the end date.
+     *
+     * @param begin
+     *            the begin date
+     * @param end
+     *            the end date
+     */
     private void validateDateRange(Date begin, Date end) {
         if (begin != null && end != null) {
             if (begin.getTime() > end.getTime()) {
@@ -54,13 +76,27 @@ public class ShiftOnDay implements ShardIdGenerator {
         }
     }
 
+    /**
+     * Returns whether this {@link ShiftOnDay} is considered applicable for the given record. It is considered applicable if:
+     * <ol>
+     * <li>This {@link ShiftOnDay} has no datatypes to filter on, or has a datatype that matches the data type of the record.</li>
+     * <li>This {@link ShiftOnDay} has no begin date to filter on, or the record's date matches or occurs after the begin date.</li>
+     * <li>This {@link ShiftOnDay} has no end date to filter on, or the record's date matches or occurs before the begin date.</li>
+     * </ol>
+     *
+     * @param record
+     *            the event record
+     * @return true if this {@link ShiftOnDay} is considered applicable for the given record, or false otherwise.
+     */
     @Override
     public boolean isApplicable(RawRecordContainer record) {
+        // Check the data type.
         if (!dataTypes.isEmpty() && !dataTypes.contains(record.getDataType().typeName())) {
             return false;
         }
 
         if (begin != null || end != null) {
+            // Check the record date.
             long recordDate = record.getDate();
             if (begin != null) {
                 if (recordDate < begin.getTime()) {
@@ -76,6 +112,18 @@ public class ShiftOnDay implements ShardIdGenerator {
         return true;
     }
 
+    /**
+     * Returns a modified form of the given sharded id where the partition portion of the shard id is shifted by the value of numShards. For example, given a
+     * value of {@code "20240110_2"} for baseShardId, and a value of 10 for numShards, {@code "20240110_12"} will be returned.
+     *
+     * @param record
+     *            the record
+     * @param baseShardId
+     *            the base shard id
+     * @param numShards
+     *            the number of shards
+     * @return the modified shard id
+     */
     @Override
     public String getShardId(RawRecordContainer record, String baseShardId, int numShards) {
         String[] parts = datawave.util.StringUtils.split(baseShardId, '_');
