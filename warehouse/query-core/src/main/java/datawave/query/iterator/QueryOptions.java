2724d597ade6b16c1b5c751d99a9cbb85e40b83a
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2025-01-21 11:47:10 +0000

    Actually set the evaluation filter so that filtering works for standard event queries

--- warehouse/query-core/src/main/java/datawave/query/iterator/QueryOptions.java
+++ warehouse/query-core/src/main/java/datawave/query/iterator/QueryOptions.java
@@ -810,6 +810,11 @@ public class QueryOptions implements OptionDescriber {
             // allows standard event queries to perform a seeking aggregation with field filtering
             evaluationFilter = getEventFilter();
         }
+
+        if (evaluationFilter != null) {
+            eventEvaluationFilter = evaluationFilter.clone();
+        }
+
         return eventEvaluationFilter != null ? eventEvaluationFilter.clone() : null;
     }
 
