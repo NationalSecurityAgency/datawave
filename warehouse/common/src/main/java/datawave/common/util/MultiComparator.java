67a196c0c7aa8806b6f01ea19d51682b803db31b
Christopher Gross <christopher.gross@red-drum.com>
2025-02-25 18:34:44 +0000

    remove unused import java.util.Collections

--- warehouse/common/src/main/java/datawave/common/util/MultiComparator.java
+++ warehouse/common/src/main/java/datawave/common/util/MultiComparator.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 
 public class MultiComparator<T> implements Comparator<T>, Serializable {
