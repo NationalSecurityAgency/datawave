f2ca09a339810212a4dd60beeebb5b00890c6996
Joe Alphonso <jfalpho@uwe.nsa.gov>
2025-04-03 14:01:37 -0400

    add missing import

--- core/utils/type-utils/src/test/java/datawave/webservice/query/util/XMLUtilTest.java
+++ core/utils/type-utils/src/test/java/datawave/webservice/query/util/XMLUtilTest.java
@@ -1,5 +1,6 @@
 package datawave.webservice.query.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
