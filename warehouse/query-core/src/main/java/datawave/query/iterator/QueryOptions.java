f2411955bb0f7652f6cc61d155ce757891a8870d
Seth <smuckeratwork@gmail.com>
2025-05-21 12:14:46 -0400

    formatting

--- warehouse/query-core/src/main/java/datawave/query/iterator/QueryOptions.java
+++ warehouse/query-core/src/main/java/datawave/query/iterator/QueryOptions.java
@@ -20,7 +20,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
