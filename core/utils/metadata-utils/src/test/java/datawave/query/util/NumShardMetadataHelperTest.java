395580067f915bbd37428c48363b82d46c01da96
Seth <smuckeratwork@gmail.com>
2025-05-28 11:34:44 -0400

    Broken deps wip

--- core/utils/metadata-utils/src/test/java/datawave/query/util/NumShardMetadataHelperTest.java
+++ core/utils/metadata-utils/src/test/java/datawave/query/util/NumShardMetadataHelperTest.java
@@ -1,7 +1,92 @@
-package datawave.query.util;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class NumShardMetadataHelperTest {
-
-}
+// package datawave.query.util;
+//
+// import java.io.File;
+// import java.io.IOException;
+// import java.net.URISyntaxException;
+// import java.util.Collections;
+// import java.util.Objects;
+// import java.util.Set;
+//
+// import org.apache.accumulo.core.client.AccumuloClient;
+// import org.apache.accumulo.core.client.AccumuloException;
+// import org.apache.accumulo.core.client.AccumuloSecurityException;
+// import org.apache.accumulo.core.client.BatchWriter;
+// import org.apache.accumulo.core.client.BatchWriterConfig;
+// import org.apache.accumulo.core.client.MutationsRejectedException;
+// import org.apache.accumulo.core.client.TableExistsException;
+// import org.apache.accumulo.core.client.TableNotFoundException;
+// import org.apache.accumulo.core.data.Mutation;
+// import org.apache.accumulo.core.data.Value;
+// import org.apache.accumulo.core.security.Authorizations;
+// import org.apache.hadoop.conf.Configuration;
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.BeforeAll;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+//
+// import com.google.common.collect.Maps;
+// import com.google.common.collect.Sets;
+//
+// import datawave.accumulo.inmemory.InMemoryAccumuloClient;
+// import datawave.accumulo.inmemory.InMemoryInstance;
+// import datawave.query.composite.CompositeMetadataHelper;
+//
+// class NumShardMetadataHelperTest {
+//
+// private static final String TABLE_METADATA = "testMetadataTable";
+// private AccumuloClient accumuloClient;
+// // private NumShards numShards;
+//
+// @BeforeAll
+// static void beforeAll() throws URISyntaxException {
+// File dir = new File(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource(".")).toURI());
+// File targetDir = dir.getParentFile();
+// System.setProperty("hadoop.home.dir", targetDir.getAbsolutePath());
+// }
+//
+// @BeforeEach
+// public void setup() throws TableNotFoundException, AccumuloException, TableExistsException, AccumuloSecurityException {
+// accumuloClient = new InMemoryAccumuloClient("root", new InMemoryInstance(MetadataHelperTest.class.toString()));
+// if (!accumuloClient.tableOperations().exists(TABLE_METADATA)) {
+// accumuloClient.tableOperations().create(TABLE_METADATA);
+// }
+// // numShards = new NumShards(new Configuration());
+// }
+//
+// @AfterEach
+// void tearDown() throws AccumuloException, TableNotFoundException, AccumuloSecurityException {
+// accumuloClient.tableOperations().delete(TABLE_METADATA);
+// }
+//
+// @Test
+// public void testSingleFieldFilter() throws TableNotFoundException, AccumuloException, AccumuloSecurityException, IOException {
+// writeMutation("rowA", "t", "dataTypeA", new Value("value"));
+//
+// numShards.updateCache();
+//
+// }
+//
+// private void writeMutation(String row, String columnFamily, String columnQualifier, Value value) throws TableNotFoundException {
+// Mutation mutation = new Mutation(row);
+// mutation.put(columnFamily, columnQualifier, value);
+// writeMutation(mutation);
+// }
+//
+// private void writeMutation(Mutation m) throws TableNotFoundException {
+// BatchWriterConfig config = new BatchWriterConfig();
+// config.setMaxMemory(0);
+// try (BatchWriter writer = accumuloClient.createBatchWriter(TABLE_METADATA, config)) {
+// writer.addMutation(m);
+// writer.flush();
+// } catch (MutationsRejectedException e) {
+// throw new RuntimeException(e);
+// }
+// }
+// // ensure table exists
+//
+// // ensure cache is updated
+//
+// // make sure exceptions are thrown correctly
+//
+// }
