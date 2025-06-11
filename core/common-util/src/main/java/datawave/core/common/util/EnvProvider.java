57ed2cd0fd9f73ca6806e3786858674172e04057
foster33 <84727868+foster33@users.noreply.github.com>
2025-06-11 21:33:18 +0000

    format commit test

--- core/common-util/src/main/java/datawave/core/common/util/EnvProvider.java
+++ core/common-util/src/main/java/datawave/core/common/util/EnvProvider.java
@@ -9,6 +9,7 @@ public class EnvProvider {
 
     private static final Logger log = ThreadConfigurableLogger.getLogger(EnvProvider.class);
 
+
     private static final String ENV_PREFIX = "env:";
 
     private EnvProvider() {}
@@ -23,9 +24,10 @@ public class EnvProvider {
     public static String resolve(String property) {
         if (property != null && property.startsWith(ENV_PREFIX)) {
             String target = property.substring(4);
-            if (StringUtils.isNotBlank(target)) {
+
+            if (StringUtils.isNotBlank(target)){
                 String value = System.getenv(target);
-                if (StringUtils.isNotBlank(value)) {
+                if (StringUtils.isNotBlank(value)){
                     log.trace("env target resolved");
                     return value;
                 } else {
