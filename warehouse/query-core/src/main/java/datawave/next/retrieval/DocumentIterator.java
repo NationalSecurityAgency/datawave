29de204b41a129c90d1b6d25ef70a3dae720ae88
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2025-06-23 12:01:37 +0000

    DocumentIterator can now aggregate non-event fields (#2968)
    Support for index only and term frequency fragments, limited to equality
    terms (i.e., regex/range not supported yet).
    
    Clean up DocumentIteratorOption parsing

--- warehouse/query-core/src/main/java/datawave/next/retrieval/DocumentIterator.java
+++ warehouse/query-core/src/main/java/datawave/next/retrieval/DocumentIterator.java
@@ -2,12 +2,13 @@ package datawave.next.retrieval;
 
 import java.io.IOException;
 import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import org.apache.accumulo.core.data.ByteSequence;
 import org.apache.accumulo.core.data.Key;
@@ -16,22 +17,28 @@ import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.iterators.IteratorEnvironment;
 import org.apache.accumulo.core.iterators.SortedKeyValueIterator;
-import org.apache.commons.jexl3.parser.ASTJexlScript;
-import org.apache.commons.jexl3.parser.ParseException;
 import org.apache.hadoop.io.Text;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
+import com.google.common.collect.TreeMultimap;
+import com.google.protobuf.InvalidProtocolBufferException;
 
+import datawave.ingest.protobuf.TermWeight;
+import datawave.ingest.protobuf.TermWeightPosition;
+import datawave.query.Constants;
 import datawave.query.attributes.Attribute;
 import datawave.query.attributes.AttributeFactory;
+import datawave.query.attributes.Content;
 import datawave.query.attributes.Document;
 import datawave.query.attributes.DocumentKey;
 import datawave.query.data.parsers.EventKey;
-import datawave.query.function.JexlEvaluation;
+import datawave.query.data.parsers.FieldIndexKey;
+import datawave.query.data.parsers.TermFrequencyKey;
 import datawave.query.function.serializer.KryoDocumentSerializer;
-import datawave.query.jexl.DatawaveJexlContext;
-import datawave.query.jexl.HitListArithmetic;
 import datawave.query.jexl.JexlASTHelper;
+import datawave.query.jexl.functions.TermFrequencyList;
+import datawave.query.postprocessing.tf.TermOffsetMap;
 import datawave.query.util.Tuple3;
 
 /**
@@ -95,6 +102,8 @@ public class DocumentIterator extends DocumentIteratorOptions implements SortedK
         EventKey parser = new EventKey();
 
         for (String candidate : candidates) {
+            // must clear state between candidates
+            context.clear();
 
             Range candidateRange = rangeForCandidate(candidate);
             source.seek(candidateRange, excludeCFs, false);
@@ -125,12 +134,13 @@ public class DocumentIterator extends DocumentIteratorOptions implements SortedK
                 d.put(field, attr);
             }
 
-            JexlEvaluation evaluation = new JexlEvaluation(query, new HitListArithmetic());
+            // collect index only fragments
+            collectIndexOnlyFragments(d, attributeFactory, candidate);
 
-            ASTJexlScript queryTree = parse(query);
-            Set<String> identifiers = JexlASTHelper.getIdentifierNames(queryTree);
+            // collect term frequency fragments
+            collectTermFrequencyFragments(d, attributeFactory, candidate);
 
-            DatawaveJexlContext context = new DatawaveJexlContext();
+            // populate context, only pulling in the attributes required by the query
             d.visit(identifiers, context);
 
             boolean matched = evaluation.apply(new Tuple3<>(tk, d, context));
@@ -169,18 +179,118 @@ public class DocumentIterator extends DocumentIteratorOptions implements SortedK
         }
     }
 
-    private ASTJexlScript parse(String query) {
-        try {
-            return JexlASTHelper.parseAndFlattenJexlQuery(query);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     private Range rangeForCandidate(String candidate) {
         Key start = new Key(range.getStartKey().getRow(), new Text(candidate));
         Key stop = start.followingKey(PartialKey.ROW_COLFAM);
         return new Range(start, true, stop, false);
+    }
+
+    private void collectIndexOnlyFragments(Document d, AttributeFactory attributeFactory, String candidate) {
+        if (indexOnlyFieldValues == null) {
+            return;
+        }
+
+        List<Range> ranges = new ArrayList<>();
+        for (String field : indexOnlyFieldValues.keySet()) {
+            for (String value : indexOnlyFieldValues.get(field)) {
+                Text cf = new Text("fi\0" + field);
+                Text cq = new Text(value + "\0" + candidate);
+                Key start = new Key(range.getStartKey().getRow(), cf, cq);
+                Range range = new Range(start, true, start.followingKey(PartialKey.ROW_COLFAM), false);
+                ranges.add(range);
+            }
+        }
+
+        FieldIndexKey fiParser = new FieldIndexKey();
+
+        for (Range range : ranges) {
+            try {
+                source.seek(range, Collections.emptySet(), false);
+                while (source.hasTop()) {
+                    Key key = source.getTopKey();
+                    fiParser.parse(key);
+                    Attribute<?> attribute = attributeFactory.create(fiParser.getField(), fiParser.getValue(), key, fiParser.getDatatype(), true, false);
+                    // I don't remember why this is important, but set the flag
+                    attribute.setFromIndex(true);
+                    d.put(fiParser.getField(), attribute);
+
+                    source.next();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Error collecting index only fragment", e);
+            }
+        }
+    }
+
+    private void collectTermFrequencyFragments(Document d, AttributeFactory attributeFactory, String candidate) {
+        if (termFrequencyFieldValues == null) {
+            return;
+        }
+
+        Text cf = new Text("tf");
+        List<Range> ranges = new ArrayList<>();
+        for (String field : termFrequencyFieldValues.keySet()) {
+            for (String value : termFrequencyFieldValues.get(field)) {
+                Text cq = new Text(candidate + "\0" + value + "\0" + field);
+                Key start = new Key(range.getStartKey().getRow(), cf, cq);
+                Range range = new Range(start, true, start.followingKey(PartialKey.ROW_COLFAM), false);
+                ranges.add(range);
+            }
+        }
+
+        TermFrequencyKey tfParser = new TermFrequencyKey();
+        Map<String,TermFrequencyList> termOffsetMap = Maps.newHashMap();
+
+        for (Range range : ranges) {
+            try {
+                source.seek(range, Collections.emptySet(), false);
+                while (source.hasTop()) {
+                    Key key = source.getTopKey();
+                    tfParser.parse(key);
+
+                    Content content = new Content(tfParser.getValue(), key, true);
+                    d.put(tfParser.getField(), content);
+
+                    TreeMultimap<TermFrequencyList.Zone,TermWeightPosition> offsets = parseTermFrequencyValue(source.getTopValue(), tfParser.getField(),
+                                    candidate);
+
+                    // First time looking up this term in a field
+                    TermFrequencyList tfl = termOffsetMap.get(tfParser.getValue());
+                    if (null == tfl) {
+                        termOffsetMap.put(tfParser.getValue(), new TermFrequencyList(offsets));
+                    } else {
+                        // Merge in the offsets for the current field+term with all previous
+                        // offsets from other fields in the same term
+                        tfl.addOffsets(offsets);
+                    }
+
+                    source.next();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Error collecting index only fragment", e);
+            }
+        }
+
+        context.set(Constants.TERM_OFFSET_MAP_JEXL_VARIABLE_NAME, new TermOffsetMap(termOffsetMap));
+    }
+
+    private final TermWeightPosition.Builder position = new TermWeightPosition.Builder();
+
+    private TreeMultimap<TermFrequencyList.Zone,TermWeightPosition> parseTermFrequencyValue(Value value, String field, String recordId) {
+        TreeMultimap<TermFrequencyList.Zone,TermWeightPosition> offsets = TreeMultimap.create();
+        try {
+            TermWeight.Info twInfo = TermWeight.Info.parseFrom(value.get());
+            TermFrequencyList.Zone twZone = new TermFrequencyList.Zone(field, true, recordId);
+
+            for (int i = 0; i < twInfo.getTermOffsetCount(); i++) {
+                position.setTermWeightOffsetInfo(twInfo, i);
+                offsets.put(twZone, position.build());
+                position.reset();
+            }
+        } catch (InvalidProtocolBufferException e) {
+            return offsets;
+        }
+        return offsets;
     }
 
     @Override
