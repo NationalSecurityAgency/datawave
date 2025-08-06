3a6967c70d8f95bd0d8498acd21f7db4018a82d9
skyler-kv <vangkskyler@gmail.com>
2025-08-06 15:43:12 +0000

    removes unnecessary tests

--- warehouse/query-core/src/test/java/datawave/query/rules/GroupedInterpretationRuleTest.java
+++ warehouse/query-core/src/test/java/datawave/query/rules/GroupedInterpretationRuleTest.java
@@ -11,16 +11,6 @@ class GroupedInterpretationRuleTest extends ShardQueryRuleTest {
         expectRuleName(RULE_NAME);
     }
 
-    /**
-     * Test query without ambiguous phrases.
-     */
-    @Test
-    void testQueryWithoutAmbiguousPhrases() throws Exception {
-        givenQuery("FOO:\"123 456\" OR FOO:bef");
-        // Do not expect any results.
-        assertResult();
-    }
-
     @Test
     void testQueryWithGroupedPhrase() throws Exception {
         givenQuery("FOO:(abc def ghi)");
@@ -89,7 +79,7 @@ class GroupedInterpretationRuleTest extends ShardQueryRuleTest {
      * Test a query with ambiguous phrases after a quoted phrase.
      */
     @Test
-    void testAmbiguousPhraseAfterQuotedFieldedTerm() throws Exception {
+    void testGroupedFieldedTerms() throws Exception {
         givenQuery("(FOO:abc AND BAR:def)");
 
         // Do not expect any results.
@@ -100,18 +90,7 @@ class GroupedInterpretationRuleTest extends ShardQueryRuleTest {
      * Test a query with ambiguous phrases before a fielded term.
      */
     @Test
-    void testAmbiguousPhraseBeforeFieldedTerm() throws Exception {
-        givenQuery("abc def FOO:ghi");
-
-        // Do not expect any results.
-        assertResult();
-    }
-
-    /**
-     * Test a query with ambiguous phrases before a fielded term.
-     */
-    @Test
-    void testMultipleFieldsWithAmbiguousPhrases() throws Exception {
+    void testMultipleFieldsWithGroupedAmbiguousPhrases() throws Exception {
         givenQuery("(FOO:abc def ghi) OR BAR:(aaa bbb ccc) AND 333 HAT:\"111\" 222 AND HEN:car VEE:elephant zebra VEE:deer FOO:(aaa bbb)");
 
         expectMessage("Operator precedence may be missing, field(s): [FOO] with value(s): [abc, def, ghi] will be interpreted as: ( FOO:abc AND def AND ghi )");
