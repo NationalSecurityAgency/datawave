598da597b67270275b08d6f30f2328138ebd98dd
austin007008 <undercovera008@gmail.com>
2025-09-29 19:18:54 +0000

    fixup

--- warehouse/query-core/src/main/java/datawave/query/transformer/SummaryTransform.java
+++ warehouse/query-core/src/main/java/datawave/query/transformer/SummaryTransform.java
@@ -110,7 +110,7 @@ public class SummaryTransform extends DocumentTransform.DefaultDocumentTransform
         if (document.containsKey(Document.DOCKEY_FIELD_NAME)) {
             Attribute<?> attr = document.get(Document.DOCKEY_FIELD_NAME);
             if (attr instanceof DocumentKey) {
-                eventIds.add((DocumentKey) document.get(Document.DOCKEY_FIELD_NAME));
+                eventIds.add((DocumentKey) attr);
             } else if (attr instanceof Attributes) {
                 // if the attr is an instanceof Attributes, then we need to find the one that best describes
                 // the root or TLD document which should be the one with the smallest CF (datatype\x00uid)
