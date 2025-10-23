63b1c5fdd13a33140eb8ef488135e388eba095ad
Seth <smuckeratwork@gmail.com>
2025-10-23 16:06:17 -0400

    update

--- warehouse/core/src/main/java/datawave/mr/bulk/BulkInputFormat.java
+++ warehouse/core/src/main/java/datawave/mr/bulk/BulkInputFormat.java
@@ -17,7 +17,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Properties;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.UUID;
@@ -41,7 +40,6 @@ import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.clientImpl.ClientConfConverter;
 import org.apache.accumulo.core.clientImpl.ClientContext;
 import org.apache.accumulo.core.clientImpl.ClientInfo;
-import org.apache.accumulo.core.clientImpl.TabletLocator;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.PartialKey;
 import org.apache.accumulo.core.data.Range;
@@ -84,7 +82,6 @@ import com.google.common.collect.Multimap;
 
 import datawave.accumulo.inmemory.InMemoryAccumuloClient;
 import datawave.accumulo.inmemory.InMemoryInstance;
-import datawave.accumulo.inmemory.impl.InMemoryTabletLocator;
 import datawave.common.util.ArgumentChecker;
 import datawave.ingest.data.config.ingest.AccumuloHelper;
 import datawave.mr.bulk.split.DefaultLocationStrategy;
@@ -1048,35 +1045,34 @@ public class BulkInputFormat extends InputFormat<Key,Value> {
         return new DefaultLocationStrategy();
     }
 
-//    /**
-//     * Initializes an Accumulo {@link TabletLocator} based on the configuration.
-//     *
-//     * @param conf
-//     *            the Hadoop configuration object
-//     * @return an accumulo tablet locator
-//     * @throws TableNotFoundException
-//     *             if the table name set on the configuration doesn't exist
-//     * @throws IOException
-//     *             if the input format is unable to read the password file from the FileSystem
-//     */
-//    protected static TabletLocator getTabletLocator(Configuration conf) throws TableNotFoundException, IOException {
-//        if (conf.getBoolean(MOCK, false))
-//            return new InMemoryTabletLocator();
-//        String tableName = getTablename(conf);
-//        Properties props = Accumulo.newClientProperties().to(conf.get(INSTANCE_NAME), conf.get(ZOOKEEPERS))
-//                        .as(getUsername(conf), new PasswordToken(getPassword(conf))).build();
-//        ClientInfo info = ClientInfo.from(props);
-//        ClientContext context = new ClientContext(SingletonManager.getClientReservation(), info, ClientConfConverter.toAccumuloConf(info.getProperties()),
-//                        Threads.UEH);
-//        return TabletLocator.getLocator(context, context.getTableId(tableName));
-//    }
+    // /**
+    // * Initializes an Accumulo {@link TabletLocator} based on the configuration.
+    // *
+    // * @param conf
+    // * the Hadoop configuration object
+    // * @return an accumulo tablet locator
+    // * @throws TableNotFoundException
+    // * if the table name set on the configuration doesn't exist
+    // * @throws IOException
+    // * if the input format is unable to read the password file from the FileSystem
+    // */
+    // protected static TabletLocator getTabletLocator(Configuration conf) throws TableNotFoundException, IOException {
+    // if (conf.getBoolean(MOCK, false))
+    // return new InMemoryTabletLocator();
+    // String tableName = getTablename(conf);
+    // Properties props = Accumulo.newClientProperties().to(conf.get(INSTANCE_NAME), conf.get(ZOOKEEPERS))
+    // .as(getUsername(conf), new PasswordToken(getPassword(conf))).build();
+    // ClientInfo info = ClientInfo.from(props);
+    // ClientContext context = new ClientContext(SingletonManager.getClientReservation(), info, ClientConfConverter.toAccumuloConf(info.getProperties()),
+    // Threads.UEH);
+    // return TabletLocator.getLocator(context, context.getTableId(tableName));
+    // }
 
     public List<Range> binRanges(ClientContext context, List<Range> ranges, Map<String,Map<KeyExtent,List<Range>>> binnedRanges)
-            throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
+                    throws AccumuloException, AccumuloSecurityException, TableNotFoundException {
         binnedRanges.put("", Collections.singletonMap(new KeyExtent(TableId.of(""), null, null), ranges));
         return Collections.emptyList();
     }
-
 
     /**
      * Read the metadata table to get tablets and match up ranges to them.
@@ -1098,7 +1094,7 @@ public class BulkInputFormat extends InputFormat<Key,Value> {
 
         // get the metadata information for these ranges
         Map<String,Map<KeyExtent,List<Range>>> binnedRanges = new HashMap<>();
-//        TabletLocator tl;
+        // TabletLocator tl;
         try {
             if (isOfflineScan(job.getConfiguration())) {
                 binnedRanges = binOfflineTable(job, tableName, ranges);
@@ -1110,9 +1106,9 @@ public class BulkInputFormat extends InputFormat<Key,Value> {
             } else {
                 try (AccumuloClient client = getClient(job.getConfiguration())) {
                     TableId tableId = null;
-//                    tl = getTabletLocator(job.getConfiguration());
+                    // tl = getTabletLocator(job.getConfiguration());
                     // its possible that the cache could contain complete, but old information about a tables tablets... so clear it
-//                    tl.invalidateCache();
+                    // tl.invalidateCache();
                     ClientInfo info = ClientInfo.from(cbHelper.newClientProperties());
                     ClientContext context = new ClientContext(SingletonManager.getClientReservation(), info,
                                     ClientConfConverter.toAccumuloConf(info.getProperties()), Threads.UEH);
@@ -1128,7 +1124,7 @@ public class BulkInputFormat extends InputFormat<Key,Value> {
                         binnedRanges.clear();
                         log.warn("Unable to locate bins for specified ranges. Retrying.");
                         TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextInt(100, 200));
-//                        tl.invalidateCache();
+                        // tl.invalidateCache();
                     }
 
                     clipRanges(binnedRanges);
