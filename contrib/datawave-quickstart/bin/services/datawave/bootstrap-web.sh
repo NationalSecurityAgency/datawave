59509e21f42d948f9c0d5871bce878eac05a0e7e
Joe Alphonso <jfalpho@uwe.nsa.gov>
2025-11-03 12:15:39 -0500

    a more accurate fix to the quickstart startup

--- contrib/datawave-quickstart/bin/services/datawave/bootstrap-web.sh
+++ contrib/datawave-quickstart/bin/services/datawave/bootstrap-web.sh
@@ -162,8 +162,8 @@ function datawaveWebStart() {
        fi
     fi
 
-    local pollInterval=4
-    local maxAttempts=15
+    local pollInterval=5
+    local maxAttempts=20
 
     info "Polling for EAR deployment status every ${pollInterval} seconds (${maxAttempts} attempts max)"
 
