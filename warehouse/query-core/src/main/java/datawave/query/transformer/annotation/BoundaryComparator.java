d1a97ad7bc4ca96f9159ae4ff466eb343d1e086b
FineAndDandy <cwilliams.work@gmail.com>
2026-01-05 22:36:01 +0000

    WIP: improve documentation for BoundaryComparator

--- warehouse/query-core/src/main/java/datawave/query/transformer/annotation/BoundaryComparator.java
+++ warehouse/query-core/src/main/java/datawave/query/transformer/annotation/BoundaryComparator.java
@@ -5,12 +5,15 @@ import java.util.Comparator;
 import datawave.annotation.protobuf.v1.SegmentBoundary;
 
 /**
- * Sort by ascending boundary type value > 1 first Sort by descending boundary type < 1 always after all boundary type greater than 0 Secondary sort for equal
- * boundary types, sort by ascending start Tertiary sort for equal start, sort by ascending end
+ * Primarily sorts ascending by boundary type value, with KNOWN boundary types to the front, ascending by value. UNKNOWN boundary type will sort after all KNOWN
+ * boundary types, followed by all UNRECOGNIZED types. Secondary sorts ascending by start. Tertiary sorts ascending by end.
+ *
+ * @see datawave.annotation.protobuf.v1.BoundaryType
  */
 public class BoundaryComparator implements Comparator<SegmentBoundary> {
     @Override
     public int compare(SegmentBoundary o1, SegmentBoundary o2) {
+        // sort null values to the end
         if (o1 == null && o2 == null) {
             return 0;
         } else if (o1 == null) {
@@ -19,18 +22,20 @@ public class BoundaryComparator implements Comparator<SegmentBoundary> {
             return -1;
         }
 
+        // primary sort on boundary type
         if (o1.getBoundaryTypeValue() != o2.getBoundaryTypeValue()) {
+            // excluding UNKNOWN and UNRECOGNIZED
             if (o1.getBoundaryTypeValue() > 0 && o2.getBoundaryTypeValue() > 0) {
                 // lower number sorts first
                 return o1.getBoundaryTypeValue() - o2.getBoundaryTypeValue();
             } else if (o1.getBoundaryTypeValue() < 1 && o2.getBoundaryTypeValue() < 1) {
-                // 0 comes before -1
+                // UNKNOWN comes before UNRECOGNIZED
                 return o2.getBoundaryTypeValue() - o1.getBoundaryTypeValue();
             } else if (o1.getBoundaryTypeValue() < 1) {
-                // anything is lower than -1/0
+                // First value is UNKNOWN/UNRECOGNIZED
                 return 1;
             } else {
-                // anything is before -1/0
+                // Second value is UNKNOWN/UNRECOGNIZED
                 return -1;
             }
         }
