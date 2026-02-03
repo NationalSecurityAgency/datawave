df31f48eb0ca4e9b66c8df042d99a542214bb761
Joe Alphonso <jfalpho@uwe.nsa.gov>
2026-02-03 16:01:43 -0500

    test code change

--- microservices/services/config/src/main/java/datawave/microservice/config/server/ConfigServerApplication.java
+++ microservices/services/config/src/main/java/datawave/microservice/config/server/ConfigServerApplication.java
@@ -15,4 +15,5 @@ public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
+    // test comment
 }
