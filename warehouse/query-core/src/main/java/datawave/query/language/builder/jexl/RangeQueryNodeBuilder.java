cc9fd1caca538251fd1b65afeffa13678ab3a9fb
Joseph Alphonso <jalphonso@users.noreply.github.com>
2026-03-27 10:03:39 -0400

    Spotless maven plugin (#3478)
    replace impsort with spotless maven plugin to automatically cleanup
    unused imports
    formatting is fast and occurs early in the maven lifecycle  
    prevents checkstyle from failing later for unused imports
    
    ---------
    
    Co-authored-by: Joe Alphonso <jfalpho@uwe.nsa.gov>
    Co-authored-by: alerman <awlerma@uwe.nsa.gov>

--- warehouse/query-core/src/main/java/datawave/query/language/builder/jexl/RangeQueryNodeBuilder.java
+++ warehouse/query-core/src/main/java/datawave/query/language/builder/jexl/RangeQueryNodeBuilder.java
@@ -1,15 +1,4 @@
-package datawave.query.language.builder.jexl;
-
-import org.apache.lucene.queryparser.flexible.core.QueryNodeException;
-import org.apache.lucene.queryparser.flexible.core.builders.QueryBuilder;
-import org.apache.lucene.queryparser.flexible.core.nodes.FieldQueryNode;
-import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
-import org.apache.lucene.queryparser.flexible.core.nodes.RangeQueryNode;
-import org.apache.lucene.queryparser.flexible.messages.MessageImpl;
-import org.apache.lucene.queryparser.flexible.standard.nodes.TermRangeQueryNode;
-import org.apache.lucene.search.TermRangeQuery;
-
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -25,6 +14,17 @@ import org.apache.lucene.search.TermRangeQuery;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package datawave.query.language.builder.jexl;
+
+import org.apache.lucene.queryparser.flexible.core.QueryNodeException;
+import org.apache.lucene.queryparser.flexible.core.builders.QueryBuilder;
+import org.apache.lucene.queryparser.flexible.core.nodes.FieldQueryNode;
+import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
+import org.apache.lucene.queryparser.flexible.core.nodes.RangeQueryNode;
+import org.apache.lucene.queryparser.flexible.messages.MessageImpl;
+import org.apache.lucene.queryparser.flexible.standard.nodes.TermRangeQueryNode;
+import org.apache.lucene.search.TermRangeQuery;
+
 import datawave.query.language.parser.jexl.JexlNode;
 import datawave.query.language.parser.jexl.JexlRangeNode;
 
