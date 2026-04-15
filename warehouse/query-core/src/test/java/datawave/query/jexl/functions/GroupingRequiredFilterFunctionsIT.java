1b95d0d8a6dd86b3ab256fda3601bc904a899c17
FineAndDandy <cwilliams.work@gmail.com>
2026-04-15 23:51:33 +0000

    Add functional tests for matchesInGroup and matchesInGroupLeft (#3512)

--- warehouse/query-core/src/test/java/datawave/query/jexl/functions/GroupingRequiredFilterFunctionsIT.java
+++ warehouse/query-core/src/test/java/datawave/query/jexl/functions/GroupingRequiredFilterFunctionsIT.java
@@ -25,7 +25,11 @@ import datawave.query.util.TypeMetadata;
 public class GroupingRequiredFilterFunctionsIT {
 
     private final Key docKey = new Key("20250703", "datatype\0uid");
+<<<<<<< HEAD
     private final Set<String> fields = Set.of("FIELD", "FIELD_A");
+=======
+    private final Set<String> fields = Set.of("FIELD", "FIELD1");
+>>>>>>> 1991d15203 (Add functional tests for matchesInGroup and matchesInGroupLeft (#3512))
 
     private String query;
     private Document document;
@@ -60,82 +64,82 @@ public class GroupingRequiredFilterFunctionsIT {
     // query;data1,data2,data3,...dataN;result
     @CsvSource(delimiter = ';', value = {
             // matchesInGroup() by default only matches on the last value AFTER the LAST .
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.3=a,FIELD_A.1.2.3=b; true",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.3=a,FIELD_A.1.2.3=b; true",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.3=a,FIELD_A.1.1.3=b; true",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.3=a,FIELD_A.2.2.3=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.3=a,FIELD1.1.2.3=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.3=a,FIELD1.1.2.3=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.3=a,FIELD1.1.1.3=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.3=a,FIELD1.2.2.3=b; true",
             // none of these should match even though they match on the left
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.0=a,FIELD_A.1.2.1=b; false",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.0=a,FIELD_A.1.1.1=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.0=a,FIELD1.1.2.1=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.0=a,FIELD1.1.1.1=b; false",
             // no overlap should never match
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.0=a,FIELD_A.0.1.1=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.0=a,FIELD1.0.1.1=b; false",
             // it does not matter if there are the same number of groups, as long as the last one matches
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2=a,FIELD_A.2.2.2=b; true",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD.2=a,FIELD_A.2.2.2=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.1.2=a,FIELD1.2.2.2=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD.2=a,FIELD1.2.2.2=b; true",
             // there must be at least one group on each field
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD=a,FIELD_A.2.2.2=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD=a,FIELD1.2.2.2=b; false",
             // even if neither has a group it doesn't match in group
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b'); FIELD=a,FIELD_A=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b'); FIELD=a,FIELD1=b; false",
             // can match on different values on the same field as long as they are in the same group
             "grouping:matchesInGroup(FIELD, 'a', FIELD, 'b'); FIELD.1.2.3=a,FIELD.1.2.3=b; true",
 
             // matches in groups final argument dictates the index of the grouping notation from the RIGHT (0 indexed)
             // 0 - is the same as default, match rightmost group only
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b', 0); FIELD.0.0.3=a,FIELD_A.9.9.3=b; true",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b', 0); FIELD.0.0.0=a,FIELD_A.9.9.9=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b', 0); FIELD.0.0.3=a,FIELD1.9.9.3=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b', 0); FIELD.0.0.0=a,FIELD1.9.9.9=b; false",
             // 1 - match two rightmost groups only
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b', 1); FIELD.1.2.3=a,FIELD_A.0.2.3=b; true",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b', 1); FIELD.1.2.3=a,FIELD_A.1.1.3=b; false",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b', 1); FIELD.1.2.3=a,FIELD_A.1.2.4=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b', 1); FIELD.1.2.3=a,FIELD1.0.2.3=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b', 1); FIELD.1.2.3=a,FIELD1.1.1.3=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b', 1); FIELD.1.2.3=a,FIELD1.1.2.4=b; false",
             // 2 - can match all grouping
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b', 2); FIELD.1.2.3=a,FIELD_A.1.2.3=b; true",
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b', 2); FIELD.1.2.3=a,FIELD_A.0.2.3=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b', 2); FIELD.1.2.3=a,FIELD1.1.2.3=b; true",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b', 2); FIELD.1.2.3=a,FIELD1.0.2.3=b; false",
             // 10 - if there aren't enough indexes in the group it can't be true even if otherwise is a match
-            "grouping:matchesInGroup(FIELD, 'a', FIELD_A, 'b', 10); FIELD.1.2.3=a,FIELD_A.1.2.3=b; false",
+            "grouping:matchesInGroup(FIELD, 'a', FIELD1, 'b', 10); FIELD.1.2.3=a,FIELD1.1.2.3=b; false",
 
             // supports regexes on either argument
-            "grouping:matchesInGroup(FIELD, 'a*', FIELD_A, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD_A.1.2.3=bbbbbbbbbb; true",
+            "grouping:matchesInGroup(FIELD, 'a*', FIELD1, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD1.1.2.3=bbbbbbbbbb; true",
             // regex can be full wildcards
-            "grouping:matchesInGroup(FIELD, '.*', FIELD_A, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD_A.1.2.3=bbbbbbbbbb; true",
+            "grouping:matchesInGroup(FIELD, '.*', FIELD1, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD1.1.2.3=bbbbbbbbbb; true",
             // regex can include range quantifiers
-            "grouping:matchesInGroup(FIELD, 'a{8}', FIELD_A, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD_A.1.2.3=bbbbbbbbbb; true",
+            "grouping:matchesInGroup(FIELD, 'a{8}', FIELD1, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD1.1.2.3=bbbbbbbbbb; true",
             // can be complex patterns with lookahead: one digit, one upper case, 8+ characters
-            "grouping:matchesInGroup(FIELD, '^(?=.*\\\\d)(?=.*[A-Z]).{8,}$', FIELD_A, 'b*'); FIELD.1.2.3=bb7dfZuq,FIELD_A.1.2.3=bbbbbbbbbb; true",
+            "grouping:matchesInGroup(FIELD, '^(?=.*\\\\d)(?=.*[A-Z]).{8,}$', FIELD1, 'b*'); FIELD.1.2.3=bb7dfZuq,FIELD1.1.2.3=bbbbbbbbbb; true",
 
             // matchesInGroupLeft should work the same as matchesInGroup but with an index offset from the left but
             // currently does not. The current behavior is show below in tests. When unexpected, the correct response
             // is indicated. Problem appears to be in EvaluationPhaseFilterFunctions.getMatchToLeftOfPeriod()
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.3=a,FIELD_A.1.2.3=b; true",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.3=a,FIELD1.1.2.3=b; true",
             // CURRENT BEHAVIOR: 0-index is acting like 1-index, this should still be true
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b'); FIELD.1.0.3=a,FIELD_A.1.9.3=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b'); FIELD.1.0.3=a,FIELD1.1.9.3=b; false",
             // CURRENT BEHAVIOR: 1-index is acting like 0-index, this should still be false
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b', 1); FIELD.000.999.0=a,FIELD_A.000.888.9=b; true",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b', 1); FIELD.000.999.0=a,FIELD1.000.888.9=b; true",
             // CURRENT BEHAVIOR: 2-index is acting like 3-index, this should be true
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b', 2); FIELD.000.999.1=a,FIELD_A.000.999.1=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b', 2); FIELD.000.999.1=a,FIELD1.000.999.1=b; false",
 
             // 0 - 1 does not match 0
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.3=a,FIELD_A.0.2.3=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.3=a,FIELD1.0.2.3=b; false",
             // 0 - explicit
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b', 0); FIELD.1.2.3=a,FIELD_A.0.2.3=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b', 0); FIELD.1.2.3=a,FIELD1.0.2.3=b; false",
             // 0 - 1 does not match 0
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.3=a,FIELD_A.0.3.3=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.3=a,FIELD1.0.3.3=b; false",
             // 0 - explicit
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b', 0); FIELD.1.2.3=a,FIELD_A.0.3.3=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b', 0); FIELD.1.2.3=a,FIELD1.0.3.3=b; false",
             // 0 - 1 does not match 0
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b'); FIELD.1.2.3=a,FIELD_A.0.2.4=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b'); FIELD.1.2.3=a,FIELD1.0.2.4=b; false",
             // 0 - explicit
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b', 0); FIELD.1.2.3=a,FIELD_A.0.2.4=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b', 0); FIELD.1.2.3=a,FIELD1.0.2.4=b; false",
             // 1 - 000.9999 matches 000.999
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b', 1); FIELD.000.999.0=a,FIELD_A.000.999.9=b; true",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b', 1); FIELD.000.999.0=a,FIELD1.000.999.9=b; true",
             // 1 - 000.999 does not match 111.999
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b', 1); FIELD.000.999.0=a,FIELD_A.111.999.9=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b', 1); FIELD.000.999.0=a,FIELD1.111.999.9=b; false",
             // 3 - if there aren't enough indexes in the group it can't be true even if otherwise is a match
-            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD_A, 'b', 3); FIELD.000.999.0=a,FIELD_A.000.999.0=b; false",
+            "grouping:matchesInGroupLeft(FIELD, 'a', FIELD1, 'b', 3); FIELD.000.999.0=a,FIELD1.000.999.0=b; false",
 
             // matchesInGroupLeft also supports regex
-            "grouping:matchesInGroupLeft(FIELD, 'a*', FIELD_A, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD_A.1.2.3=bbbbbbbbbb; true",
-            "grouping:matchesInGroupLeft(FIELD, 'a{8}', FIELD_A, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD_A.1.2.3=bbbbbbbbbb; true",
-            "grouping:matchesInGroupLeft(FIELD, '^(?=.*\\\\d)(?=.*[A-Z]).{8,}$', FIELD_A, 'b*'); FIELD.1.2.3=bb7dfZuq,FIELD_A.1.2.3=bbbbbbbbbb; true",})
+            "grouping:matchesInGroupLeft(FIELD, 'a*', FIELD1, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD1.1.2.3=bbbbbbbbbb; true",
+            "grouping:matchesInGroupLeft(FIELD, 'a{8}', FIELD1, 'b*'); FIELD.1.2.3=aaaaaaaa,FIELD1.1.2.3=bbbbbbbbbb; true",
+            "grouping:matchesInGroupLeft(FIELD, '^(?=.*\\\\d)(?=.*[A-Z]).{8,}$', FIELD1, 'b*'); FIELD.1.2.3=bb7dfZuq,FIELD1.1.2.3=bbbbbbbbbb; true",})
     @ParameterizedTest(name = "{0} against {1} should be {2}")
     public void functionalTests(String query, String data, boolean result) {
         withQuery(query);
