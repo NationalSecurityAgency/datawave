d67651f42d369629c296af28254c4020a189388e
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2026-06-02 09:32:43 -0400

    Add module core/table (#3567)
    Create module core/tableto begin breaking apart the cyclic dependency
    between query and ingest
    
    Extracted ColumnFamilyConstants and LocalityGroupConstants from
    ExtendedDataTypeHandler to core/data module
    
    Replaced all references to ExtendedDataTypeHandler constants with
    references to core/data

--- warehouse/ingest-json/src/test/java/datawave/ingest/json/mr/handler/ColumnBasedHandlerTestUtil.java
+++ warehouse/ingest-json/src/test/java/datawave/ingest/json/mr/handler/ColumnBasedHandlerTestUtil.java
@@ -33,6 +33,7 @@ import datawave.ingest.mapreduce.job.BulkIngestKey;
 import datawave.ingest.mapreduce.job.writer.AbstractContextWriter;
 import datawave.ingest.test.StandaloneStatusReporter;
 import datawave.ingest.test.StandaloneTaskAttemptContext;
+import datawave.table.constants.ColumnFamilyConstants;
 import datawave.util.TableName;
 
 /**
@@ -50,7 +51,7 @@ public class ColumnBasedHandlerTestUtil {
     private static Logger log = Logger.getLogger(ColumnBasedHandlerTestUtil.class);
 
     public static boolean isDocumentKey(Key k) {
-        return isShardKey(k) && k.getColumnFamily().toString().equals(ExtendedDataTypeHandler.FULL_CONTENT_COLUMN_FAMILY);
+        return isShardKey(k) && k.getColumnFamily().toString().equals(ColumnFamilyConstants.FULL_CONTENT);
     }
 
     public static boolean isShardKey(Key k) {
