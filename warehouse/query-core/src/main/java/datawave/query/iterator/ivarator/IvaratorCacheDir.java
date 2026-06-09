a81134731630c79f14c27924688b14672dae54f1
Moriarty <22225248+apmoriarty@users.noreply.github.com>
2026-06-09 16:13:06 +0000

    Move directory validation logic to IvaratorCacheDir

--- warehouse/query-core/src/main/java/datawave/query/iterator/ivarator/IvaratorCacheDir.java
+++ warehouse/query-core/src/main/java/datawave/query/iterator/ivarator/IvaratorCacheDir.java
@@ -1,8 +1,15 @@
 package datawave.query.iterator.ivarator;
 
+import java.io.IOException;
+
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FsStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IvaratorCacheDir {
+
+    private static final Logger log = LoggerFactory.getLogger(IvaratorCacheDir.class);
 
     final protected IvaratorCacheDirConfig config;
 
@@ -24,6 +31,26 @@ public class IvaratorCacheDir {
 
     public FileSystem getFs() {
         return fs;
+    }
+
+    public boolean validateFS() {
+        FsStatus fsStatus = null;
+        try {
+            fsStatus = getFs().getStatus();
+        } catch (IOException e) {
+            log.warn("Unable to determine status of the filesystem: {}", getFs());
+        }
+
+        // determine whether this fs is a good candidate
+        if (fsStatus != null) {
+            long availableStorageMiB = fsStatus.getRemaining() / 0x100000L;
+            double availableStoragePercent = (double) fsStatus.getRemaining() / fsStatus.getCapacity();
+
+            // if we are using less than our storage limit, the cache dir is valid
+            return availableStorageMiB >= config.getMinAvailableStorageMiB() && availableStoragePercent >= config.getMinAvailableStoragePercent();
+        }
+
+        return false;
     }
 
     public String getPathURI() {
