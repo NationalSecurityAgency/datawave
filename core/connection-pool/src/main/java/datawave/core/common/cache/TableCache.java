1c988b193d6f601ea099cac8c2ea7cdbcc4efa9e
Daniel Roberts ddanielr <ddanielr@gmail.com>
2026-07-13 16:35:47 +0000

    Switch over to new InMemoryAccumulo

--- core/connection-pool/src/main/java/datawave/core/common/cache/TableCache.java
+++ core/connection-pool/src/main/java/datawave/core/common/cache/TableCache.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 
-import datawave.accumulo.inmemory.InMemoryInstance;
+import datawave.accumulo.inmemory.InMemoryAccumulo;
 import datawave.core.common.connection.AccumuloConnectionFactory;
 
 public interface TableCache extends Callable<Boolean>, Serializable {
@@ -24,7 +24,7 @@ public interface TableCache extends Callable<Boolean>, Serializable {
 
     AccumuloConnectionFactory getConnectionFactory();
 
-    InMemoryInstance getInstance();
+    InMemoryAccumulo getInstance();
 
     SharedCacheCoordinator getWatcher();
 
@@ -44,7 +44,7 @@ public interface TableCache extends Callable<Boolean>, Serializable {
 
     void setConnectionFactory(AccumuloConnectionFactory connectionFactory);
 
-    void setInstance(InMemoryInstance instance);
+    void setInstance(InMemoryAccumulo instance);
 
     void setWatcher(SharedCacheCoordinator watcher);
 
