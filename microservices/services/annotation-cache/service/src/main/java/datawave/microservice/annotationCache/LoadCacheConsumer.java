c97533487714236dd47d3b0bbbebc42cae9332d9
FineAndDandy <cwilliams.work@gmail.com>
2026-07-17 19:49:30 +0000

    demonstrate pulling an object off the queue and adding it to the hazelcast

--- microservices/services/annotation-cache/service/src/main/java/datawave/microservice/annotationCache/LoadCacheConsumer.java
+++ microservices/services/annotation-cache/service/src/main/java/datawave/microservice/annotationCache/LoadCacheConsumer.java
@@ -1,43 +1,51 @@
 package datawave.microservice.annotationCache;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
+import com.beust.jcommander.Strings;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.HazelcastInstanceAware;
+import com.hazelcast.map.IMap;
 
 import datawave.annotation.protobuf.v1.Annotation;
 
-@Component
-public class LoadCacheConsumer implements HazelcastInstanceAware {
+/**
+ * This is used by application.yml to subscribe to loadCache for incoming messages. They should add annotations to the cache if not already present
+ */
+@Configuration
+public class LoadCacheConsumer {
     private static Logger log = LoggerFactory.getLogger(LoadCacheConsumer.class);
 
-    private HazelcastInstance hazelcastInstance;
+    private final HazelcastInstance hazelcastInstance;
 
-    //
-    // public LoadCacheConsumer(HazelcastInstance instance) {
-    // this.instance = instance;
-    // log.info("Wired up LoadCacheConsumer: " + instance);
-    // }
-
-    @Override
-    public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
+    public LoadCacheConsumer(HazelcastInstance hazelcastInstance) {
         this.hazelcastInstance = hazelcastInstance;
-        log.info("activated hzInstance: " + hazelcastInstance);
-    }
-
-    public LoadCacheConsumer() {
-        log.info("created");
+        log.info("LoadCacheConsumer successfully created and activated with Hazelcast: " + hazelcastInstance);
     }
 
     @Bean
     public Consumer<Annotation> loadCache() {
         return annotation -> {
             log.info("got annotation off queue: " + annotation.getAnnotationId());
+            if (hazelcastInstance != null) {
+                IMap<String,Annotation> annotationMap = hazelcastInstance.getMap("annotations");
+
+                String docId = docKey(annotation);
+                if (annotationMap.putIfAbsent(docId, annotation).equals(annotation)) {
+                    log.info("stored object from queue to cache: " + annotation.getAnnotationId());
+                } else {
+                    log.info("existing object already in cache: " + annotation.getAnnotationId());
+                }
+            }
         };
+    }
+
+    private String docKey(Annotation a) {
+        return Strings.join("/", List.of(a.getShard(), a.getDataType(), a.getUid(), a.getAnnotationId()));
     }
 }
