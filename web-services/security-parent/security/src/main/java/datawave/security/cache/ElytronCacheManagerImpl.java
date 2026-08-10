2ae49ae5e162832e8f7e4fa60f4bad08dc29b65b
Laura Schanno <lbschanno@gmail.com>
2026-08-10 07:12:14 -0400

    Use read/write lock in ElytronCacheManagerImpl

--- web-services/security-parent/security/src/main/java/datawave/security/cache/ElytronCacheManagerImpl.java
+++ web-services/security-parent/security/src/main/java/datawave/security/cache/ElytronCacheManagerImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 import javax.annotation.security.PermitAll;
@@ -25,63 +27,101 @@ import datawave.security.authorization.DatawaveUser;
 public class ElytronCacheManagerImpl implements ElytronCacheManager {
 
     private static final Logger log = LoggerFactory.getLogger(ElytronCacheManagerImpl.class);
-
+    
+    /**
+     * The list of managed Elytron caches.
+     */
     private final List<ElytronCache> caches = new ArrayList<>();
-
+    
+    /**
+     * Guards against concurrent read/write operations.
+     */
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    
     @Override
     public void addCache(ElytronCache cache) {
         Preconditions.checkNotNull(cache, "cache cannot be null");
+       
+        lock.writeLock().lock();
         if (log.isTraceEnabled()) {
             log.trace("Adding cache {}", cache.getClass().getName());
         }
-        this.caches.add(cache);
+        try {
+            this.caches.add(cache);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     @Override
     public Set<DatawaveUser> getUsers() {
-        // @formatter:off
-        return caches.stream()
-                        .map(ElytronCache::getUsers)
-                        .flatMap(Collection::stream)
-                        .collect(Collectors.toSet());
-        // @formatter:on
+        lock.readLock().lock();
+        try {
+            // @formatter:off
+            return caches.stream()
+                            .map(ElytronCache::getUsers)
+                            .flatMap(Collection::stream)
+                            .collect(Collectors.toSet());
+            // @formatter:on
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public Set<DatawaveUser> getUsersWhereNameContains(String substring) {
         Preconditions.checkNotNull(substring, "substring cannot be null");
-        // @formatter:off
-        return caches.stream()
-                        .map(cache -> cache.getUsersWhereNameContains(substring))
-                        .flatMap(Collection::stream)
-                        .collect(Collectors.toSet());
-        // @formatter:on
+        lock.readLock().lock();
+        try {
+            // @formatter:off
+            return caches.stream()
+                            .map(cache -> cache.getUsersWhereNameContains(substring))
+                            .flatMap(Collection::stream)
+                            .collect(Collectors.toSet());
+            // @formatter:on
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public DatawaveUser getUserWithName(String name) {
         Preconditions.checkNotNull(name, "name cannot be null");
-
-        for (ElytronCache cache : caches) {
-            DatawaveUser user = cache.getUserWithName(name);
-            if (user != null) {
-                return user;
+        lock.readLock().lock();
+        try {
+            for (ElytronCache cache : caches) {
+                DatawaveUser user = cache.getUserWithName(name);
+                if (user != null) {
+                    return user;
+                }
             }
+            return null;
+        } finally {
+            lock.readLock().unlock();
         }
-        return null;
     }
 
     @Override
     public void evictUsersWithName(String name) {
-        if (log.isTraceEnabled()) {
-            log.trace("Evicting all users with name {}", name);
+        lock.writeLock().lock();
+        try {
+            if (log.isTraceEnabled()) {
+                log.trace("Evicting all users with name {}", name);
+            }
+            caches.forEach(delegate -> delegate.evictUsersWithName(name));
+        } finally {
+            lock.writeLock().unlock();
         }
-        caches.forEach(delegate -> delegate.evictUsersWithName(name));
     }
 
     @Override
     public void clear() {
-        log.trace("Clearing all caches");
-        caches.forEach(ElytronCache::clear);
+        lock.writeLock().lock();
+        try {
+            log.trace("Clearing all caches");
+            caches.forEach(ElytronCache::clear);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 }
