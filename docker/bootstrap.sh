81759157ce662c0688494a4608601074008df15c
Adam Lerman <awlerma@uwe.nsa.gov>
2026-08-18 19:13:53 -0400

    Codex assisted: Replace Quickstart container with using the containers from NationalSecurityAgency/datawave-stack-docker-images

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -3,7 +3,7 @@ echo
 
 # Ensure that permissions are set correctly for the config files
 chmod -R 755 config pki rabbitmq-config
-chmod -R 755 config pki qr-rabbitmq-config
+chmod -R 755 config pki rabbitmq-query-config
 
 DW_HOSTNAME=$(hostname)
 DW_HOSTNAME=${DW_HOSTNAME%%.*}
@@ -21,26 +21,23 @@ if [ "$1" == "hybrid" ] ; then
    DW_ZOOKEEPER_HOST=${DW_HOSTNAME}
    DW_HADOOP_HOST=${DW_HOSTNAME}
 else
-   COMPOSE_PROFILES=quickstart
-   DW_ZOOKEEPER_HOST=quickstart
-   DW_HADOOP_HOST=quickstart
+   COMPOSE_PROFILES=datawave-stack
+   DW_ZOOKEEPER_HOST=zookeeper
+   DW_HADOOP_HOST=hdfs-nn
 fi
 
 ENV_CONF="\
-# If set to quickstart, enables the quickstart container
+# Enables the reusable Hadoop/Accumulo stack and DataWave fixture loader
 # Note: More than one profile may be set.
 COMPOSE_PROFILES=\"${COMPOSE_PROFILES}\"
 
 # These environment variables are used to create extra hosts which
-# allow containers to route to the host quickstart deployment.
-# The extra hosts aren't used when deploying the docker quickstart,
-# but the variables still need to be set for the compose file to be valid.
+# allow containers to route to services running on the host in hybrid mode.
 DW_HOSTNAME=\"${DW_HOSTNAME}\"
 DW_HOST_FQDN=\"${DW_HOST_FQDN}\"
 DW_HOST_IP=\"${DW_HOST_IP}\"
 
-# These environment variables must be set when running the quickstart
-# from the host machine in hybrid mode.
+# Backend service locations for the selected deployment mode.
 DW_ZOOKEEPER_HOST=\"${DW_ZOOKEEPER_HOST}\"
 DW_HADOOP_HOST=\"${DW_HADOOP_HOST}\"
 "
