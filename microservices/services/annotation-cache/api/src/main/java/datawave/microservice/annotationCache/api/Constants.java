a5aae840511c87f5077ac622448a314fbb91bcdf
FineAndDandy <cwilliams.work@gmail.com>
2026-09-17 17:46:18 +0000

    Add PersistenceMode to annotation-cache api

--- microservices/services/annotation-cache/api/src/main/java/datawave/microservice/annotationCache/api/Constants.java
+++ microservices/services/annotation-cache/api/src/main/java/datawave/microservice/annotationCache/api/Constants.java
@@ -12,5 +12,8 @@ public final class Constants {
     /** AnnotationMessage parameter identifying the region in which the message originated. */
     public static final String REGION_ID_PARAMETER = "region.id";
 
+    /** AnnotationMessage parameter controlling whether a Hazelcast insertion should be written through to RabbitMQ. */
+    public static final String PERSISTENCE_MODE_PARAMETER = "persistence.mode";
+
     private Constants() {}
 }
